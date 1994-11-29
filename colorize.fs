d96e764cf4a2ed9da10c63a90fe7e26511e2a1e3
pazsan <pazsan>
1994-11-29 16:50:24 +0000

    * forgotten file

---
+++ colorize.fs
@@ -0,0 +1,40 @@
+\ COLORIZE.STR  Coloured .NAME and WORDS                20may93jaw
+
+include ansi.fs
+
+decimal
+
+CREATE CT 30 cells allot
+: CT! cells CT + ! ;
+: CT@ cells CT + @ ;
+
+VARIABLE Color 20 Color !
+
+: Color: Color @ 1 Color +! constant ;
+
+Color: Hig#
+
+<A White >f A>          0 CT!
+<A White >f bold A>     Ali# CT!
+<A Brown >f A>          Con# CT!
+<A Green >f A>          Var# CT!
+<A Cyan >f A>           Def# CT!
+<A Yellow >f A>         Val# CT!
+<A Brown >f bold A>     Doe# CT!
+<A Yellow >f bold A>    Col# CT!
+<A Blue >f bold A>      Pri# CT!
+<A Red >f bold A>       Str# CT!
+<A Green >f bold A>     Com# CT!
+<A Red >b A>            Hig# CT!
+
+: (.name) ( lfa -- )
+        cell+ dup
+        wordinfo cells ct + @ attr!
+        count $1f and type blackspace ;
+
+: .name (.name) ct @ attr! ;
+
+: words  context @
+         BEGIN  @ dup  WHILE  dup (.name)  REPEAT drop
+         ct @ attr! ;
+
