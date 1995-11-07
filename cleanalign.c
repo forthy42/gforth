dc1b1a934f1777b54a7a9f69dc2b4374030dfd1c
anton <anton>
1995-11-07 18:08:47 +0000

    source.fs contains some stuff factored out from assert.fs and debugging.fs cleanalign contains alignment-clean replacements for library functions

---
+++ cleanalign.c
@@ -0,0 +1,42 @@
+/* alignment-clean replacements for library functions
+
+  Copyright (C) 1995 Free Software Foundation, Inc.
+
+  This file is part of Gforth.
+
+  Gforth is free software; you can redistribute it and/or
+  modify it under the terms of the GNU General Public License
+  as published by the Free Software Foundation; either version 2
+  of the License, or (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+
+void *memcpy(void *dest, const void *src, int n)
+{
+  int i;
+  char *s, *t;
+  for (s=dest, t=src, i=0; i<n; i++)
+    *s++=*t++;
+  return dest;
+}
+
+char *memmove(char *dest, const char *src, long n)
+{
+  int i;
+
+  if (dest<src)
+    for (i=0; i<n; i++)
+      dest[i]=src[i];
+  else
+    for(i=n-1; i>=0; i--)
+      dest[i]=src[i];
+  return dest;
+}
---
+++ source.fs
@@ -0,0 +1,44 @@
+\ source location handling
+
+\ Copyright (C) 1995 Free Software Foundation, Inc.
+
+\ This file is part of Gforth.
+
+\ Gforth is free software; you can redistribute it and/or
+\ modify it under the terms of the GNU General Public License
+\ as published by the Free Software Foundation; either version 2
+\ of the License, or (at your option) any later version.
+
+\ This program is distributed in the hope that it will be useful,
+\ but WITHOUT ANY WARRANTY; without even the implied warranty of
+\ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+\ GNU General Public License for more details.
+
+\ You should have received a copy of the GNU General Public License
+\ along with this program; if not, write to the Free Software
+\ Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+
+\ related stuff can be found in kernal.fs
+
+\ this stuff is used by (at least) assert.fs and debugging.fs
+
+require struct.fs
+
+struct
+    1 cells: field sourcepos-name#
+    1 cells: field sourcepos-line#
+end-struct sourcepos
+    
+: sourcepos, ( -- )
+    \ record the current source position HERE
+    loadfilename# @ , loadline @ , ;
+
+: get-sourcepos ( a-addr -- c-addr u n )
+    \ c-addr u is the filename, n is the line number
+    included-files 2@ drop over sourcepos-name# @ 2* cells + 2@
+    rot sourcepos-line# @ ;
+
+: print-sourcepos ( a-addr -- )
+    get-sourcepos
+    >r type ." :" r> 0 .r ;
