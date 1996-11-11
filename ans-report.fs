38981f6333daf1958bdcb34f1aab5d7d9c975d21
anton <anton>
1996-11-11 16:59:14 +0000

    Added ans-report.fs objects.fs

---
+++ ans-report.fs
@@ -0,0 +1,166 @@
+\ report words used from the various wordsets
+
+\ Copyright (C) 1996 Free Software Foundation, Inc.
+
+\ This file is part of Gforth.
+
+\ Gforth is free software; you can redistribute it and/or
+\ modify it under the terms of the GNU General Public License
+\ as published by the Free Software Foundation; either version 2
+\ of the License, or (at your option) any later version.
+
+\ This program is distributed in the hope that it will be useful,
+\ but WITHOUT ANY WARRANTY; without even the implied warranty of
+\ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+\ GNU General Public License for more details.
+
+\ You should have received a copy of the GNU General Public License
+\ along with this program; if not, write to the Free Software
+\ Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+
+\ Use this program like this:
+\ include it, then the program you want to check; then say print-ans-report
+\ e.g., start it with
+\  gforth ans-report.fs myprog.fs -e "print-ans-report bye"
+
+\ Caveats:
+
+\ Note that this program just checks which words are used, not whether
+\ they are used in an ANS Forth conforming way!
+
+\ Some words are defined in several wordsets in the standard. This
+\ program reports them for only one of the wordsets, and not
+\ necessarily the one you expect.
+
+
+\ This program uses Gforth internals and won't be easy to port
+\ to other systems.
+
+vocabulary ans-report-words ans-report-words definitions
+
+: wordset ( "name" -- )
+    lastxt >body
+    create
+    0 , \ link to next wordset
+    0 0 2, \ array of nfas
+    ( lastlinkp ) last @ swap ! \ set link ptr of last wordset
+;
+
+wordlist constant wordsets wordsets set-current
+create CORE 0 , 0 0 2,
+wordset CORE-EXT
+wordset BLOCK
+wordset BLOCK-EXT
+wordset DOUBLE
+wordset DOUBLE-EXT
+wordset EXCEPTION
+wordset EXCEPTION-EXT
+wordset FACILITY
+wordset FACILITY-EXT
+wordset FILE
+wordset FILE-EXT
+wordset FLOAT
+wordset FLOAT-EXT
+wordset LOCAL
+wordset LOCAL-EXT
+wordset MEMORY
+wordset SEARCH
+wordset SEARCH-EXT
+wordset STRING
+wordset TOOLS
+wordset TOOLS-EXT
+wordset non-ANS
+ans-report-words definitions
+
+: answord ( "name wordset pronounciation" -- )
+    \ check the documentaion of an ans word
+    name { D: wordname }
+    name { D: wordset }
+    name { D: pronounciation }
+    wordname find-name
+    ?dup-if
+	sp@ cell nextname create drop
+	wordset wordsets search-wordlist 0= abort" wordlist unknown" ,
+    endif ;
+
+table constant answords answords set-current
+warnings @ warnings off
+include answords.fs
+warnings !
+ans-report-words definitions
+
+: add-unless-present ( nt addr -- )
+    \ add nt to array described by addr 2@, unless it contains nt
+    >r ( nt )
+    r@ 2@ bounds
+    u+do ( nt )
+	dup i @ =
+	if
+	    drop rdrop UNLOOP EXIT
+	endif
+	cell
+    +loop
+    r@ 2@ cell extend-mem r> 2!
+    ( nt addr ) ! ;
+
+
+: note-name ( nt -- )
+    \ remember name in the appropriate wordset, unless already there
+    \ or the word is defined in the checked program
+    dup [ here ] literal >		     \ word defined by the application
+    over locals-buffer dup 1000 + within or  \ or a local
+    if
+	drop EXIT
+    endif
+    sp@ cell answords search-wordlist ( nt xt true | nt false )
+    if \ ans word
+	>body @ >body
+    else \ non-ans word
+	[ get-order wordsets swap 1+ set-order ] non-ANS [ previous ]
+    endif
+    ( nt wordset ) cell+ add-unless-present ;
+
+: find&note-name ( c-addr u -- nt/0 )
+    \ find-name replacement. Takes note of all the words used.
+    lookup @ (search-wordlist) dup
+    if
+	dup note-name
+    endif ;
+
+: replace-word ( xt cfa -- )
+    \ replace word at cfa with xt. !! This is quite general-purpose
+    \ and should migrate elsewhere.
+    dodefer: over code-address!
+    >body ! ;
+
+forth definitions
+ans-report-words
+
+: print-ans-report ( -- )
+    cr
+    ." The program uses the following words" cr
+    [ get-order wordsets swap 1+ set-order ] [(')] core [ previous ]
+    begin
+	dup 0<>
+    while
+	dup >r name>int >body dup @ swap cell+ 2@ dup
+	if
+	    ." from " r@ .name ." :" cr
+	    bounds
+	    u+do
+		i @ .name
+		cell
+	    +loop
+	    cr
+	else
+	    2drop
+	endif
+	rdrop
+    repeat
+    drop ;
+	
+\ the following sequence "' replace-word forth execute" is necessary
+\ to restore the default search order without effect on the "used
+\ word" lists
+' find&note-name ' find-name ' replace-word forth execute
---
+++ objects.fs
@@ -0,0 +1,261 @@
+\ yet another Forth objects extension
+
+\ written by Anton Ertl 1996
+\ public domain
+
+\ This is in ANS Forth (with an environmental dependence on case
+\ insensitivity; convert everything to upper case for state sensitive
+\ systems). It needs some non-core words (in particular, it uses the
+\ memory allocation wordset), but I have not made a complete list.
+
+\ Manual:
+
+\ A class is defined like this:
+
+\ <parent> class
+\   ... field <name>
+\   ...
+
+\   ... inst-var <name>
+\   ...
+
+\ selector <name>
+
+\ :noname ( ... object -- ... )
+\   ... ;
+\ method <name> \ new method
+\ ...
+
+\ :noname ( ... object -- ... )
+\   ... ;
+\ overrides <name> \ existing method
+\ ...
+
+\ end-class <name>
+
+\ you can write fields, inst-vars, selectors, methods and overrides in
+\ any order.
+
+\ A call of a method looks like this:
+
+\ ... <object> <method>
+
+\ (<object> just needs to reside on the stack, there's no need to name it).
+
+\ Instead of defining a method with ':noname ... ;', you can define it
+\ also with 'm: ... ;m'. The difference is that with ':noname' the
+\ "self" object is on the top of stack; with 'm:' you can get it with
+\ 'this'. You should use 'this' only in an 'm:' method even though the
+\ sample implementation does not enforce this.
+
+\ The difference between a field and and inst-var is that the field
+\ refers to an object at the top of data stack (i.e. a field has the
+\ stack effect (object -- addr), whereas the inst-var refers to this
+\ (i.e., it has the stack effect ( -- addr )); obviously, an inst-var
+\ can only be used in an 'm:' method.
+
+\ 'method' defines a new method selector and binds a method to it.
+
+\ 'selector' defines a new method selector without binding a method to
+\ it (you can use this to define abstract classes)
+
+\ 'overrides' binds a different method (than the parent class) to an
+\ existing method selector.
+
+\ If you want to perform early binding, you can do it like this:
+
+\ ... <object> [bind] <class> <method> \ compilation
+\ ... <object> bind  <class> <method> \ interpretation
+
+\ You can get at the method from the method selector and the class like
+\ this:
+
+\ bind' <class> <method>
+\ \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
+\ needs struct.fs
+
+\ helper words
+
+: -rot ( a b c -- c a b )
+    rot rot ;
+
+: perform ( ... addr -- ... )
+    @ execute ;
+
+: save-mem	( addr1 u -- addr2 u ) \ gforth
+    \ copy a memory block into a newly allocated region in the heap
+    swap >r
+    dup allocate throw
+    swap 2dup r> -rot move ;
+
+: extend-mem	( addr1 u1 u -- addr addr2 u2 )
+    \ extend memory block allocated from the heap by u aus
+    \ the (possibly reallocated piece is addr2 u2, the extension is at addr )
+    over >r + dup >r resize throw
+    r> over r> + -rot ;
+
+: 2,	( w1 w2 -- ) \ gforth
+    here 2 cells allot 2! ;
+
+: const-field ( size1 align1 -- size2 align2 )
+    1 cells: field
+does> ( addr -- w )
+    @ + @ ;
+
+struct
+    1 cells: field object-map
+end-struct object-struct
+
+struct
+    2 cells: field class-map
+    const-field class-instance-size \ aus
+    const-field class-instance-align \ aus
+end-struct class-struct
+
+2variable current-map \ address and size (in aus) of the current map
+
+: class ( class -- size align )
+    dup class-map 2@ save-mem current-map 2!
+    dup class-instance-size
+    swap class-instance-align ;
+
+: end-class ( size align "name" -- )
+    create
+    current-map 2@ 2,
+    swap , , ;
+
+: no-method ( -- )
+    abort" no method defined" ;
+
+: method ( xt "name" -- )
+    \ define method and selector
+    current-map 2@ ( xt map-addr map-size )
+    create dup ,
+    1 cells extend-mem current-map 2!
+    !
+does> ( ... object -- ... )
+    ( object addr )
+    @ over ( object-map ) @ + ( object xtp ) perform ;
+
+: selector ( "name" -- )
+    \ define a method selector for later overriding in subclasses
+    ['] no-method method ;
+
+: override! ( xt method-xt -- )
+    >body @ current-map 2@ drop + ! ;
+
+: overrides ( xt "selector" -- )
+    \ replace default method "method" with xt
+    ' override! ;
+
+: alloc-instance ( class -- object )
+    \ make a new, (almost) uninitialized instance of a class
+    dup class-instance-size allocate throw
+    swap class-map 2@ drop over ( object-map ) ! ;
+
+\ this/self, instance variables etc.
+
+variable thisp
+: this ( -- object )
+    \ rename this into self if you are a Smalltalk fiend
+    thisp @ ;
+
+: m: ( -- xt colon-sys ) ( run-time: object -- )
+    :noname 
+    POSTPONE this
+    POSTPONE >r
+    POSTPONE thisp
+    POSTPONE ! ;
+
+: ;m ( colon-sys -- ) ( run-time: -- )
+    POSTPONE r>
+    POSTPONE thisp
+    POSTPONE !
+    POSTPONE ; ; immediate
+
+: catch ( ... xt -- ... n )
+    \ make it safe to call CATCH within a method.
+    \ should also be done with all words containing CATCH.
+    this >r catch r> thisp ! ;
+
+: inst-var ( size1 align1 size align -- size2 align2 )
+    field
+does> ( -- addr )
+    ( addr1 ) @ this + ;
+
+\ early binding stuff
+
+\ this is not generally used, only where you want to do something like
+\ superclass method invocation (so that you don't have to name your methods)
+
+: (bind) ( class method-xt -- xt )
+    >body @ swap class-map 2@ drop + @ ;
+
+: bind' ( "class" "method" -- xt )
+    ' >body ' (bind) ;
+
+: bind ( ... object "class" "method" -- ... )
+    bind' execute ;
+
+: [bind] ( compile-time: "class" "method" -- ; run-time: ... object -- ... )
+    bind' compile, ; immediate
+
+\ the object class
+
+0 0 save-mem current-map 2!
+object-struct \ no class to inherit from, so we have to do this manually
+:noname ( object -- )
+    ." object:" dup . ." class:" object-map @ . ;
+method print
+end-class object
+
+\ examples
+true [if]
+cr object alloc-instance print
+
+object class
+:noname ( object -- )
+    drop ." undefined" ;
+overrides print
+end-class nothing
+
+nothing alloc-instance constant undefined
+
+cr undefined print
+
+\ instance variables and this
+object class
+1 cells: inst-var count-n
+m: ( object -- )
+    count-n @ . ;m
+overrides print
+m: ( object -- )
+   0 count-n ! ;m
+method init
+m: ( object -- )
+    1 count-n +! ;m
+method inc
+end-class counter
+
+counter alloc-instance constant counter1
+
+cr
+counter1 init
+counter1 print
+counter1 inc
+counter1 print
+counter1 inc
+counter1 inc
+counter1 inc
+counter1 print
+counter1 print
+
+\ examples of static binding
+
+cr undefined bind object print
+: object-print ( object -- )
+    [bind] object print ;
+
+cr undefined object-print
+[then]
