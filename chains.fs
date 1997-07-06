668a529c573f7d2d945eac12624467b8aea111d1
jwilke <jwilke>
1997-07-06 14:15:10 +0000

    Generic execution chains. Will be used for the init-chain! Jens

---
+++ chains.fs
@@ -0,0 +1,30 @@
+\ chains.fs execution chains for gforth			21jun97jaw
+
+0 [IF]
+This defines execution chains.
+The first application for this is building initialization chains:
+Think of many modules or program parts, each of it with some specific
+initialization code. If we hardcode the initialization routines into a
+"master-init" we will get unflexible and are not able only to load some
+specific modules...
+
+The chain is basicaly a linked-list. Define a Variable for the head of
+linked-list. Name it "foo8" or "foo-chain" to indicate it is a execution
+chain.
+
+You can add a word to the list with "' my-init foo8 chained". You can
+execute all the code with "foo8 chainperform".
+[THEN]
+
+[IFUNDEF] linked
+: linked        here over @ a, swap ! ;
+[THEN]
+
+\ generic chains
+
+: chained 	( xt list -- ) \ gforth
+  linked , ;
+
+: chainperform	( list -- ) \ gforth
+  BEGIN @ dup WHILE dup cell+ perform REPEAT drop ;
+
