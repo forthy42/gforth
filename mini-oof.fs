3f30c902f6066509850e03753e533d1c1eaac3e1
pazsan <pazsan>
1998-08-24 20:17:28 +0000

    Small fix, thanks to srb@concentric.net

--- mini-oof.fs
+++ mini-oof.fs
@@ -5,7 +5,7 @@
 : class ( class -- class methods vars ) dup 2@ ;
 : end-class  ( class methods vars -- )
   Create  here >r , dup , 2 cells ?DO ['] noop , 1 cells +LOOP
-  cell+ dup cell+ swap @ 2 - cells r> 2 cells + swap move ;
+  cell+ dup cell+ swap @ 2 cells - r> 2 cells + swap move ;
 : defines ( xt class "name" -- ) ' >body @ + ! ;
 : new ( class -- o )  here over @ allot swap over ! ;
 : :: ( class "name" -- ) ' >body @ + @ compile, ;
