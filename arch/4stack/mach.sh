8af328ed0887915b674f883b5bdf7f845011fed4
crook <crook>
2001-07-10 20:41:32 +0000

    kernl-4stack.fi now builds. Fixed paths in mach.sh, and changed reference therein from gforth to ./gforth to ensure that the engine is consistent with the source tree in which the .fi file is being built. Revised README to reflect the current file organisation.

--- arch/4stack/mach.sh
+++ arch/4stack/mach.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-gforth arch/4stack/relocate.fs \
- -e "s\" $1-\" read-gforth s\" ../4stack/gforth.4o\" write-gforth bye"
+./gforth arch/4stack/relocate.fs \
+ -e "s\" $1-\" read-gforth s\" arch/4stack/gforth.4o\" write-gforth bye"
 cp $1- $1
