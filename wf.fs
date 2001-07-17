61594340860b43674f225a07da5a279d17694f56
pazsan <pazsan>
2001-07-17 11:39:21 +0000

    Improvements of wiki for Forth Added small text data base

--- wf.fs
+++ wf.fs
@@ -8,6 +8,7 @@ require string.fs
 
 : tag ( addr u -- ) '< emit type '> emit ;
 : /tag ( addr u -- ) '< emit '/ emit type '> emit ;
+: tagged ( addr1 u1 addr2 u2 -- )  2dup 2>r tag type 2r> /tag ;
 
 \ environment handling
 
@@ -23,13 +24,17 @@ Variable envs 10 0 [DO] 0 , [LOOP]
 : +env  1 envs +! ;
 : -env -1 envs +! env? ;
 : -envs envs @ 0 ?DO  -env cr  LOOP ;
+: >env ( addr u -- ) +env env env? ;
 
 \ link creation
 
 Variable link
 Variable link-suffix
 
-: link-icon? ( -- )
+Variable do-icon
+Variable do-size
+
+: link-icon? ( -- )  do-icon @ 0= ?EXIT
     link $@ '. $split link-suffix $! 2drop s" .*" link-suffix $+!
     s" icons" open-dir throw >r
     BEGIN
@@ -40,12 +45,23 @@ Variable link-suffix
     UNTIL  ELSE  '( emit link-suffix $@ 2 - type ') emit  THEN
     r> close-dir throw ;
 
-: link-size? ( -- )
+: link-size? ( -- )  do-size @ 0= ?EXIT
     link $@ r/o open-file IF  drop  EXIT  THEN >r
     r@ file-size throw $400 um/mod nip ."  (" 0 u.r ." k)"
     r> close-file throw ;
 
-: .link ( -- )  '[ parse type '] parse '| $split link $!
+: link-options ( addr u -- addr' u' )
+    do-icon off  do-size off
+    BEGIN  dup 1 >=  WHILE
+	over c@ CASE
+	    '% OF  do-size on 1 /string  ENDOF
+	    '& OF  do-icon on 1 /string  ENDOF
+	    drop  EXIT
+	ENDCASE
+    REPEAT ;
+
+: .link ( -- )  '[ parse type '] parse '| $split
+    link-options link $!
     link $@len 0= IF  2dup link $! s" .html" link $+!  THEN
     link-icon? .' <a href="' link $@ type .' ">' type s" a" /tag
     link-size? ;
@@ -54,7 +70,7 @@ Variable link-suffix
 
 : parse-tag ( addr u char -- )
     >r r@ parse type
-    2dup tag r> parse type /tag ;
+    r> parse 2swap tagged ;
 
 : .bold ( -- )  s" b" '* parse-tag ;
 : .em   ( -- )  s" em" '_ parse-tag ;
@@ -87,7 +103,7 @@ Variable end-sec
 
 longtags set-current
 
-: --- s" hr" tag ;
+: --- cr s" hr" tag ;
 : * s" h1" line ;
 : ** s" h2" line ;
 : *** s" h3" line ;
@@ -115,10 +131,9 @@ definitions
 
 : .title ( addr u -- )
     .' <!doctype html public "-//w3c//dtd html 4.0 transitional//en">' cr
-    s" html" +env env env?
-    s" head" +env env env?
+    s" html" >env s" head" >env
     .'   <meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1">' cr
-    s" title" tag type s" title" /tag cr
+    s" title" tagged cr
     -env ;
 
 \ HTML trailer
@@ -127,12 +142,11 @@ Variable mail
 Variable mail-name
 
 : .trailer
-    s" address" tag
-    ." Changed last on " time&date rot 0 u.r swap 1-
+    s" address" >env s" center" >env
+    ." Last modified: " time&date rot 0 u.r swap 1-
     s" janfebmaraprmayjunjulaugsepoctnovdec" rot 3 * /string 3 min type
     0 u.r
     .'  by <a href="mailto:' mail $@ type .' ">' mail-name $@ type s" a" /tag
-    s" address" /tag
     -envs ;
 
 \ top word
@@ -150,3 +164,21 @@ Variable mail-name
     r> to outfile-id
     dup 0< IF  throw  ELSE  drop  THEN ;
 
+\ simple text data base
+
+: get-rest ( addr -- ) 0 parse -trailing rot $! ;
+
+Variable last-entry
+Variable field#
+
+: table: ( xt n -- )  Create , ,  1 field# !
+    DOES> 2@ >in @ >r longtags set-current
+    Create definitions swap , r> >in !
+    here last-entry !
+    dup 0 DO  0 ,  LOOP
+    1 DO  s" " last-entry @ I cells + $!  LOOP
+    last-entry @ get-rest
+    DOES> dup cell+ swap perform ;
+
+: field:  Create field# @ , 1 field# +!
+DOES> @ cells last-entry @ + get-rest ;
