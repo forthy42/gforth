b64eb3f883e329e904f3c6020a8ee71052d698ba
anton <anton>
2003-03-09 12:54:30 +0000

    added copyright messages to a lot of files removed some obsolete or non-source files

--- fixpath.fs
+++ fixpath.fs
@@ -1,5 +1,23 @@
 \ fix path in gforth*.exe
 
+\ Copyright (C) 2000 Free Software Foundation, Inc.
+
+\ This file is part of Gforth.
+
+\ Gforth is free software; you can redistribute it and/or
+\ modify it under the terms of the GNU General Public License
+\ as published by the Free Software Foundation; either version 2
+\ of the License, or (at your option) any later version.
+
+\ This program is distributed in the hope that it will be useful,
+\ but WITHOUT ANY WARRANTY; without even the implied warranty of
+\ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+\ GNU General Public License for more details.
+
+\ You should have received a copy of the GNU General Public License
+\ along with this program; if not, write to the Free Software
+\ Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111, USA.
+
 ." Fixing " 3 arg type ."  with " 2 arg type cr
 
 $7410 Constant gforth.exe
--- iss.sh
+++ iss.sh
@@ -1,4 +1,23 @@
 #!/bin/bash
+
+#Copyright (C) 2000 Free Software Foundation, Inc.
+
+#This file is part of Gforth.
+
+#Gforth is free software; you can redistribute it and/or
+#modify it under the terms of the GNU General Public License
+#as published by the Free Software Foundation; either version 2
+#of the License, or (at your option) any later version.
+
+#This program is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.#See the
+#GNU General Public License for more details.
+
+#You should have received a copy of the GNU General Public License
+#along with this program; if not, write to the Free Software
+#Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111, USA.
+
 # This is the horror shell script to create an automatic install for
 # Windoze.
 # Note that I use sed to create a setup file
@@ -15,7 +34,7 @@ cat <<EOT
 Bits=32
 AppName=Gforth
 AppVerName=gforth 0.5.0
-AppCopyright=Copyright © 1995-2000 by Free Software Foundation
+AppCopyright=Copyright © 1995,1996,1997,1998,2000 Free Software Foundation
 DefaultDirName=gforth
 DefaultGroupName=Gforth
 AllowNoIcons=1
