d5e1270a46253ce73f0893c3c7813dd0a2b3ff97
anton <anton>
2003-12-02 21:33:18 +0000

    gforth-native now survives make check

--- intcomp.fs
+++ intcomp.fs
@@ -50,22 +50,22 @@ constant no-interpretation-does-code
     cell+ cell+ maxaligned /does-handler + \ to does-code
     swap @ does-code! ;
 
-: (interpretation>) ( -- )
-    latestxt interpret/compile-int r@ fix-does-code ;
+: (interpretation>1) ( addr R:retaddr -- )
+    latestxt interpret/compile-int swap fix-does-code ;
 
 : interpretation> ( compilation. -- orig colon-sys ) \ gforth
-    POSTPONE (interpretation>) POSTPONE ahead
+    here 4 cells +  POSTPONE literal POSTPONE (interpretation>1) POSTPONE ahead
     dodoes, defstart dead-code off 0 set-locals-size-list ; immediate restrict
 
 : <interpretation ( compilation. orig colon-sys -- ) \ gforth
     ?struc POSTPONE exit
     POSTPONE then ; immediate restrict
 
-: (compilation>) ( -- )
-    latestxt interpret/compile-comp r@ fix-does-code ;
+: (compilation>1) ( addr R:retaddr -- )
+    latestxt interpret/compile-comp swap fix-does-code ;
 
 : compilation> ( compilation. -- orig colon-sys ) \ gforth
-    POSTPONE (compilation>) POSTPONE ahead
+    here 4 cells + POSTPONE literal POSTPONE (compilation>1) POSTPONE ahead
     dodoes, defstart dead-code off 0 set-locals-size-list POSTPONE >body ; immediate restrict
 
 comp' <interpretation drop
