106ca3dba7740e148bf5fb95b1354e19be760497
anton <anton>
2003-12-31 16:26:13 +0000

    updated copyright years, or added copyright notices.

--- arch/ia64/machine.h
+++ arch/ia64/machine.h
@@ -1,7 +1,7 @@
 /*
   This is the machine-specific part for Intel ia64 compatible processors
 
-  Copyright (C) 2000 Free Software Foundation, Inc.
+  Copyright (C) 2000,2003 Free Software Foundation, Inc.
 
   This file is part of Gforth.
 
