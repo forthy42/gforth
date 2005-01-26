552a8a6d8f3cf6b11c2991f0c0a36154bdb44b87
anton <anton>
2005-01-26 21:41:27 +0000

    deleted pre-xchar history stuff

--- history.fs
+++ history.fs
@@ -28,23 +28,6 @@ interpret/compile: ctrl  ( "<char>" -- ctrl-code )
 
 : >string  ( span addr pos1 -- span addr pos1 addr2 len )
   over 3 pick 2 pick chars /string ;
-: type-rest ( span addr pos1 -- span addr pos1 back )
-  >string tuck type ;
-: (del)  ( max span addr pos1 -- max span addr pos2 )
-  1- >string over 1+ -rot move
-  rot 1- -rot  #bs emit  type-rest bl emit 1+ backspaces ;
-: (ins)  ( max span addr pos1 char -- max span addr pos2 )
-  >r >string over 1+ swap move 2dup chars + r> swap c!
-  rot 1+ -rot type-rest 1- backspaces 1+ ;
-: ?del ( max span addr pos1 -- max span addr pos2 0 )
-  dup  IF  (del)  THEN  0 ;
-: (ret)  type-rest drop true space ;
-: back  dup  IF  1- #bs emit  ELSE  #bell emit  THEN 0 ;
-: forw 2 pick over <> IF  2dup + c@ emit 1+  ELSE  #bell emit  THEN 0 ;
-: <del> ( max span addr pos1 -- max span addr pos2 0 )
-  2 pick over <>
-	IF  forw drop (del)  ELSE  #bell emit  THEN  0 ;
-: eof  2 pick over or 0=  IF  bye  ELSE  <del>  THEN ;
 
 : bindkey ( xt key -- )  cells ctrlkeys + ! ;
 
@@ -89,8 +72,8 @@ s" os-class" environment? [IF] s" unix" str= [ELSE] true [THEN]
 : clear-line ( max span addr pos1 -- max addr )
   backspaces over spaces swap backspaces ;
 
-: clear-tib ( max span addr pos -- max 0 addr 0 false )
-  clear-line 0 tuck dup ;
+\ : clear-tib ( max span addr pos -- max 0 addr 0 false )
+\   clear-line 0 tuck dup ;
 
 : hist-pos    ( -- ud )  history file-position drop ( throw ) ;
 : hist-setpos ( ud -- )  history reposition-file drop ( throw ) ;
@@ -117,20 +100,13 @@ s" os-class" environment? [IF] s" unix" str= [ELSE] true [THEN]
 : prev-line  ( max span addr pos1 -- max span addr pos2 false )
     clear-line find-prev-line 2dup type 0 ;
 
-Create lfpad #lf c,
+\ Create lfpad #lf c,
 
 : (enter)  ( max span addr pos1 -- max span addr pos2 true )
   >r end^ 2@ hist-setpos
   2dup swap history write-line drop ( throw ) \ don't worry about errors
   hist-pos 2dup backward^ 2! end^ 2!
   r> (ret) ;
-
-\ some other key commands                              16oct94py
-
-: first-pos  ( max span addr pos1 -- max span addr 0 0 )
-  backspaces 0 0 ;
-: end-pos  ( max span addr pos1 -- max span addr span 0 )
-  type-rest 2drop over 0 ;
 
 : extract-word ( addr len -- addr' len' )  dup >r
   BEGIN  1- dup 0>=  WHILE  2dup + c@ bl =  UNTIL  THEN  1+
@@ -203,43 +179,11 @@ Create prefix-found  0 , 0 ,
 	[ -1 cells ] Literal +LOOP
     prefix-string ;
 
-: kill-expand ( max span addr pos1 -- max span addr pos2 )
-    prefix-found cell+ @  0 ?DO  (del)  LOOP ;
-
 : tib-full? ( max span addr pos addr' len' -- max span addr pos addr1 u flag )
     5 pick over 4 pick + prefix-found @ 0<> - < ;
 
-: tab-expand ( max span addr pos1 -- max span addr pos2 0 )
-    key? IF  #tab (ins) 0  EXIT  THEN
-    kill-expand  2dup extract-word dup 0= IF  nip EXIT  THEN
-    search-prefix  tib-full?
-    IF    7 emit  2drop  0 0 prefix-found 2!
-    ELSE  bounds ?DO  I c@ (ins)  LOOP  THEN
-    prefix-found @ IF  bl (ins)  THEN  0 ;
-
 : kill-prefix  ( key -- key )
   dup #tab <> IF  prefix-off  THEN ;
-
-[IFUNDEF] everyline defer everyline [THEN]
-
-: char-history ( -- )
-    \ might be useful on dumb terminals, otherwise unused
-    ['] forw        ctrl F bindkey
-    ['] back        ctrl B bindkey
-    ['] ?del        ctrl H bindkey
-    ['] eof         ctrl D bindkey
-    ['] <del>       ctrl X bindkey
-    ['] next-line   ctrl N bindkey
-    ['] prev-line   ctrl P bindkey
-    ['] clear-tib   ctrl K bindkey
-    ['] first-pos   ctrl A bindkey
-    ['] end-pos     ctrl E bindkey
-    ['] (enter)     #lf    bindkey
-    ['] (enter)     #cr    bindkey
-    ['] tab-expand  #tab   bindkey
-    ['] (ins)       IS insert-char
-    ['] kill-prefix IS everychar
-    ['] noop        IS everyline ;
 
 \ UTF-8 support
 
