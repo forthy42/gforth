5810f43b4152e3801c3aea58817e760a368a9323
pazsan <pazsan>
2006-03-25 20:43:34 +0000

    Stripped down errors file for Gforth EC

---
+++ arch/r8c/errors.fs
@@ -0,0 +1,13 @@
+\ Error strings
+
+>rom
+s" Aborted" -&1 ErrLink linkstring
+s" Undefined word" -&13 ErrLink linkstring
+s" Stack underflow" -&4 ErrLink linkstring
+s" Interpreting a compile-only word" -&14 ErrLink linkstring
+s" Control structure mismatch" -&22 ErrLink linkstring
+\ s" Pictured numeric ouput string overflow" -&17 ErrLink linkstring
+s" Attempt to use zero-length string as a name" -&16 ErrLink linkstring
+>ram
+
+
