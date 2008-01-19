dd0d390ef9c14433e74ec28f68842a4d75f0e678
dvdkhlng <dvdkhlng>
2008-01-19 00:31:03 +0000

    fixed cacheflush-linux.c to actually compile

--- arch/arm/cacheflush-linux.c
+++ arch/arm/cacheflush-linux.c
@@ -1,5 +1,5 @@
 /*
-  ARM icache flush support using Linux syscall
+  ARM icache flush support using Linux syscall.  
 
   Copyright (C) 2000,2008 Free Software Foundation, Inc.
 
@@ -18,14 +18,29 @@
   You should have received a copy of the GNU General Public License
   along with this program; if not, write to the Free Software
   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111, USA.
+
+  If this compiles without error, cacheflush() should be guaranteed to
+  work.  The nice thing about using linux/unistd.h for the syscall
+  implementation is, that it uses the correct syscall ABI for the
+  system.  Yes, on ARM there are at least two different ABIs out
+  there, which can be selected at kernel compile time.
 */
 
-#include <asm/unistd.h>
+/* syscall macros not exported, unless __KERNEL__ set :( */
+#define __KERNEL__
+#include <linux/unistd.h>
 
-_syscall2(void, arm_cacheflush_syscall, __ARM_NR_cacheflush, 
-	  long, start, long, end, unsigned long flags);
+#include <stddef.h>
+#include <errno.h>
+
+/* need __NR_ suffixed syscall number for syscall macros to work
+   correctly */
+#define __NR_ARM_cacheflush __ARM_NR_cacheflush	
+
+static _syscall3(void, ARM_cacheflush, 
+	  long, start, long, end, unsigned long, flags);
 
 void cacheflush(void *p, size_t size) {
-   arm_cacheflush_syscall ((long)p, (long)p + size, 0);
+  ARM_cacheflush ((long)p, (long)p + size, 0);
 }
 
