0d74f877412bb91a24b87b1950e155e7737a0198
dvdkhlng <dvdkhlng>
2008-01-26 15:00:52 +0000

    Thumb-mode fixes for ARM cacheflush syscall (untested)

--- arch/arm/cacheflush-linux.c
+++ arch/arm/cacheflush-linux.c
@@ -18,12 +18,6 @@
   You should have received a copy of the GNU General Public License
   along with this program; if not, write to the Free Software
   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111, USA.
-
-  If this compiles without error, cacheflush() should be guaranteed to
-  work.  The nice thing about using linux/unistd.h for the syscall
-  implementation is, that it uses the correct syscall ABI for the
-  system.  Yes, on ARM there are at least two different ABIs out
-  there, which can be selected at kernel compile time.
 */
 #include <stddef.h>
 
@@ -37,22 +31,28 @@ void cacheflush(void *p, size_t size)
     * kernels got first support with version 2.2.18.
     */
 
-   asm("mov r0, %0\n\t"
-       "mov r1, %1\n\t"
-       "mov r2, #0\n\t"
-#    if defined (__ARM_EABI__) || defined (__thumb__)
-       /* EABI or Thumb syscall: syscall number passed in 'r7' (syscall base
-	* number is 0x0).  Note that Thumb and EABI are generally not the
-	* same.  It just happens that the simple cacheflush syscall doesn't
-	* expose any of differences in calling conventions.
+   asm("mov r0, %0\n"
+       "mov r1, %1\n"
+       "mov r2, #0\n"
+#    if defined (__THUMB__)
+       /* Thumb syscall, either EABI or OABI, syscall number is passed in
+	* 'r7'.  Note that Thumb EABI and OABI are generally not the same.  It
+	* just happens that the simple cacheflush syscall doesn't expose any
+	* of differences in calling conventions. */
+       "mov r7, #0xf\n\t"	/* Thumb mode only allows 8-bit constants */
+       "lsl r7, #16\n\t"
+       "add r7, #0x2\n\t"
+#    elif defined (__ARM_EABI__)
+       /* EABI non-Thumb syscall: syscall number passed in 'r7' (syscall base
+	* number is 0x0).
         */
-       "mov r7, #0xf0000\n\t"
-       "orr r7, r7, #0x00002\n\t"
+       "mov r7, #0xf0000\n\t"	/* only shifted 8-bit constants allowed */
+       "orr r7, r7, #0x00002\n\t"	
        "swi #0\n\t"      
 #    else
-       /* OABI syscall: syscall number passed as part of 'swi' instruction,
-	* base number is 0x900000 */
-       "swi #0x9f0002\n\t" 
+       /* OABI non-Thumb syscall: syscall number passed as part of 'swi'
+	* instruction, base number is 0x900000 */
+       "swi #0x9f0002\n" ::
 #     endif
        :: "r"(p), "r"((long)p+size) :
        "r0", "r1", "r2", "r7");
