9f8a32efb2cac802fac8eb681b1853b4466e8d60
pazsan <pazsan>
2008-05-17 20:53:36 +0000

    First part of itools adaption in socket.fs

--- unix/socket.fs
+++ unix/socket.fs
@@ -19,16 +19,26 @@
 
 \c #include <netdb.h>
 c-function gethostbyname gethostbyname a -- a ( name -- hostent )
+\c #include <unistd.h>
+c-function gethostname gethostname a n -- n ( c-addr u -- ior )
+\c #include <errno.h>
+c-function 'errno __errno_location -- a ( -- addr )
 \c #include <sys/types.h>
 \c #include <sys/socket.h>
 c-function socket socket n n n -- n ( class type proto -- fd )
+c-function closesocket close n -- n ( fd -- ior )
 c-function connect connect n a n -- n ( fd sock size -- err )
+c-function send send n a n n -- n ( socket buffer count flags -- size )
+c-function recv recv n a n n -- n ( socket buffer count flags -- size )
 \c #include <stdio.h>
 c-function fdopen fdopen n a -- a ( fd fileattr -- file )
+\c #include <fcntl.h>
+c-function fcntl fcntl n n n -- n ( fd n1 n2 -- ior )
 \c #include <arpa/inet.h>
 c-function htonl htonl n -- n ( x -- x' )
 c-function htons htons n -- n ( x -- x' )
 c-function ntohl ntohl n -- n ( x -- x' )
+c-function fileno fileno a -- n ( file* -- fd )
 
 4 4 2Constant int%
 2 2 2Constant short%
@@ -62,9 +72,14 @@ sockaddr-tmp sockaddr_in %size dup allot erase
     c-string gethostbyname dup 0= abort" address not found"
     h_addr_list @ @ @ ntohl ;
 
-2 Constant PF_INET
-1 Constant SOCK_STREAM
-6 Constant IPPROTO_TCP
+   2 Constant PF_INET
+   1 Constant SOCK_STREAM
+   6 Constant IPPROTO_TCP
+   4 Constant F_SETFL
+  11 Constant EWOULDBLOCK
+$100 Constant MSG_WAITALL
+$802 Constant O_NONBLOCK|O_RDWR
+2000 Value    SOCKET-TIMEOUT
 
 : new-socket ( -- socket )
     PF_INET SOCK_STREAM IPPROTO_TCP socket
@@ -82,3 +97,57 @@ sockaddr-tmp sockaddr_in %size dup allot erase
     new-socket >r
     r@ sockaddr-tmp sockaddr_in %size connect 0< abort" can't connect"
     r> s" w+" c-string fdopen ;
+
+\ from itools.frt
+
+: ms@  utime 1000 um/mod nip ; ( -- u ) 
+
+: $put ( c-addr1 u1 c-addr2 -- ) swap cmove ;
+
+: $+ 	( c-addr1 u1 c-addr2 u2 -- c-addr3 u3 )
+    { c-addr1 u1 c-addr2 u2 }
+    u1 u2 + allocate throw 
+    c-addr1 u1  2 pick       $put 
+    c-addr2 u2  2 pick u1 +  $put  
+    u1 u2 + ;
+
+Create hostname$ 256 chars allot
+
+Create crlf 2 c, 13 c, 10 c,
+
+: +cr  ( c-addr1 u1 -- c-addr2 u2 ) crlf count $+ ;
+
+: blocking-mode ( socket flag -- ) >r fileno
+    f_setfl r> IF  0  
+    ELSE  o_nonblock|o_rdwr  
+    THEN  
+    fcntl 0< abort" blocking-mode failed" ;
+
+: errno ( -- #error ) 'errno @ ;
+: hostname ( -- c-addr u ) hostname$ count ;
+: open-service ( c-addr u port# -- socket ) open-socket ;
+: set-socket-timeout ( u -- ) 200 + to socket-timeout ;
+: get-socket-timeout ( -- u ) socket-timeout 200 - ;
+: write-socket ( c-addr size socket -- ) fileno -rot 0 send 0< throw ;
+: close-socket ( socket -- ) fileno closesocket drop ;
+
+: (rs)  ( socket c-addr maxlen -- c-addr size ) 
+    2 pick >r r@ false blocking-mode  rot fileno -rot
+    over >r msg_waitall recv
+    dup 0<  IF  0 max
+	errno dup 0<> swap ewouldblock <> and abort" (rs) :: socket read error"
+    THEN
+    r> swap
+    r> true blocking-mode ;
+
+: read-socket ( socket c-addr maxlen -- c-addr u )
+    ms@ socket-timeout + { socket c-addr maxlen tmax -- c-addr size }
+    BEGIN 
+	socket c-addr maxlen (rs) dup 0=
+	ms@ tmax u< and 
+    WHILE 
+	    2drop
+    REPEAT ;
+
+hostname$ 1+ 255 gethostname drop
+hostname$ 1+ 255 0 scan nip 255 swap - hostname$ c!
