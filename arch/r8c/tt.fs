1c5cba67b883d0945e2ee2c5da5b0cb751ae5f53
pazsan <pazsan>
2009-02-15 22:15:37 +0000

    Removed copyright comments, because they don't load on r8c

--- arch/r8c/tt.fs
+++ arch/r8c/tt.fs
@@ -1,16 +1,5 @@
 \ tt.pfe
-\ Tetris for terminals, redone in ANSI-Forth.
-\ Written 05Apr94 by Dirk Uwe Zoller,
-\  e-mail duz@roxi.rz.fht-mannheim.de.
-\ Look&feel stolen from Mike Taylor's "TETRIS FOR TERMINALS"
-\
-\ Please copy and share this program, modify it for your system
-\ and improve it as you like. But don't remove this notice.
-\
-\ Thank you.
-\
-\ Variables, constants
-
+\ see ../../tt.fs
 rom
 
 bl bl 2constant empt
