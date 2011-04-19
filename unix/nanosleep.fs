842651486d7dad759e3a59e01610656783ca4e5f
pazsan <pazsan>
2011-04-19 10:35:34 +0000

    Added more functions to nanosleep

--- unix/nanosleep.fs
+++ unix/nanosleep.fs
@@ -1,6 +1,6 @@
 \ nanosleep
 
-\ Copyright (C) 1998,2000,2003,2005,2006,2007,2008,2009,2010 Free Software Foundation, Inc.
+\ Copyright (C) 2011 Free Software Foundation, Inc.
 
 \ This file is part of Gforth.
 
@@ -18,12 +18,35 @@
 \ along with this program. If not, see http://www.gnu.org/licenses/.
 
 c-library nanosleep
-\c #include <time.h>
-\c gforth_nanosleep(long deltat) {
-\c struct timespec req;
-\c req.tv_sec = deltat/1000000000;
-\c req.tv_nsec = deltat%1000000000;
-\c nanosleep(&req, &req);
-\c }
-c-function nanosleep gforth_nanosleep n -- void ( nsec -- )
+    s" rt" add-lib
+    \c #include <time.h>
+    \c unsigned long gforth_nanosleep(unsigned long deltat) {
+    \c struct timespec req;
+    \c req.tv_sec = deltat/1000000000;
+    \c req.tv_nsec = deltat%1000000000;
+    \c nanosleep(&req, &req);
+    \c return req.tv_sec*1000000000+req.tv_nsec;
+    \c }
+    \c unsigned long gforth_clock_nanosleep(unsigned long abst) {
+    \c struct timespec req;
+    \c req.tv_sec = abst/1000000000;
+    \c req.tv_nsec = abst%1000000000;
+    \c clock_nanosleep(CLOCK_MONOTONIC, TIMER_ABSTIME, &req, &req);
+    \c return req.tv_sec*1000000000+req.tv_nsec;
+    \c }
+    \c unsigned long gforth_clock_gettime() {
+    \c struct timespec req;
+    \c clock_gettime(CLOCK_MONOTONIC, &req);
+    \c return req.tv_sec*1000000000+req.tv_nsec;
+    \c }
+    \c unsigned long gforth_clock_getres() {
+    \c struct timespec req;
+    \c clock_getres(CLOCK_MONOTONIC, &req);
+    \c return req.tv_sec*1000000000+req.tv_nsec;
+    \c }
+    
+    c-function nanosleep gforth_nanosleep n -- n ( nsec -- left )
+    c-function clock_nanosleep gforth_clock_nanosleep n -- n ( nsec -- left )
+    c-function clock_gettime gforth_clock_gettime -- n ( -- nsec )
+    c-function clock_getres gforth_clock_getres -- n ( -- nsec )
 end-c-library
