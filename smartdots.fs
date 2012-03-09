513bbcaaaffb323a4ab9608972fd62d81031662a
pazsan <pazsan>
2012-03-09 12:59:54 +0000

    Use .s.

--- smartdots.fs
+++ smartdots.fs
@@ -1,27 +1,43 @@
-\ smart .s
+\ smart .s                                             09mar2012py
+
+\ Copyright (C) 1995,1998,1999,2001,2003,2006,2007,2011 Free Software Foundation, Inc.
+
+\ This file is part of Gforth.
+
+\ Gforth is free software; you can redistribute it and/or
+\ modify it under the terms of the GNU General Public License
+\ as published by the Free Software Foundation, either version 3
+\ of the License, or (at your option) any later version.
+
+\ This program is distributed in the hope that it will be useful,
+\ but WITHOUT ANY WARRANTY; without even the implied warranty of
+\ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+\ GNU General Public License for more details.
+
+\ You should have received a copy of the GNU General Public License
+\ along with this program. If not, see http://www.gnu.org/licenses/.
+
+\ idea: Gerald Wodni
 
 : addr? ( addr -- flag )
-    TRY  @  IFERROR  2drop  false  ELSE  drop  true  THEN   ENDTRY ;
+    TRY  @  IFERROR  2drop  false nothrow  ELSE  drop  true  THEN   ENDTRY ;
 
 : string? ( addr u -- flag )
     TRY  bounds ?DO  I c@ bl < IF  -1 throw  THEN  LOOP
-	IFERROR  2drop drop false  ELSE  true  THEN  ENDTRY ;
+	IFERROR  2drop drop false nothrow ELSE  true  THEN  ENDTRY ;
 
 : .string. ( addr u -- )
     '"' emit type '"' emit space ;
 : .addr. ( addr -- )  hex. ;
 
-: .s ( -- ) \ tools dot-s
-\G Display the number of items on the data stack, followed by a list
-\G of the items (but not more than specified by @code{maxdepth-.s};
-\G TOS is the right-most item.
-    ." <" depth 0 .r ." > "
-    depth 0 max maxdepth-.s @ min
-    dup 0
-    ?do
-	dup i - pick  over i - pick  2dup string? IF  .string. 2
-	ELSE  drop dup addr? IF  .addr. 1
-	    ELSE  .s. 1  THEN
-	THEN
-    +loop
-    drop ;
+Variable smart.s-skip
+
+: smart.s. ( n -- )
+    smart.s-skip @  smart.s-skip off IF  drop  EXIT  THEN
+    over r> i swap >r - pick  2dup string? IF
+	.string. smart.s-skip on
+    ELSE  drop dup addr? IF  .addr.
+	ELSE  .  THEN
+    THEN ;
+
+' smart.s. IS .s.
