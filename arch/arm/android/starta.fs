f8bd97ef866f07df3a1cc08376f92e40ae9389d6
pazsan <pazsan>
2012-08-17 02:08:57 +0000

    Android startup with own terminal

--- arch/arm/android/starta.fs
+++ arch/arm/android/starta.fs
@@ -1,4 +1,10 @@
 \ startup stuff
-require unix/terminal-server.fs get-connection
+require unix/terminal-server.fs \ get-connection
+require unix/pthread.fs
 require android.fs
+require gl-terminal.fs
+\ get-connection
 \ require gl-sample.fs
+2Variable dummyinput  dummyinput create_pipe
+dummyinput @ to infile-id
+>screen
