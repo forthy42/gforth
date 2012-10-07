d2f0f8753564456645487610045e9357d817b609
pazsan <pazsan>
2012-10-07 21:09:27 +0000

    More little Windows stuff

--- arch/arm/android/build.sh
+++ arch/arm/android/build.sh
@@ -10,15 +10,7 @@ else
    EXT=$ENGINE
 fi
 
-<<<<<<< HEAD
-<<<<<<< build.sh
-sed -e 's/android:value="gforth-[a-z]*"/android:value="gforth'$ENGINE'"/g' <AndroidManifest.xml >AndroidManifest.xml+
-=======
 sed -e 's/android:value="gforth[a-z-]*"/android:value="gforth'$ENGINE'"/g' <AndroidManifest.xml >AndroidManifest.xml+
->>>>>>> 1.12
-=======
-sed -e 's/android:value="gforth[a-z-]*"/android:value="gforth'$ENGINE'"/g' <AndroidManifest.xml >AndroidManifest.xml+
->>>>>>> refs/heads/origin
 mv AndroidManifest.xml+ AndroidManifest.xml
 
 SRC=../../..
