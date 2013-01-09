3db00e6fdb04936d6c0c48fbc095e8b30e1ba4f0
Bernd Paysan <bernd.paysan@gmx.de>
2013-01-09 23:46:09 +0100

    New mini-OOF with current object added

---
+++ mini-oof2.fs
@@ -0,0 +1,22 @@
+\ Mini-OOF2, using current object+Gforth primitives    09jan12py
+
+: o ( -- addr )  o#+ [ 0 , ] ;
+: vt ( -- addr ) o#+ [ -1 cells , ] @ ;
+: method-compile  compile>  >body @ ['] o#exec compile, , ;
+: method ( m v "name" -- m' v ) Create  over , swap cell+ swap
+    method-compile
+  DOES> ( ... -- ... ) @ vt + perform ;
+: var-compile   compile>  >body @ ['] o#+ compile, , ;
+: var ( m v size "name" -- m v' ) Create  over , +
+    var-compile
+  DOES> ( -- addr ) @ o + ;
+: class ( class -- class methods vars ) dup 2@ ;
+: end-class  ( class methods vars "name" -- )
+  Create  here >r , dup , 2 cells ?DO ['] noop , 1 cells +LOOP
+  cell+ dup cell+ r> rot @ 2 cells /string move ;
+: >vt ( class "name" -- addr )  ' >body @ + ;
+: bind ( class "name" -- xt )    >vt @ ;
+: defines ( xt class "name" -- ) >vt ! ;
+: new ( class -- o )  here over @ allot swap over ! cell+ ;
+: :: ( class "name" -- ) bind compile, ;
+Create object  1 cells , 2 cells ,
