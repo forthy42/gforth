7f2e90dee71d64720347ed37c37bfa908a54d94c
Bernd Paysan <bernd.paysan@gmx.de>
2013-12-23 19:50:56 +0100

    Simpler and thread-safe store-backtrace

--- unix/filestat.fs
+++ unix/filestat.fs
@@ -39,8 +39,14 @@ c-library filestat
     c-function stat stat a a -- n ( path buf -- r )
     c-function fstat fstat n a -- n ( fd buf -- r )
     c-function lstat lstat a a -- n ( path buf -- r )
-    c-function utimensat utimensat n a a n -- n ( fd path times flags -- r )
-    c-function futimens futimens n a -- n ( fd times -- r )
+    e? os-type s" darwin" string-prefix? [IF]
+	c-function utimes utimes a a -- n ( fd times -- r )
+	c-function lutimes lutimes a a -- n ( fd times -- r )
+	c-function futimes futimes n a -- n ( fd times -- r )
+    [ELSE] \ linux stuff
+	c-function utimensat utimensat n a a n -- n ( fd path times flags -- r )
+	c-function futimens futimens n a -- n ( fd times -- r )
+    [THEN]
     c-function chmod chmod a n -- n ( path mode -- r )
     c-function fchmod fchmod n n -- n ( fd mode -- r )
     c-function chown chown a n n -- n ( path uid git -- r )
@@ -48,38 +54,76 @@ c-library filestat
     c-function lchown lchown a n n -- n ( path uid git -- r )
 end-c-library
 
-: utimens ( a a -- r )  -100 -rot 0 utimensat ;
-: lutimens ( a a -- r )  -100 -rot $100 utimensat ;
+e? os-type s" darwin" string-prefix? [IF]
+    : futimens ( a a -- r )  dup cell+ @ 1000 / over cell+ ! futimes ;
+    : utimens ( a a -- r )  dup cell+ @ 1000 / over cell+ ! utimes ;
+    : lutimens ( a a -- r )  dup cell+ @ 1000 / over cell+ ! lutimes ;
+[ELSE]
+    : utimens ( a a -- r )  -100 -rot 0 utimensat ;
+    : lutimens ( a a -- r )  -100 -rot $100 utimensat ;
+[THEN]
 
 begin-structure file-stat
-cell 8 = [IF]
-    drop 0 8 +field st_dev
-    drop 8 field: st_ino
-    drop 24 4 +field st_mode
-    drop 28 4 +field st_uid
-    drop 32 4 +field st_gid
-    drop 40 8 +field st_rdev
-    drop 48 field: st_size
-    drop 56 field: st_blksize
-    drop 64 field: st_blocks
-    drop 72 16 +field st_atime
-    drop 88 16 +field st_mtime
-    drop 104 16 +field st_ctime
-    drop 144
+e? os-type s" darwin" string-prefix? [IF]
+    cell 8 = [IF]
+	drop 0 4 +field st_dev
+	drop 8 8 +field st_ino
+	drop 6 2 +field st_mode
+	drop 16 4 +field st_uid
+	drop 20 4 +field st_gid
+	drop 24 4 +field st_rdev
+	drop 96 8 +field st_size
+	drop 112 4 +field st_blksize
+	drop 104 8 +field st_blocks
+	drop 32 16 +field st_atime
+	drop 48 16 +field st_mtime
+	drop 64 16 +field st_ctime
+	drop 144
+    [ELSE]
+	drop 0 4 +field st_dev
+	drop 8 8 +field st_ino
+	drop 6 2 +field st_mode
+	drop 16 4 +field st_uid
+	drop 20 4 +field st_gid
+	drop 24 4 +field st_rdev
+	drop 60 8 +field st_size
+	drop 76 4 +field st_blksize
+	drop 68 8 +field st_blocks
+	drop 28 8 +field st_atime
+	drop 36 8 +field st_mtime
+	drop 44 8 +field st_ctime
+	drop 108
+    [THEN]
 [ELSE]
-    drop 0 8 +field st_dev
-    drop 12 field: st_ino
-    drop 16 4 +field st_mode
-    drop 24 4 +field st_uid
-    drop 28 4 +field st_gid
-    drop 32 8 +field st_rdev
-    drop 44 field: st_size
-    drop 48 field: st_blksize
-    drop 52 field: st_blocks
-    drop 56 8 +field st_atime
-    drop 64 8 +field st_mtime
-    drop 72 8 +field st_ctime
-    drop 88
+    cell 8 = [IF]
+	drop 0 8 +field st_dev
+	drop 8 field: st_ino
+	drop 24 4 +field st_mode
+	drop 28 4 +field st_uid
+	drop 32 4 +field st_gid
+	drop 40 8 +field st_rdev
+	drop 48 field: st_size
+	drop 56 field: st_blksize
+	drop 64 field: st_blocks
+	drop 72 16 +field st_atime
+	drop 88 16 +field st_mtime
+	drop 104 16 +field st_ctime
+	drop 144
+    [ELSE]
+	drop 0 8 +field st_dev
+	drop 12 field: st_ino
+	drop 16 4 +field st_mode
+	drop 24 4 +field st_uid
+	drop 28 4 +field st_gid
+	drop 32 8 +field st_rdev
+	drop 44 field: st_size
+	drop 48 field: st_blksize
+	drop 52 field: st_blocks
+	drop 56 8 +field st_atime
+	drop 64 8 +field st_mtime
+	drop 72 8 +field st_ctime
+	drop 88
+    [THEN]
 [THEN]
 end-structure
 
