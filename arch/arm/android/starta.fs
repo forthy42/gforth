fe31f5a972babed6e04ae578ad095d2d9d72a5a5
Bernd Paysan <bernd.paysan@gmx.de>
2014-12-03 21:59:05 +0100

    Better starta.fs

--- arch/arm/android/starta.fs
+++ arch/arm/android/starta.fs
@@ -1,10 +1,15 @@
 \ startup stuff
+: ++fpath ( addr u -- )
+    open-fpath-file IF fpath also-path close-file throw
+    ELSE 2drop drop THEN ;
+s" unix" ++fpath
+s" minos2" ++fpath
 ." load terminal-server" cr stdout flush-file throw
-require unix/terminal-server.fs
+require terminal-server.fs
 ." load android" cr stdout flush-file throw
-require unix/android.fs
+require android.fs
 ." load gl-terminal" cr stdout flush-file throw
-require minos2/gl-terminal.fs
+require gl-terminal.fs
 ." done loading" cr stdout flush-file throw
 >screen
 : t get-connection ;
