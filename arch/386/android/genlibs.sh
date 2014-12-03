cf474f836ba3d2123b4e6cbc890e5d6f435672ed
Bernd Paysan <bernd.paysan@gmx.de>
2014-12-03 01:07:09 +0100

    Copy libtypeset only .a and .so

--- arch/386/android/genlibs.sh
+++ arch/386/android/genlibs.sh
@@ -88,4 +88,4 @@ cp SOIL2.h $TOOLCHAIN/sysroot/usr/include))
 
 $TARGET-libtool  --tag=CC   --mode=link $TARGET-gcc  -O2   -o libtypeset.la -rpath /home/bernd/proj/android-toolchain/sysroot/usr/lib $(find $FREETYPE $HARFBUZZ freetype-gl -name '*.lo') -lm -lGLESv2 -lz -llog
 
-cp .libs/libtypeset.* $TOOLCHAIN/sysroot/usr/lib
+cp .libs/libtypeset.{a,so} $TOOLCHAIN/sysroot/usr/lib
