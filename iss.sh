7e46eb0a446cb0a45744ff493fb3bee0b3de1b47
Bernd Paysan <bernd.paysan@gmx.de>
2015-04-05 04:55:31 +0200

    Add firewall rules for Gforth to allow networking stuff

--- iss.sh
+++ iss.sh
@@ -165,6 +165,67 @@ begin
   if Result = True then
      Result := Pos(';' + UpperCase(Param) + '\;', ';' + UpperCase(OrigPath) + ';') = 0; 
 end;
+
+// Utility functions for Inno Setup
+//   used to add/remove programs from the windows firewall rules
+// Code originally from http://news.jrsoftware.org/news/innosetup/msg43799.html
+
+const
+  NET_FW_SCOPE_ALL = 0;
+  NET_FW_IP_VERSION_ANY = 2;
+
+procedure SetFirewallException(AppName,FileName:string);
+var
+  FirewallObject: Variant;
+  FirewallManager: Variant;
+  FirewallProfile: Variant;
+begin
+  try
+    FirewallObject := CreateOleObject('HNetCfg.FwAuthorizedApplication');
+    FirewallObject.ProcessImageFileName := FileName;
+    FirewallObject.Name := AppName;
+    FirewallObject.Scope := NET_FW_SCOPE_ALL;
+    FirewallObject.IpVersion := NET_FW_IP_VERSION_ANY;
+    FirewallObject.Enabled := True;
+    FirewallManager := CreateOleObject('HNetCfg.FwMgr');
+    FirewallProfile := FirewallManager.LocalPolicy.CurrentProfile;
+    FirewallProfile.AuthorizedApplications.Add(FirewallObject);
+  except
+  end;
+end;
+
+procedure RemoveFirewallException( FileName:string );
+var
+  FirewallManager: Variant;
+  FirewallProfile: Variant;
+begin
+  try
+    FirewallManager := CreateOleObject('HNetCfg.FwMgr');
+    FirewallProfile := FirewallManager.LocalPolicy.CurrentProfile;
+    FireWallProfile.AuthorizedApplications.Remove(FileName);
+  except
+  end;
+end;
+
+// event called at install
+procedure CurStepChanged(CurStep: TSetupStep);
+begin
+  if CurStep=ssPostInstall then
+     SetFirewallException('Gforth', ExpandConstant('{app}')+'\gforth.exe');
+     SetFirewallException('Gforth', ExpandConstant('{app}')+'\gforth-fast.exe');
+     SetFirewallException('Gforth', ExpandConstant('{app}')+'\gforth-itc.exe');
+     SetFirewallException('Gforth', ExpandConstant('{app}')+'\gforth-ditc.exe');
+end;
+
+// event called at uninstall
+procedure CurUninstallStepChanged(CurUninstallStep: TUninstallStep);
+begin
+  if CurUninstallStep=usPostUninstall then
+     RemoveFirewallException(ExpandConstant('{app}')+'\gforth.exe');
+     RemoveFirewallException(ExpandConstant('{app}')+'\gforth-fast.exe');
+     RemoveFirewallException(ExpandConstant('{app}')+'\gforth-itc.exe');
+     RemoveFirewallException(ExpandConstant('{app}')+'\gforth-ditc.exe');
+end;
 EOT
 
 sed -e 's/$/\r/' <README >README.txt
