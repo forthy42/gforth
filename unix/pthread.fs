1476d8a79b699ce3b1e0135a3ff5832f0e87b552
Bernd Paysan <bernd.paysan@gmx.de>
2015-04-29 01:24:48 +0200

    pthread_cancel not available on Android

--- unix/pthread.fs
+++ unix/pthread.fs
@@ -159,7 +159,9 @@ c-library pthread
     c-function pthread_create pthread_create a{(pthread_t*)} a a a -- n ( thread attr start arg )
     c-function pthread_exit pthread_exit a -- void ( retaddr -- )
     c-function pthread_kill pthread_kill a{*(pthread_t*)} n -- n ( id sig -- rvalue )
-    c-function pthread_cancel pthread_cancel a{*(pthread_t*)} -- n ( addr -- r )
+    e? os-type s" linux-android" str= [IF]
+	c-function pthread_cancel pthread_cancel a{*(pthread_t*)} -- n ( addr -- r )
+    [THEN]
     c-function pthread_mutex_init pthread_mutex_init a a -- n ( mutex addr -- r )
     c-function pthread_mutex_destroy pthread_mutex_destroy a -- n ( mutex -- r )
     c-function pthread_mutex_lock pthread_mutex_lock a -- n ( mutex -- r )
@@ -352,7 +354,12 @@ event: ->sleep  stop ;
     <event ->sleep event> ;
 : kill ( task -- )
     \G Kill a task
-    user' pthread-id + pthread_cancel drop ;
+    user' pthread-id +
+    [IFDEF] pthread_cancel
+	pthread_cancel drop
+    [ELSE]
+	15 pthread_kill drop
+    [THEN] ;
 
 : elit,  ( x -- ) ->lit cell event+ [ cell 8 = ] [IF] x! [ELSE] l! [THEN] ;
 \G sends a literal
