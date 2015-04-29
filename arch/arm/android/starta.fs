1b5a9c7807a7e6e7147728fac53757da1eba7f69
Bernd Paysan <bernd.paysan@gmx.de>
2015-04-29 22:43:08 +0200

    Remove ++fpath, should not be needed

--- arch/arm/android/starta.fs
+++ arch/arm/android/starta.fs
@@ -1,16 +1,11 @@
 \ startup stuff
-: ++fpath ( addr u -- )
-    open-fpath-file 0=
-    IF fpath also-path close-file throw
-    ELSE 2drop drop THEN ;
-s" unix" ++fpath
-s" minos2" ++fpath
+." load terminal-server" cr stdout flush-file throw
+require ansi.fs
+require unix/terminal-server.fs
 ." load android" cr stdout flush-file throw
 require unix/android.fs
 ." load gl-terminal" cr stdout flush-file throw
 require minos2/gl-terminal.fs
 ." done loading" cr stdout flush-file throw
-." load terminal-server" cr stdout flush-file throw
-require unix/terminal-server.fs
 >screen
 : t get-connection ;
