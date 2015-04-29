0d3316820bc82963fbc965fc279c5128d52448d6
Bernd Paysan <bernd.paysan@gmx.de>
2015-04-29 16:21:30 +0200

    Terminal server loaded last

--- arch/arm/android/starta.fs
+++ arch/arm/android/starta.fs
@@ -5,12 +5,12 @@
     ELSE 2drop drop THEN ;
 s" unix" ++fpath
 s" minos2" ++fpath
-." load terminal-server" cr stdout flush-file throw
-require unix/terminal-server.fs
 ." load android" cr stdout flush-file throw
 require unix/android.fs
 ." load gl-terminal" cr stdout flush-file throw
 require minos2/gl-terminal.fs
 ." done loading" cr stdout flush-file throw
+." load terminal-server" cr stdout flush-file throw
+require unix/terminal-server.fs
 >screen
 : t get-connection ;
