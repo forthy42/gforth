c0bf13350b0bfec7e91e75573a7e99385370c2a3
Bernd Paysan <bernd.paysan@gmx.de>
2015-06-11 15:26:02 +0200

    Automated build of more than one Android plattform

--- arch/mips/android/build.sh
+++ arch/mips/android/build.sh
@@ -1,4 +1,39 @@
 #!/bin/bash
+#Copyright (C) 2011,2012,2013,2014 Free Software Foundation, Inc.
+
+#This file is part of Gforth.
+
+#Gforth is free software; you can redistribute it and/or
+#modify it under the terms of the GNU General Public License
+#as published by the Free Software Foundation, either version 3
+#of the License, or (at your option) any later version.
+
+#This program is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.#See the
+#GNU General Public License for more details.
+
+#You should have received a copy of the GNU General Public License
+#along with this program. If not, see http://www.gnu.org/licenses/.
+
+while [ "${1%%[^\+]*}" == '+' ]
+do
+    arch+=" ${1#+}"
+    shift
+done
+
+echo "Extra build in $arch"
+
+for i in $arch
+do
+    newdir=../../../../android-$i/arch/$i/android
+    if [ -d $newdir ]
+    then
+	(cd $newdir && git pull && ./build.sh "$@")
+    else
+	echo "Can't cd to $newdir"
+    fi
+done
 
 if [ ! -f build.xml ]
 then
