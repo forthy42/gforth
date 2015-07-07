94138162f82f6802af9d72c028059259459b85a5
Bernd Paysan <bernd.paysan@gmx.de>
2015-07-07 16:30:20 +0200

    Fix stack effect of case

--- extend.fs
+++ extend.fs
@@ -66,35 +66,36 @@ decimal
 [then]
 
 : case ( compilation  -- case-sys ; run-time  -- ) \ core-ext
-    postpone begin ['] drop 0 ; immediate restrict
+    postpone begin ['] drop ; immediate restrict
 
 : ?of ( compilation  -- of-sys ; run-time  f -- ) \ gforth
-    2>r POSTPONE if 2r> ; immediate restrict
+    POSTPONE if ; immediate restrict
 
 : of ( compilation  -- of-sys ; run-time x1 x2 -- |x1 ) \ core-ext
     \ !! the implementation does not match the stack effect
     postpone over postpone = postpone ?of postpone drop ; immediate restrict
 
 : endof ( compilation case-sys1 of-sys -- case-sys2 ; run-time  -- ) \ core-ext end-of
-    2>r postpone else 1 cs-roll 2r> 1+ ; immediate restrict
+    postpone leave postpone then ; immediate restrict
 
 : contof ( compilation case-sys1 of-sys -- case-sys2 ; run-time  -- )
     \ like @code{endof}, but loops back to the @code{case}
-    2>r 1 cs-pick postpone again postpone then 2r> ; immediate restrict
+    2>r 2>r 0 cs-pick postpone again
+    2r> 2r> postpone then ; immediate restrict
 
 : n-thens ( orig1 ... origu u -- )
     0 ?do postpone then loop ;
 
 : default: ( case-sys2 -- case-sys2' )
-    nip ['] noop swap ; immediate restrict
+    drop ['] noop ; immediate restrict
 
-: endcase ( compilation case-sys -- ; run-time x -- ) \ core-ext end-case
-    >r >r cs-drop r> compile, r> n-thens ; immediate restrict
+: endcase ( case-sys -- ; run-time x -- ) \ core-ext end-case
+    compile, postpone done ; immediate restrict
 
 : nextcase ( compilation case-sys -- ; run-time x -- ) \ gforth-undocumented
     \ like ENDCASE, but start again from the beginning if this is
     \ reached by fallthrough
-    >r compile, postpone again r> n-thens ; immediate restrict
+    compile, 0 cs-pick postpone again postpone done ; immediate restrict
 
 
 \ C"                                                    17may93jaw
