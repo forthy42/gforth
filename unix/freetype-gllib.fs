9ed52694e86004cad79de23de7567666786b0494
Bernd Paysan <bernd.paysan@gmx.de>
2015-09-10 03:39:47 +0200

    Move preload for android library ahead

--- unix/freetype-gllib.fs
+++ unix/freetype-gllib.fs
@@ -5,7 +5,7 @@ Vocabulary freetype-gl
 get-current also freetype-gl definitions
 
 e? os-type s" linux-android" string-prefix? [IF]
-    s" libtypeset.so" open-path-lib drop
+    s" libtypeset.so" also c-lib open-path-lib drop previous
 [THEN]
 
 c-library freetype-gllib
