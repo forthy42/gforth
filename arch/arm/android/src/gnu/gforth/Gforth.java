994817f4dd9210c48e8885d954e18b8a77638212
Bernd Paysan <bernd.paysan@gmx.de>
2015-11-08 21:39:40 +0100

    Set intent flag to bring activity to front

--- arch/arm/android/src/gnu/gforth/Gforth.java
+++ arch/arm/android/src/gnu/gforth/Gforth.java
@@ -407,7 +407,7 @@ public class Gforth
 	gforthintent = PendingIntent.getActivity(this,
 						 1, new Intent(this, Gforth.class),
 						 PendingIntent.FLAG_UPDATE_CURRENT);
-
+	gforthintent.setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
 
 	recConnectivity = new BroadcastReceiver() {
 		public void onReceive(Context context, Intent intent) {
