b1daa39fdae5045eba95ad93e9a5f30becbc2e89
Bernd Paysan <bernd.paysan@gmx.de>
2015-11-25 18:27:49 +0100

    Use custom SurfaceView to get all events

--- arch/arm/android/src/gnu/gforth/Gforth.java
+++ arch/arm/android/src/gnu/gforth/Gforth.java
@@ -51,19 +51,27 @@ import android.view.OrientationEventListener;
 import android.view.ViewTreeObserver.OnGlobalLayoutListener;
 import android.view.WindowManager;
 import android.view.inputmethod.InputMethodManager;
+import android.view.inputmethod.BaseInputConnection;
+import android.view.inputmethod.CompletionInfo;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputConnection;
+import android.text.Editable;
+import android.text.InputType;
+import android.text.SpannableStringBuilder;
 import android.app.Activity;
 import android.app.ProgressDialog;
 import android.app.AlarmManager;
 import android.app.PendingIntent;
 import android.net.ConnectivityManager;
 import android.util.Log;
+import android.util.AttributeSet;
 import android.widget.Toast;
 import java.lang.Object;
 import java.lang.Runnable;
 import java.lang.String;
 import java.io.File;
 import java.util.Locale;
-import gnu.gforth.GforthView;
+import gnu.gforth.R;
 
 public class Gforth
     extends android.app.Activity
@@ -115,6 +123,176 @@ public class Gforth
     public native void startForth(String libdir, String locale);
 
     // own subclasses
+    static class GforthView extends SurfaceView implements SurfaceHolder.Callback2 {
+	Gforth mActivity;
+	EditorInfo moutAttrs;
+	MyInputConnection mInputConnection;
+	
+	static class MyInputConnection extends BaseInputConnection {
+	    private SpannableStringBuilder mEditable;
+	    private GforthView mView;
+	    
+	    public MyInputConnection(GforthView targetView, boolean fullEditor) {
+		super(targetView, fullEditor);
+		mView = targetView;
+	    }
+	    
+	    public Editable getEditable() {
+		if (mEditable == null) {
+		    mEditable = (SpannableStringBuilder) Editable.Factory.getInstance()
+			.newEditable("");
+		}
+		return mEditable;
+	    }
+	    
+	    public void setEditLine(String line, int curpos) {
+		Log.v(TAG, "IC.setEditLine: \"" + line + "\" at: " + curpos);
+		getEditable().clear();
+		getEditable().append(line);
+		setSelection(curpos, curpos);
+	    }
+	    
+	    public boolean commitText(CharSequence text, int newcp) {
+		if(text != null) {
+		    mView.mActivity.onEventNative(12, text.toString());
+		} else {
+		    mView.mActivity.onEventNative(12, 0);
+		}
+		return true;
+	    }
+	    public boolean setComposingText(CharSequence text, int newcp) {
+		if(text != null) {
+		    mView.mActivity.onEventNative(13, text.toString());
+		} else {
+		    mView.mActivity.onEventNative(13, "");
+		}
+		return true;
+	    }
+	    public boolean finishComposingText () {
+		mView.mActivity.onEventNative(12, 0);
+		return true;
+	    }
+	    public boolean commitCompletion(CompletionInfo text) {
+		if(text != null) {
+		    mView.mActivity.onEventNative(12, text.toString());
+		} else {
+		    mView.mActivity.onEventNative(12, 0);
+		}
+		return true;
+	    }
+	    public boolean deleteSurroundingText (int before, int after) {
+		int i;
+		String send="";
+		for(i=0; i<before; i++) {
+		    send+="\b";
+		}
+		for(i=0; i<after; i++) {
+		    send+="\033[3~";
+		}
+		mView.mActivity.onEventNative(12, send); 
+		return true;
+	    }
+	    public boolean setComposingRegion (int start, int end) {
+		end-=start;
+		if(end < 0) {
+		    start+=end;
+		    end = -end;
+		}
+		mView.mActivity.onEventNative(19, start);
+		mView.mActivity.onEventNative(20, end);
+		return super.setComposingRegion(start, start+end);
+	    }
+	    public boolean sendKeyEvent (KeyEvent event) {
+		mView.mActivity.onEventNative(0, event);
+		return true;
+	    }
+	}
+	
+	public void init(Context context) {
+	    mActivity=(Gforth)context;
+	    setFocusable(true);
+	    setFocusableInTouchMode(true);
+	}
+	
+	public GforthView(Context context) {
+	    super(context);
+	    init(context);
+	}
+	
+	public GforthView(Context context, AttributeSet attrs) {
+	    super(context, attrs);
+	    init(context);
+	}
+	
+	public GforthView(Context context, AttributeSet attrs, int defStyle) {
+	    super(context, attrs, defStyle);
+	    init(context);
+	}
+	
+	@Override
+	public boolean onCheckIsTextEditor () {
+	    return true;
+	}
+	@Override
+	public InputConnection onCreateInputConnection (EditorInfo outAttrs) {
+	    moutAttrs=outAttrs;
+	    outAttrs.inputType = (InputType.TYPE_CLASS_TEXT |
+				  InputType.TYPE_TEXT_FLAG_AUTO_COMPLETE |
+				  InputType.TYPE_TEXT_FLAG_AUTO_CORRECT);
+	    outAttrs.initialSelStart = 1;
+	    outAttrs.initialSelEnd = 1;
+	    outAttrs.packageName = "gnu.gforth";
+	    mInputConnection = new MyInputConnection(this, true);
+	    return mInputConnection;
+	}
+	@Override
+	public void onSizeChanged(int w, int h, int oldw, int oldh) {
+	    mActivity.onEventNative(14, w);
+	    mActivity.onEventNative(15, h);
+	}
+	@Override
+	public boolean dispatchKeyEvent (KeyEvent event) {
+	    mActivity.onEventNative(0, event);
+	    return true;
+	}
+	@Override
+	public boolean onKeyDown (int keyCode, KeyEvent event) {
+	    mActivity.onEventNative(0, event);
+	    return true;
+	}
+	@Override
+	public boolean onKeyUp (int keyCode, KeyEvent event) {
+	    mActivity.onEventNative(0, event);
+	    return true;
+	}
+	@Override
+	public boolean onKeyMultiple (int keyCode, int repeatCount, KeyEvent event) {
+	    mActivity.onEventNative(0, event);
+	    return true;
+	}
+	@Override
+	public boolean onKeyLongPress (int keyCode, KeyEvent event) {
+	    mActivity.onEventNative(0, event);
+	    return true;
+	}
+	@Override
+	public void surfaceCreated(SurfaceHolder holder) {
+	    mActivity.surfaceCreated(holder);
+	}
+	@Override
+	public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+	    mActivity.surfaceChanged(holder, format, width, height);
+	}
+	@Override
+	public void surfaceRedrawNeeded(SurfaceHolder holder) {
+	    mActivity.surfaceRedrawNeeded(holder);
+	}
+	@Override
+	public void surfaceDestroyed(SurfaceHolder holder) {
+	    mActivity.surfaceDestroyed(holder);
+	}
+    }
+
     public class RunForth implements Runnable {
 	long xt;
 	RunForth(long initxt) {
