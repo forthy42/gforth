8a4881a7f259768115852384ae2e3f838361fcd1
Bernd Paysan <bernd.paysan@gmx.de>
2015-12-03 22:02:42 +0100

    Update to memory.fs proposal

--- memory.fs
+++ memory.fs
@@ -2,7 +2,7 @@
 
 \ this file is in the public domain, it's just the reference
 \ implementation. Assumes byte addressed access
-\ and 32 bit minimum
+\ and 16 bit minimum
 
 : w>< ( w -- w' )
     dup 8 rshift $FF and swap $FF and 8 lshift or ;
@@ -18,28 +18,31 @@
     [ELSE]
 	: xd>< ( x -- x' ) l>< swap l>< ;
     [THEN]
+[ELSE]
+    : ld>< ( ld -- ld' ) w>< swap w>< ;
+    : xq>< ( xq -- xq' ) l>< 2swap l>< ;
 [THEN]
 
 pad off 1 pad ! pad c@ 1 = [IF]
     \ little endian
     : w@ ( addr -- w )  dup c@ swap 1+ c@ 8 lshift or ;
     : w! ( w addr -- )  2dup c! 1+ >r 8 rshift r> c! ;
-    1 cells 8 = [IF]
+    1 cells 4 >= [IF]
 	: l@ ( addr -- l )  dup w@ swap 2 + w@ 16 lshift or ;
-	: x@ ( addr -- x )  dup l@ swap 4 + l@ 32 lshift or ;
 	: l! ( l addr -- )  2dup w! 2 + >r 16 rshift r> w! ;
+    [THEN]
+    1 cells 8 = [IF]
+	: x@ ( addr -- x )  dup l@ swap 4 + l@ 32 lshift or ;
 	: x! ( x addr -- )  2dup l! 4 + >r 32 rshift r> l! ;
     [ELSE]
 	1 cells 4 = [IF]
-	    : l@ ( addr -- l )  dup w@ swap 2 + w@ 16 lshift or ;
-	    : x@ ( addr -- x )  dup >r l@ r> 4 + l@ ;
-	    : l! ( l addr -- )  2dup w! 2 + >r 16 rshift r> w! ;
-	    : x! ( x addr -- )  tuck 4 + l! l! ;
+	    : xd@ ( addr -- xd )  dup >r l@ r> 4 + l@ ;
+	    : xd! ( xd addr -- )  tuck 4 + l! l! ;
 	[ELSE]
-	    : l@ ( addr -- l )  dup w@ swap 2 + w@ ;
-	    : x@ ( addr -- x )  dup >r ld@ r> 4 + ld@ ;
-	    : l! ( l addr -- )  2dup w! 2 + w! ;
-	    : x! ( x addr -- )  dup >r ld! r> 4 + ld! ;
+	    : ld@ ( addr -- ld )  dup w@ swap 2 + w@ ;
+	    : xq@ ( addr -- xq )  dup >r ld@ r> 4 + ld@ ;
+	    : ld! ( ld addr -- )  2dup w! 2 + w! ;
+	    : xq! ( xq addr -- )  dup >r ld! r> 4 + ld! ;
 	[THEN]
     [THEN]
     synonym wbe w><
@@ -51,22 +54,22 @@ pad off 1 pad ! pad c@ 1 = [IF]
 [ELSE]
     : w@ ( addr -- w )  dup c@ 8 lshift swap 1+ c@ or ;
     : w! ( w addr -- )  2dup 1+ c! >r 8 rshift r> c! ;
-    1 cells 8 = [IF]
+    1 cells 4 >= [IF]
 	: l@ ( addr -- l )  dup w@ 16 lshift swap 2 + w@ or ;
-	: x@ ( addr -- x )  dup l@ 32 lshift swap 4 + l@ or ;
 	: l! ( l addr -- )  2dup 2 + w! >r 16 rshift r> w! ;
+    [THEN]
+    1 cells 8 = [IF]
+	: x@ ( addr -- x )  dup l@ 32 lshift swap 4 + l@ or ;
 	: x! ( x addr -- )  2dup 4 + l! >r 32 rshift r> l! ;
     [ELSE]
 	1 cells 4 = [IF]
-	    : l@ ( addr -- l )  dup 2 + w@ swap w@ 16 lshift or ;
-	    : x@ ( addr -- x )  dup >r 4 + l@ r> l@ ;
-	    : l! ( l addr -- )  2dup 2 + w! >r 16 rshift r> w! ;
-	    : x! ( x addr -- )  tuck l! 4 + l! ;
+	    : xd@ ( addr -- x )  dup >r 4 + l@ r> l@ ;
+	    : xd! ( x addr -- )  tuck l! 4 + l! ;
 	[ELSE]
-	    : l@ ( addr -- l )  dup 2 + w@ swap w@ ;
-	    : x@ ( addr -- x )  dup >r 4 + ld@ r> ld@ ;
-	    : l! ( l addr -- )  2dup 2 + w! w! ;
-	    : x! ( x addr -- )  dup >r 4 + ld! r> ld! ;
+	    : ld@ ( addr -- ld )  dup 2 + w@ swap w@ ;
+	    : xq@ ( addr -- xq )  dup >r 4 + ld@ r> ld@ ;
+	    : ld! ( ld addr -- )  2dup 2 + w! w! ;
+	    : xq! ( xq addr -- )  dup >r 4 + ld! r> ld! ;
 	[THEN]
     [THEN]
     synonym wle w><
