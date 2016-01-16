24443aca4b4136ba18330f1c67764e32186fbfd8
Bernd Paysan <bernd.paysan@gmx.de>
2016-01-16 23:23:03 +0100

    Another typo fix

--- engine/androidmain.c
+++ engine/androidmain.c
@@ -195,7 +195,7 @@ void addfileargs(char* filename)
   }
 }
 
-static const char *paths[] = { "--path=/sdcard/gforth/" PACKAGE_VERSION ":/sdcard/gforth" ARCH "/gforth/" PACKAGE_VERSION ":/sdcard/gforth/site-forth",
+static const char *paths[] = { "--",
 			       "--path=/mnt/sdcard/gforth/" PACKAGE_VERSION ":/mnt/sdcard/gforth-" ARCH "/gforth/" PACKAGE_VERSION ":/mnt/sdcard/gforth/site-forth",
 			       "--path=/data/data/gnu.gforth/files/gforth/" PACKAGE_VERSION ":/data/data/gnu.gforth/files/gforth-" ARCH "/gforth/" PACKAGE_VERSION ":/data/data/gnu.gforth/files/gforth/site-forth" };
 static const char *folder[] = { "/sdcard", "/mnt/sdcard", "/data/data/gnu.gforth/files" };
