fd7038723871167e9389a1a02bae51ca85ef3e82
Bernd Paysan <bernd.paysan@gmx.de>
2016-01-16 20:01:33 +0100

    Separate arch-specific and generic zip files

--- arch/arm/android/build.sh
+++ arch/arm/android/build.sh
@@ -117,10 +117,11 @@ then
 
     cp *.{fs,fi,png,jpg} $SRC/debian/sdcard/gforth/site-forth
     (cd $SRC/debian/sdcard
-	rm -rf gforth/$GFORTH_VERSION/$machine/libcc-named
-	mkdir -p gforth/home
-	gforth ../../archive.fs gforth/home/ $(find gforth -type f)) | gzip -9 >$LIBS/libgforthgz.so
-	gforth ../../archive.fs gforth-$machine $(find gforth -type f)) | gzip -9 >$LIBS/libgforth-${machine}gz.so
+     rm -rf gforth/$GFORTH_VERSION/$machine/libcc-named
+     mkdir -p gforth/home
+     gforth ../../archive.fs gforth/home/ $(find gforth -type f)) | gzip -9 >$LIBS/libgforthgz.so
+    (cd $SRC/debian/sdcard
+     gforth ../../archive.fs gforth-$machine $(find gforth -type f)) | gzip -9 >$LIBS/libgforth-${machine}gz.so
 else
     shift
 fi
