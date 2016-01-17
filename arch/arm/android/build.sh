22c07ae294826ff750d1aaaff8af6a797b4e1986
Bernd Paysan <bernd.paysan@gmx.de>
2016-01-17 03:09:02 +0100

    Spinner hiding needs post, too

--- arch/arm/android/build.sh
+++ arch/arm/android/build.sh
@@ -119,8 +119,8 @@ then
     mkdir -p $SRC/debian/sdcard/gforth/$machine/gforth/site-forth
     cp *.{fs,fi,png,jpg} $SRC/debian/sdcard/gforth/$machine/gforth/site-forth
     (cd $SRC/debian/sdcard
-     mkdir -p gforth/home
-     gforth archive.fs gforth/home/ gforth/site-forth $(find gforth/$GFORTH_VERSION -type f) $(find gforth/site-forth -type f)) | gzip -9 >$LIBS/libgforthgz.so
+     mkdir -p gforth/home gforth/site-forth
+     gforth archive.fs gforth/home/ gforth/site-forth/ $(find gforth/$GFORTH_VERSION -type f) $(find gforth/site-forth -type f)) | gzip -9 >$LIBS/libgforthgz.so
     (cd $SRC/debian/sdcard
      rm gforth/$machine/lib*
      rm -rf gforth/$machine/gforth/$GFORTH_VERSION/$machine/libcc-named
