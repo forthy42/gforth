a6202918f4378f415814c850862606c0bdda27fa
Bernd Paysan <bernd.paysan@gmx.de>
2016-03-08 20:18:13 +0100

    Autodetect toolchain directory

--- arch/arm/android/gentoolchain.sh
+++ arch/arm/android/gentoolchain.sh
@@ -13,9 +13,9 @@ then
      ~/Downloads/android-ndk-r$NDK-linux-$CPU.bin)
 fi
 
-for i in arm arm64 386 amd64 mips
+for i in arm aarch64 i686 x86_64 mipsel
 do
     mkdir -p ~/proj/android-toolchain-$i
     (cd ~/proj/android-toolchain-$i
-     ~/proj/android-ndk-r$NDK/build/tools/make-standalone-toolchain.sh --arch=arm --platform=android-14 --ndk-dir=/home/bernd/proj/android-ndk-r$NDK --install-dir=$PWD --toolchain=arm-linux-androideabi-4.9)
+     ~/proj/android-ndk-r$NDK/build/tools/make-standalone-toolchain.sh --arch=arm --platform=android-21 --ndk-dir=/home/bernd/proj/android-ndk-r$NDK --install-dir=$PWD --toolchain=$i-linux-androideabi-4.9)
 done
