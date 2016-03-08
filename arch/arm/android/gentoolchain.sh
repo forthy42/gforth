0be0b57e05e734fe560912ea664a057f0af2809e
Bernd Paysan <bernd.paysan@gmx.de>
2016-03-08 22:48:17 +0100

    Toolchain build works

--- arch/arm/android/gentoolchain.sh
+++ arch/arm/android/gentoolchain.sh
@@ -24,28 +24,34 @@ then
      tar zxvf ~/Downloads/libtool-$LIBT.tar.gz)
 fi
 
-for i in arm aarch64 i686 x86_64 mipsel mips64el
+for i in arm aarch64 x86 x86_64 mipsel mips64el
 do
+    unset ABIX
+    unset ARCHX
     ARCH=$i
     ABI=""
     case $i in
 	arm)
 	    ABI=-linux-androideabi
+	    ARCHX=$i
 	    ;;
 	aarch64)
 	    ABI=-linux-android
 	    ARCH=arm64
+	    ARCHX=$i
 	    ;;
 	mipsel|mips64el)
 	    ARCH=${i%el}
 	    ABI=-linux-android
+	    ARCHX=$i
 	    ;;
-	i686)
-	    ARCH=x86
+	x86)
+	    ARCHX=i686
 	    ABIX=-linux-android
 	    ;;
 	x86_64)
 	    ABIX=-linux-android
+	    ARCHX=$i
 	    ;;
     esac
     ABIX=${ABIX-$ABI}
@@ -53,7 +59,7 @@ do
     (cd ~/proj/android-toolchain-$ARCH
      ~/proj/android-ndk-r$NDK/build/tools/make-standalone-toolchain.sh --arch=$ARCH --platform=android-21 --ndk-dir=/home/bernd/proj/android-ndk-r$NDK --install-dir=$PWD --toolchain=$i$ABI-$CCVER)
     (cd ~/proj/libtool-$LIBT
-     ./configure --host=$i$ABIX --program-prefix=$i$ABIX- --prefix=$HOME/proj/android-toolchain-$ARCH host_alias=$CPU-linux-gnu
+     ./configure --host=$ARCHX$ABIX --program-prefix=$ARCHX$ABIX- --prefix=$HOME/proj/android-toolchain-$ARCH host_alias=$CPU-linux-gnu
      make && make install && make clean
     )
 done
