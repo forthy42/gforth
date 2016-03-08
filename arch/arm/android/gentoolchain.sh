0eb2f0414d11e3346816c6b649b0c691ffd0cfa0
Bernd Paysan <bernd.paysan@gmx.de>
2016-03-08 21:55:19 +0100

    Toolchain build works

--- arch/arm/android/gentoolchain.sh
+++ arch/arm/android/gentoolchain.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 NDK=${NDK-10e}
+LIBT=${LIBT-2.4.6}
 CPU=$(uname -p)
+CCVER=${CCVER-4.9}
 
 if [ ! -d ~/proj/android-ndk-r$NDK ]
 then
@@ -13,9 +15,44 @@ then
      ~/Downloads/android-ndk-r$NDK-linux-$CPU.bin)
 fi
 
-for i in arm aarch64 i686 x86_64 mipsel
+if [ ! -d ~/proj/libtool-$LIBT ]
+then
+    (cd ~/Downloads
+     wget http://ftpmirror.gnu.org/libtool/libtool-$LIBT.tar.gz
+     mkdir -p ~/proj
+     cd ~/proj
+     tar zxvf ~/Downloads/libtool-$LIBT.tar.gz)
+fi
+
+for i in arm aarch64 x86 x86_64 mipsel mips64el
 do
-    mkdir -p ~/proj/android-toolchain-$i
-    (cd ~/proj/android-toolchain-$i
-     ~/proj/android-ndk-r$NDK/build/tools/make-standalone-toolchain.sh --arch=arm --platform=android-21 --ndk-dir=/home/bernd/proj/android-ndk-r$NDK --install-dir=$PWD --toolchain=$i-linux-androideabi-4.9)
+    ARCH=$i
+    case $i in
+	arm)
+	    ABI=-linux-androideabi
+	    ;;
+	aarch64)
+	    ABI=-linux-android
+	    ARCH=arm64
+	    ;;
+	mipsel|mips64el)
+	    ARCH=${i%el}
+	    ABI=-linux-android
+	    ;;
+	*)
+	    ABI=""
+	    ;;
+    esac
+    mkdir -p ~/proj/android-toolchain-$ARCH
+    (cd ~/proj/android-toolchain-$ARCH
+     ~/proj/android-ndk-r$NDK/build/tools/make-standalone-toolchain.sh --arch=$ARCH --platform=android-21 --ndk-dir=/home/bernd/proj/android-ndk-r$NDK --install-dir=$PWD --toolchain=$i$ABI-$CCVER)
+    (cd ~/proj/libtool-$LIBT
+     ./configure --host=$i$ABI --program-prefix=$i$ABI- --prefix=$HOME/proj/android-toolchain-$ARCH host_alias=$CPU-linux-gnu
+     make && make install && make clean
+    )
 done
+
+cat <<EOF
+Add the following line to your .bashrc:
+PATH=\$PATH:\$(echo ~/proj/android-toolchain-{arm,arm64,x86,x86_64,mips,mips64}/bin | tr ' ' ':')
+EOF
