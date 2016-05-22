88d741a35328bc0294f76650be1ee484be2e4428
Bernd Paysan <bernd.paysan@gmx.de>
2016-05-22 03:31:41 +0200

    Add FSF style copyright to MINOS2 files

--- minos2/soil-texture.fs
+++ minos2/soil-texture.fs
@@ -1,5 +1,22 @@
 \ soil texture
 
+\ Copyright (C) 2014 Free Software Foundation, Inc.
+
+\ This file is part of Gforth.
+
+\ Gforth is free software; you can redistribute it and/or
+\ modify it under the terms of the GNU General Public License
+\ as published by the Free Software Foundation, either version 3
+\ of the License, or (at your option) any later version.
+
+\ This program is distributed in the hope that it will be useful,
+\ but WITHOUT ANY WARRANTY; without even the implied warranty of
+\ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+\ GNU General Public License for more details.
+
+\ You should have received a copy of the GNU General Public License
+\ along with this program. If not, see http://www.gnu.org/licenses/.
+
 require gl-helper.fs
 s" unix/soil2.fs" open-fpath-file 0= [IF]
     \ prefer soil2 over soil
--- minos2/widgets.fs
+++ minos2/widgets.fs
@@ -1,5 +1,22 @@
 \ MINOS2 widget basis
 
+\ Copyright (C) 2014 Free Software Foundation, Inc.
+
+\ This file is part of Gforth.
+
+\ Gforth is free software; you can redistribute it and/or
+\ modify it under the terms of the GNU General Public License
+\ as published by the Free Software Foundation, either version 3
+\ of the License, or (at your option) any later version.
+
+\ This program is distributed in the hope that it will be useful,
+\ but WITHOUT ANY WARRANTY; without even the implied warranty of
+\ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+\ GNU General Public License for more details.
+
+\ You should have received a copy of the GNU General Public License
+\ along with this program. If not, see http://www.gnu.org/licenses/.
+
 \ A MINOS2 widget is composed of drawable elements, boxes and actors.
 \ to make things easier, neither drawable elements nor boxes need an actor.
 
