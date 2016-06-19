12cc6882c09ac52c22ab83bcb891e663494a7b26
Bernd Paysan <bernd.paysan@gmx.de>
2016-06-19 15:34:53 +0200

    wlscope example in rec-scope.fs, not yet activated by default

--- rec-scope.fs
+++ rec-scope.fs
@@ -17,12 +17,19 @@
 \ You should have received a copy of the GNU General Public License
 \ along with this program. If not, see http://www.gnu.org/licenses/.
 
-: rec:scope ( addr u -- xt | r:fail )
-    ':' $split 2swap find-name dup IF
+: scope-split ( addr u -- addr' u' wid/0 )
+    ':' $split dup 0= IF  nip  EXIT  THEN
+    2swap find-name dup IF
 	dup >does-code [ ' forth >does-code ]L = IF
-	    >body find-name-in dup 0= IF  drop  r:fail  THEN
-	    EXIT
-	THEN
-    THEN  drop 2drop r:fail ;
+	    >body  EXIT  THEN  drop 0  THEN ;
+
+: rec:scope ( addr u -- xt | r:fail )
+    scope-split dup IF find-name-in dup 0= IF  drop  r:fail  THEN
+    ELSE  drop 2drop r:fail  THEN ;
 
 get-recognizers 1+ ' rec:scope -rot set-recognizers
+
+: :wlscope ( addr u -- addr' u' wid )
+    2dup scope-split dup IF  >r 2nip r>  ELSE  drop 2drop defers wlscope  THEN ;
+
+\ activate with ' :wlscope is wlscope
