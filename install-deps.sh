753af8f6a04110becfe6757a9c44dcd426b066c0
Bernd Paysan <bernd.paysan@gmx.de>
2016-07-18 17:41:24 +0200

    Separate install of libtool-bin into another line, since this may fail on some debians

--- install-deps.sh
+++ install-deps.sh
@@ -2,7 +2,8 @@
 
 install_linux() {
   sudo apt-get update
-  sudo apt-get install gforth libffi-dev libltdl7 libsoil-dev libtool-bin yodl
+  sudo apt-get install gforth libffi-dev libltdl7 libsoil-dev yodl
+  sudo apt-get install libtool-bin
   if [ `uname -m`$M32 = x86_64-m32 ]; then
     sudo apt-get --fix-missing install gcc-multilib libltdl7:i386
   fi
