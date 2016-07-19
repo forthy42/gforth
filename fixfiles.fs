48c7410f89629749b170f4f0120f178049b42a15
Bernd Paysan <bernd.paysan@gmx.de>
2016-07-19 15:40:27 +0200

    Added fix for filenames in image

---
+++ fixfiles.fs
@@ -0,0 +1,21 @@
+\ fix loaded files
+
+\ Copyright (C) 2016 Free Software Foundation, Inc.
+
+\ This file is part of Gforth.
+
+\ Gforth is free software; you can redistribute it and/or
+\ modify it under the terms of the GNU General Public License
+\ as published by the Free Software Foundation, either version 3
+\ of the License, or (at your option) any later version.
+
+\ This program is distributed in the hope that it will be useful,
+\ but WITHOUT ANY WARRANTY; without even the implied warranty of
+\ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+\ GNU General Public License for more details.
+
+\ You should have received a copy of the GNU General Public License
+\ along with this program. If not, see http://www.gnu.org/licenses/.
+
+s" GFORTHDESTDIR" getenv
+included-files $[]# 0 [?DO]  [I] included-files $[]@ open-fpath-file 0= [IF] rot close-file throw 2over 2over string-prefix? [IF] 2 pick /string [THEN] [I] included-files $[] off [I] included-files $[]! [THEN] [LOOP] 2drop
