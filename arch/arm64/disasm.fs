4f620c284751b2d95d8fd6e8e0c1eeb7a8ab3581
Bernd Paysan <bernd.paysan@gmx.de>
2016-07-22 14:43:19 +0200

    More on ARM64 disassembler

--- arch/arm64/disasm.fs
+++ arch/arm64/disasm.fs
@@ -94,7 +94,7 @@ disassembler also definitions
     endcase ;
 : ucbranch ( opcode -- )
     dup >opc dup #5 u> IF  drop unallocated
-    ELSE  nip s" br  blr ret eretdrps" rot .4" space .rn  THEN
+    ELSE  nip s" br  blr ret eretdrps" rot .4" space .rn  THEN ;
 
 Create inst-table
 \ branches
