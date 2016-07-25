5412126bb7d73c20169219ab236973a334fee845
Bernd Paysan <bernd.paysan@gmx.de>
2016-07-25 21:47:49 +0200

    Reorder optional installs for Travis build

--- install-deps.sh
+++ install-deps.sh
@@ -2,8 +2,11 @@
 
 install_linux() {
   sudo apt-get update
-  sudo apt-get install gforth gforth-lib gforth-common libffi-dev libltdl7 libsoil-dev libtool install-info libx11-dev libgles2-mesa-dev libgl-dev yodl bison libpcre3-dev libboost-dev # yodl, bison... are for swig
+  sudo apt-get install gforth gforth-lib gforth-common libffi-dev libltdl7 libsoil-dev libtool install-info yodl bison libpcre3-dev libboost-dev # yodl, bison... are for swig
   sudo apt-get install libtool-bin
+  sudo apt-get install libx11-dev
+  sudo apt-get install libgles2-mesa-dev
+  sudo apt-get install libgl1-mesa-dev
   if [ `uname -m`$M32 = x86_64-m32 ]; then
     sudo apt-get --fix-missing install gcc-multilib libltdl7:i386
   fi
