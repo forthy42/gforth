d96b07a4438d296467242de4d7b7cae961ecdcc6
Anton Ertl <anton@mips.complang.tuwien.ac.at>
2016-08-25 19:14:36 +0200

    objects.fs is a standard program again

--- objects.fs
+++ objects.fs
@@ -350,7 +350,11 @@ variable public-wordlist
 \ this/self, instance variables etc.
 
 \ rename "this" into "self" if you are a Smalltalk fiend
-uvalue this ( -- object ) \ objects- objects
+[defined] uvalue [if]
+    uvalue this ( -- object ) \ objects- objects
+[else]
+    0 value this ( -- object ) \ objects- objects
+[then]
 \g the receiving object of the current method (aka active object).
 : to-this ( object -- ) \ objects- objects
     \g Set @code{this} (used internally, but useful when debugging).
