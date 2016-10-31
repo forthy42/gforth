f1b898572ce37cf5302a3e899acb257e745dafc1
Bernd Paysan <bernd.paysan@gmx.de>
2016-11-01 00:44:12 +0100

    Cleanup

--- unix/filestat.fs
+++ unix/filestat.fs
@@ -17,7 +17,7 @@
 \ You should have received a copy of the GNU General Public License
 \ along with this program. If not, see http://www.gnu.org/licenses/.
 
-c-library filestat
+c-library stat
     \c #include <sys/types.h>
     \c #include <sys/stat.h>
     \c #include <sys/time.h>
