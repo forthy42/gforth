71911cc08b46862a32eb2ea5b16f961e88f300c8
Bernd Paysan <bernd.paysan@gmx.de>
2016-11-16 23:24:41 +0100

    Chinese font for Android 7

--- minos2/widgets-test.fs
+++ minos2/widgets-test.fs
@@ -66,9 +66,10 @@ also freetype-gl
 atlas fontsize#
 [IFDEF] android
     "/system/fonts/DroidSans.ttf"
-[ELSE] "/usr/share/fonts/truetype/LiberationSans-Regular.ttf" 2dup file-status nip [IF]
-	2drop
-	"/usr/share/fonts/truetype/liberation/LiberationSans-Regular.ttf"
+[ELSE]
+    "/usr/share/fonts/truetype/LiberationSans-Regular.ttf"
+    2dup file-status nip [IF]
+	2drop "/usr/share/fonts/truetype/liberation/LiberationSans-Regular.ttf"
     [THEN]
 [THEN]
 2dup file-status throw drop
@@ -77,11 +78,14 @@ texture_font_new_from_file Value font1
 atlas fontsize#
 [IFDEF] android  "/system/fonts/DroidSansFallback.ttf"
     2dup file-status nip [IF]
-	2drop "/system/fonts/NotoSansSC-Regular.otf"
+	2drop "/system/fonts/NotoSansSC-Regular.otf" \ for Android 6
+	2dup file-status nip [IF]
+	    2drop "/system/fonts/NotoSansCJK-Regular.ttc" \ for Android 7
+	[THEN]
     [THEN]
-[ELSE] "/usr/share/fonts/truetype/gkai00mp.ttf" 2dup file-status nip [IF]
-	2drop
-	"/usr/share/fonts/truetype/arphic-gkai00mp/gkai00mp.ttf"
+[ELSE] "/usr/share/fonts/truetype/gkai00mp.ttf"
+    2dup file-status nip [IF]
+	2drop "/usr/share/fonts/truetype/arphic-gkai00mp/gkai00mp.ttf"
     [THEN]
 [THEN]
 2dup file-status throw drop
