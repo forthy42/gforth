f71c72f53b21eee871022e6f744786944434af22
Bernd Paysan <bernd.paysan@gmx.de>
2017-04-07 20:40:15 +0200

    PNG support for all other OSes

--- unix/png.fs
+++ unix/png.fs
@@ -14,26 +14,22 @@ c-library png
 	\c #include "../../../../libpng/pngconf.h"
 	\c #include "../../../../libpng/png.h"
     [ELSE]
-	e? os-type s" linux" string-prefix?
-	e? os-type s" darwin" string-prefix?
-	e? os-type s" cywgin" str= or or [IF]
-	    s" unix/pnglib16.fs" open-fpath-file 0= [IF]
-		2drop close-file throw
-		: png16 ;
-	    [THEN]
-	    [IFDEF] png16
-		s" png16" add-lib
-		\c #define PNG_STDIO_SUPPORTED 1
-		\c #define PNG_ERROR_TEXT_SUPPORTED 1
-		\c #include <zlib.h>
-		\c #include <libpng16/pngconf.h>
-		\c #include <libpng16/png.h>
-	    [ELSE]
-		s" png12" add-lib
-		\c #include <zlib.h>
-		\c #include <libpng12/pngconf.h>
-		\c #include <libpng12/png.h>
-	    [THEN]
+	s" unix/pnglib16.fs" open-fpath-file 0= [IF]
+	    2drop close-file throw
+	    : png16 ;
+	[THEN]
+	[IFDEF] png16
+	    s" png16" add-lib
+	    \c #define PNG_STDIO_SUPPORTED 1
+	    \c #define PNG_ERROR_TEXT_SUPPORTED 1
+	    \c #include <zlib.h>
+	    \c #include <libpng16/pngconf.h>
+	    \c #include <libpng16/png.h>
+	[ELSE]
+	    s" png12" add-lib
+	    \c #include <zlib.h>
+	    \c #include <libpng12/pngconf.h>
+	    \c #include <libpng12/png.h>
 	[THEN]
     [THEN]
     [IFDEF] png16
