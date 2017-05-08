a9fd79cfde4b89299bd062131c4a8584894bb8a0
Bernd Paysan <bernd.paysan@gmx.de>
2017-05-08 15:13:21 +0200

    Added meta recognizer to select the recognizer you want to use

---
+++ rec-meta.fs
@@ -0,0 +1,31 @@
+\ meta-recognizer for disambiguation
+
+\ Copyright (C) 2017 Free Software Foundation, Inc.
+
+\ This file is part of Gforth.
+
+\ Gforth is free software; you can redistribute it and/or
+\ modify it under the terms of the GNU General Public License
+\ as published by the Free Software Foundation, either version 3
+\ of the License, or (at your option) any later version.
+
+\ This program is distributed in the hope that it will be useful,
+\ but WITHOUT ANY WARRANTY; without even the implied warranty of
+\ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+\ GNU General Public License for more details.
+
+\ You should have received a copy of the GNU General Public License
+\ along with this program. If not, see http://www.gnu.org/licenses/.
+
+: rec:meta ( addr u -- xt r:to | r:fail )
+    \G words prefixed with @var{recognizer}@code{?} are treated as if
+    \G preceeded by @code{rec:}@var{recognizer}
+    '?' $split dup 0= IF  2drop
+    ELSE
+	2swap [: ." rec:" type ;] $tmp find-name ?dup-IF
+	    name?int execute  EXIT
+	THEN
+    THEN
+    2drop r:fail ;
+
+get-recognizers ' rec:meta swap 1+ set-recognizers
