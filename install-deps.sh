cef689aa2d0d4b80fa58d535a5a32097a05fc348
Bernd Paysan <bernd.paysan@gmx.de>
2017-05-14 00:24:30 +0200

    Travis for 0.7.3

--- install-deps.sh
+++ install-deps.sh
@@ -3,9 +3,11 @@
 install_linux() {
   sudo apt-get update
   sudo apt-get install gforth gforth-lib gforth-common libffi-dev libltdl7 libsoil-dev libtool install-info
-  sudo apt-get install libtool-bin
   if [ `uname -m`$M32 = x86_64-m32 ]; then
     sudo apt-get --fix-missing install gcc-multilib libltdl7:i386
+    sudo apt-get install libtool-bin:i386
+  else
+    sudo apt-get install libtool-bin
   fi
 }
 
