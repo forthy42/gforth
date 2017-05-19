3599ea7c2aa85757f184e01cae6ff1ce426cdfa9
Bernd Paysan <bernd.paysan@gmx.de>
2017-05-19 23:24:58 +0200

    Added reverse words

---
+++ reverse-words.fs
@@ -0,0 +1,30 @@
+\ display words in reverse
+
+\ Copyright (C) 2017 Free Software Foundation, Inc.
+
+\ This file is part of Gforth.
+
+\ Gforth is free software; you can redistribute it and/or
+\ modify it under the terms of the GNU General Public License
+\ as published by the Free Software Foundation, either version 3
+\ of the License, or (at your option) any later version.
+
+\ This program is distributed in the hope that it will be useful,
+\ but WITHOUT ANY WARRANTY; without even the implied warranty of
+\ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+\ GNU General Public License for more details.
+
+\ You should have received a copy of the GNU General Public License
+\ along with this program. If not, see http://www.gnu.org/licenses/.
+
+Variable words[]
+
+0 warnings !@
+: wordlist-words ( wid -- )
+    [: words[] dup $[]# swap $[] ! true ;] swap traverse-wordlist
+    cr 0  words[] $@ bounds cell- swap cell- U-DO
+	I @ .word
+    cell -LOOP  drop
+    words[] $free ;
+: words ( -- ) context @ wordlist-words ;
+warnings !
