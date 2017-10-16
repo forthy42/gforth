bbd5fd4781c891508b6dc1b3130660656f22c6f5
Bernd Paysan <bernd.paysan@gmx.de>
2017-10-16 02:05:36 +0200

    Harfbuzz bindings

---
+++ unix/harfbuzz.i
@@ -0,0 +1,31 @@
+// this file is in the public domain
+%module harfbuzz
+%insert("include")
+%{
+#include <harfbuzz/hb.h>
+#ifdef __gnu_linux__
+#undef stderr
+extern struct _IO_FILE *stderr;
+#endif
+%}
+
+%apply unsigned int { hb_codepoint_t, hb_tag_t };
+%apply int { hb_position_t };
+
+// exec: sed -e 's/ s n / a n /g' -e 's/c-function hb_glyph_info_get_glyph_flags/\\ c-function hb_glyph_info_get_glyph_flags/g'
+
+#define HB_BEGIN_DECLS
+#define HB_END_DECLS
+
+%include "harfbuzz/hb.h"
+%include "harfbuzz/hb-blob.h"
+%include "harfbuzz/hb-buffer.h"
+%include "harfbuzz/hb-common.h"
+%include "harfbuzz/hb-deprecated.h"
+%include "harfbuzz/hb-face.h"
+%include "harfbuzz/hb-font.h"
+%include "harfbuzz/hb-set.h"
+%include "harfbuzz/hb-shape.h"
+%include "harfbuzz/hb-shape-plan.h"
+%include "harfbuzz/hb-unicode.h"
+%include "harfbuzz/hb-version.h"
---
+++ unix/harfbuzzlib.fs
@@ -0,0 +1,40 @@
+\ wrapper to load Swig-generated libraries
+
+\ Copyright (C) 2016 Free Software Foundation, Inc.
+
+\ This file is part of Gforth.
+
+\ Gforth is free software; you can redistribute it and/or
+\ modify it under the terms of the GNU General Public License
+\ as published by the Free Software Foundation, either version 3
+\ of the License, or (at your option) any later version.
+
+\ This program is distributed in the hope that it will be useful,
+\ but WITHOUT ANY WARRANTY; without even the implied warranty of
+\ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+\ GNU General Public License for more details.
+
+\ You should have received a copy of the GNU General Public License
+\ along with this program. If not, see http://www.gnu.org/licenses/.
+
+cs-vocabulary harfbuzz \ needs to be case sensitive
+get-current also harfbuzz definitions
+
+e? os-type s" linux-android" string-prefix? [IF]
+    s" libtypeset.so" also c-lib open-path-lib drop previous
+[THEN]
+
+c-library harfbuzzlib
+    \c #include <harfbuzz/hb.h>
+
+
+    e? os-type s" linux-android" string-prefix? [IF]
+	s" typeset" add-lib
+    [ELSE]
+	s" harfbuzz" add-lib
+    [THEN]
+    
+    include unix/harfbuzz.fs
+end-c-library
+
+set-current
