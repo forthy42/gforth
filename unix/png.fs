8b7606ac94313c6a7294fb325ce087e45ea516ad
Bernd Paysan <bernd.paysan@gmx.de>
2017-11-21 15:21:17 +0100

    png.fs corrected for Android build

--- unix/png.fs
+++ unix/png.fs
@@ -8,34 +8,23 @@ Vocabulary pnglib
 get-current also pnglib definitions
 
 c-library png
-    e? os-type s" linux-android" string-prefix? [IF]
-	s" png" add-lib
-	\c #include <zlib.h>
-	\c #include "../../../../libpng/pngconf.h"
-	\c #include "../../../../libpng/png.h"
-    [ELSE]
-	s" unix/pnglib16.fs" open-fpath-file 0= [IF]
-	    2drop close-file throw
-	    : png16 ;
-	[THEN]
-	[IFDEF] png16
-	    s" png16" add-lib
-	    \c #define PNG_STDIO_SUPPORTED 1
-	    \c #define PNG_ERROR_TEXT_SUPPORTED 1
-	    \c #include <zlib.h>
-	    \c #include <libpng16/pngconf.h>
-	    \c #include <libpng16/png.h>
-	[ELSE]
-	    s" png12" add-lib
-	    \c #include <zlib.h>
-	    \c #define PNG_SKIP_SETJMP_CHECK 1
-	    \c #include <libpng12/pngconf.h>
-	    \c #include <libpng12/png.h>
-	[THEN]
+    s" unix/pnglib16.fs" open-fpath-file 0= [IF]
+	2drop close-file throw
+	: png16 ;
     [THEN]
+    \c #include <zlib.h>
     [IFDEF] png16
+	s" png16" add-lib
+	\c #define PNG_STDIO_SUPPORTED 1
+	\c #define PNG_ERROR_TEXT_SUPPORTED 1
+	\c #include <libpng16/pngconf.h>
+	\c #include <libpng16/png.h>
 	include unix/pnglib16.fs
     [ELSE]
+	s" png12" add-lib
+	\c #define PNG_SKIP_SETJMP_CHECK 1
+	\c #include <libpng12/pngconf.h>
+	\c #include <libpng12/png.h>
 	include unix/pnglib.fs
     [THEN]
 end-c-library
