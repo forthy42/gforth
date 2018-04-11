9b11fef315fdbb5877ff9920c6a36bc6e90e77bc
Bernd Paysan <bernd.paysan@gmx.de>
2018-04-11 22:54:08 +0200

    nested tab expansion for rec-scope.fs

--- rec-scope.fs
+++ rec-scope.fs
@@ -18,11 +18,15 @@
 \ along with this program. If not, see http://www.gnu.org/licenses/.
 
 : find-name?in ( addr u wid/0 -- nt/0 )
-     ?dup-IF  find-name-in  ELSE  find-name  THEN ;
+    ?dup-IF  find-name-in  ELSE  find-name  THEN ;
+
+: nosplit? ( addr1 u1 addr2 u2 --  addr1 u1 addr2 u2 flag )
+    \G is true if it didn't split
+    dup 0= IF  over >r 2over + r> =  ELSE  false  THEN ;
 
 : scope-split ( addr u wid -- nt/0 )
     BEGIN  >r
-	':' $split dup 0= IF  2drop r> find-name?in  EXIT  THEN
+	':' $split nosplit? IF  2drop r> find-name?in  EXIT  THEN
 	2swap r> find-name?in
 	dup WHILE
 	    dup >does-code [ ' forth >does-code ]L = WHILE
@@ -41,13 +45,20 @@ get-recognizers 1+ ' rec-scope -rot set-recognizers
     get-current >r also ' execute definitions previous ' execute
     r> set-current ;
 
+: ?search-prefix ( addr len wid/0 -- addr' len' )
+    ?dup-IF
+	wordlist-id @ 0 search-voc prefix-string
+    ELSE   simple-search-prefix  THEN ;
+
 : scope-search-prefix ( addr1 len1 -- addr2 len2 )
-    2dup ':' $split dup 0= IF  2drop 2drop  simple-search-prefix  EXIT  THEN
-    2swap find-name dup dup IF  >does-code  THEN  [ ' forth >does-code ]L =
-    IF
-	>body wordlist-id @ 0 search-voc prefix-string  2nip
-    ELSE
-	drop 2drop simple-search-prefix
-    THEN ;
+    0  BEGIN >r
+	2dup ':' $split nosplit? IF
+	    2drop 2drop r> ?search-prefix  EXIT
+	THEN
+	2swap r> find-name?in
+	dup  WHILE
+	    dup >does-code [ ' forth >does-code ]L =  WHILE
+		>body >r 2nip r>  REPEAT  THEN
+    nip nip ?search-prefix ;
 
 ' scope-search-prefix is search-prefix
