16b388ee484b3e92934c9ef691b03a5fe3512d4b
Bernd Paysan <bernd.paysan@gmx.de>
2018-12-14 17:58:54 +0100

    Improve coverage, improve tests on date.fs

--- date.fs
+++ date.fs
@@ -4,7 +4,7 @@
 : /mod3 ( n1 n2 -- r q )
     dup >r /mod dup 4 = IF  drop r@ + 3  THEN  rdrop ;
  
-: day2dow ( day -- dow )  2 + 7 mod ;
+: day2dow ( day -- dow )  1+ 7 mod ;
  
 \ julian calendar
 
@@ -19,7 +19,7 @@
     1- -rot
     2 - dup 0<= IF  12 + swap 1- swap  THEN
     153 5 */mod swap 0= >r 31 - swap
-    4 /mod swap 365 * swap >r + + r> swap r> + swap ;
+    4 /mod swap 365 * swap >r + + r> swap r> + 1+ swap ;
  
 : j-ymd2day ( y m d -- day )  (ymd2day) 1461 * + ;
  
@@ -30,11 +30,11 @@
  
 : day2ymd ( day -- y m d )
     dup gregorian >= IF
-	1 - 146097 /mod 400 * swap
+	2 - 146097 /mod 400 * swap
 	36524 /mod3 100 * rot + swap
 	j-day2ymd 2>r + 2r>
     ELSE
-	1 + j-day2ymd
+	j-day2ymd
     THEN ;
  
 : ymd2day ( y m d -- day )
@@ -48,6 +48,7 @@
     THEN ;
 
 [defined] t{ [defined] cov% and [IF]
+    t{ 0 3 1 ymd2day -> 0 }t
     13 1 [DO] t{ 2018 [I] 13 ymd2day day2ymd -> 2018 [I] 13 }t [LOOP] cov% cr
     32 1 [DO] t{ 2018 12 [I] ymd2day day2ymd -> 2018 12 [I] }t [LOOP] cov% cr
     t{ 2018 1 1 ymd2day 1- day2ymd -> 2017 12 31 }t
