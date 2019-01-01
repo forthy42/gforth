c6f3264d04b44a72c40712acd4b989f7c91876a7
Bernd Paysan <bernd.paysan@gmx.de>
2019-01-01 16:29:50 +0100

    Ask for permission on SD card

--- arch/arm/android/src/gnu/gforth/Gforth.java
+++ arch/arm/android/src/gnu/gforth/Gforth.java
@@ -481,18 +481,18 @@ public class Gforth
 
 	gforth=this;
 
-	if ((Context.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
+	if ((checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE)
 	     != PackageManager.PERMISSION_GRANTED)) {
-	    if (Context.shouldShowRequestPermissionRationale(this,
-								    Manifest.permission.READ_EXTERNAL_STORAGE)) {
+	    if (.shouldShowRequestPermissionRationale(this,
+						      Manifest.permission.READ_EXTERNAL_STORAGE)) {
 		Log.v(TAG, "Write to SD card needs explanation");
         // Show an explanation to the user *asynchronously* -- don't block
         // this thread waiting for the user's response! After the user
         // sees the explanation, try again to request the permission.
 	    }
-	    Context.requestPermissions(this,
-				       new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
-				       1);
+	    requestPermissions(this,
+			       new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+			       1);
 	}
 
 	progress=null;
