1a4f59e42838677450b8d3baad230bf287e73c32
Bernd Paysan <bernd.paysan@gmx.de>
2019-03-02 14:41:32 +0100

    Correct stringify of symbol version number

--- engine/symver.h
+++ engine/symver.h
@@ -1,6 +1,7 @@
 /* specific sym versions for a few symbols */
 
-#define STRINGIFY(x) #x
+#define TOSTRING(x) #x
+#define STRINGIFY(x) TOSTRING(x)
 #ifdef FORCE_SYMVER
 __asm__(".symver pow,pow@GLIBC_" STRINGIFY(FORCE_SYMVER));
 __asm__(".symver exp,exp@GLIBC_" STRINGIFY(FORCE_SYMVER));
