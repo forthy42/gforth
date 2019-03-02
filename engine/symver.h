0f2edf9639d9b74efb0cb5cd30fa8f9d5431fadd
Bernd Paysan <bernd.paysan@gmx.de>
2019-03-02 14:35:34 +0100

    Allow specifying forced symver directly

--- engine/symver.h
+++ engine/symver.h
@@ -1,7 +1,7 @@
 /* specific sym versions for a few symbols */
 
 #ifdef FORCE_SYMVER
-__asm__(".symver pow,pow@GLIBC_2.2.5");
-__asm__(".symver exp,exp@GLIBC_2.2.5");
-__asm__(".symver log,log@GLIBC_2.2.5");
+__asm__(".symver pow,pow@GLIBC_" #FORCE_SYMVER);
+__asm__(".symver exp,exp@GLIBC_" #FORCE_SYMVER);
+__asm__(".symver log,log@GLIBC_" #FORCE_SYMVER);
 #endif
