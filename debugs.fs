fdde987f11fb819d2c60a9109530674d66d834c9
Anton Ertl <anton@mips.complang.tuwien.ac.at>
2019-05-03 08:37:08 +0200

    revert to classic .S; ~~ now uses ...; ... does F.S and is unwind-protected

--- debugs.fs
+++ debugs.fs
@@ -36,7 +36,7 @@
 require source.fs
 
 defer printdebugdata ( -- ) \ gforth print-debug-data
-:noname .s fdepth IF cr ." F:" f.s THEN ; IS printdebugdata
+' .s is printdebugdata
 
 defer .debugline ( nfile nline -- ) \ gforth print-debug-line
 \G Print the source code location indicated by @var{nfile nline}, and
--- smartdots.fs
+++ smartdots.fs
@@ -66,11 +66,15 @@ Variable smart.s-skip
 	ELSE  .  THEN
     THEN ;
 
-' smart.s. is .s.
-
 : ... ( x1 .. xn -- x1 .. xn )
-    action-of .s. >r
-    ['] smart.s. IS .s. .s
-    r> IS .s. ;
+    action-of .s. {: old-.s. :}
+    try
+	['] smart.s. IS .s. .s
+    recover
+	old-.s. is .s. throw
+    endtry
+    fdepth IF
+	cr ." F:" f.s THEN ;
 
+' ... IS printdebugdata
 
