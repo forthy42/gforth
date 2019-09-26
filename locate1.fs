aa40f6855b73a3a0f19b6c7bd5fc8f6861755586
Anton Ertl <anton@mips.complang.tuwien.ac.at>
2019-09-26 18:09:47 +0200

    replaced LW with WW NW BW

--- locate1.fs
+++ locate1.fs
@@ -19,6 +19,7 @@
 
 $variable where-results
 \ addresses in WHERES that contain the results of the last WHERE
+variable where-index -1 where-index !
 
 variable included-file-buffers
 \ Bernd-array of c-addr u descriptors for read-only buffers that
@@ -102,7 +103,9 @@ variable included-file-buffers
 
 : l ( -- )
     \g Display line of source after compiler error or locate
-    cr located-view @ view>filename type  ': emit
+    located-view @ dup -1 = if
+        true [: ." no current location" ;] ?warning drop exit then
+    cr view>filename type  ': emit
     located-top @ dec.
     l1 ;
 
@@ -266,19 +269,35 @@ variable code-locations 0 code-locations !
 	then
     where-struct +loop ;
 
-: where ( "name" -- )
-    parse-name find-name dup 0= #-13 and throw [: over = ;] forwheres drop ;
+: where ( "name" -- ) \ gforth
+    parse-name find-name dup 0= #-13 and throw [: over = ;] forwheres
+    drop -1 where-index ! ;
 
-: wr-location ( u -- f )
-    \ locate-setup where result with index u; returns true iff successful
+: ww ( u -- ) \ gforth
+    \G The next @code{l} or @code{g} shows the @code{where} result
+    \G with index @i{u}
+    dup where-index !
     where-results $@ rot cells tuck u<= if
-        2drop false exit then
-    + @ 2@ name>string nip set-located-view true ;
+        2drop -1 0 -1 where-index !
+    else
+        + @ 2@ name>string nip then
+    set-located-view ;
 
-: lw ( u -- ) \ gforth
-    \G locate the @i{u}th where result
-    wr-location if
-        l then ;
+: nw ( -- ) \ gforth
+    \G The next @code{l} or @code{g} shows the next @code{where}
+    \G result; if the current one is the last one, after @code{nw}
+    \G there is no current one.  If there is no current one, after
+    \G @code{nw} the first one is the current one.
+    where-index @ 1+ ww ;
+
+: bw ( -- ) \ gforth
+    \G The next @code{l} or @code{g} shows the previous @code{where}
+    \G result; if the current one is the first one, after @code{bw}
+    \G there is no current one.    If there is no current one, after
+    \G @code{bw} the last one is the current one.
+    where-index @ dup 0< if
+        drop where-results $@ nip cell/ then
+    1- ww ;
 
 \ count word usage
 
