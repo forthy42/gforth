2fe1179570e89234eacc0b5cd2c4b77961e7e870
Anton Ertl <anton@mips.complang.tuwien.ac.at>
2019-09-29 19:10:02 +0200

    whereg files are now deleted

--- locate2.fs
+++ locate2.fs
@@ -6,15 +6,26 @@ require unix/libc.fs
 : plain-output ( xt -- )
     op-vector @ >r  plain-out  catch  r> op-vector !  throw ;
 
+2variable whereg-filename 0 0 whereg-filename 2!
+
+: delete-whereg ( -- )
+    \ delete whereg file
+    whereg-filename 2@ dup if
+	2dup delete-file throw drop free throw then ;
+
 : whereg ( "name" -- ) \ gforth
     \g Like @code{where}, but puts the output in the editor.  In
     \g Emacs, you can then use the compilation-mode commands
     \g (@pxref{Compilation Mode,,,emacs,GNU Emacs Manual}) to inspect
     \g specific occurences more closely.
-    "/tmp/gforth-whereg-XXXXXX\0" save-mem 1- over mkstemp dup ?ior fd>file
+    delete-whereg
+    "/tmp/gforth-whereg-XXXXXX\0" save-mem 1- 2dup whereg-filename 2!
+    over mkstemp dup ?ior fd>file
     [:  "-*- mode: compilation; default-directory: \"" type
 	here unused getcwd dup 0= ?errno-throw dup strlen type
 	"\" -*-" type
 	['] where plain-output
     ;] over outfile-execute close-file throw
     `edit-file-cmd >string-execute 2dup system drop free throw ;
+
+: bye delete-whereg bye ;
