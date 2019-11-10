70489f9173f619a9b3a69629ff223371fefad285
Bernd Paysan <bernd.paysan@gmx.de>
2019-11-10 20:01:43 +0100

    Sections still not working as desired

--- kernel/cond.fs
+++ kernel/cond.fs
@@ -417,7 +417,7 @@ defer adjust-locals-list ( wid -- )
 : int-[: ( -- flag colon-sys )
     wrap@ ['] (int-;]) :noname ;
 : comp-[: ( -- quotation-sys flag colon-sys )
-    [ has? peephole [IF] ] finish-code [ [THEN] ]
+    basic-block-end
     wrap@
     next-section
     locals-list @ locals-list off
