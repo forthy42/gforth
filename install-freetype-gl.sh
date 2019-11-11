6047823fc4ddfb8fe6a27f4e898e83bbf7a4273d
Bernd Paysan <bernd.paysan@gmx.de>
2019-11-12 00:10:52 +0100

    Don't need install-soil2.sh anymore

--- install-freetype-gl.sh
+++ install-freetype-gl.sh
@@ -16,8 +16,13 @@ in
 	TARGET=/usr/local/lib
 	;;
 esac
+case "$CC" in
+    *-m32*)
+	platform="--platform=gcc32"
+	;;
+esac
 
 git clone https://github.com/forthy42/soil2.git
-(cd soil2 && premake4 gmake && (cd make/$OS; make config=release) && (cp $(find . -name '*.a') $(find . -name '*.so') $TARGET) && (cp src/SOIL2/SOIL2.h /usr/include))
+(cd soil2 && premake4 $platform gmake && (cd make/$OS; make config=release) && (cp $(find . -name '*.a') $(find . -name '*.so') $TARGET) && (cp src/SOIL2/SOIL2.h /usr/include))
 git clone -b android https://github.com/forthy42/freetype-gl.git
 (cd freetype-gl && ./autogen.sh && ./configure "$@" && make && sudo make install)
