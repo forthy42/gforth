bb448bb49ba7bff0ef5246a07c071bbfffebd2c5
Bernd Paysan <bernd.paysan@gmx.de>
2019-12-05 15:02:43 +0100

    Thread-safe exif classes

--- minos2/jpeg-exif.fs
+++ minos2/jpeg-exif.fs
@@ -21,70 +21,95 @@
 \ Exif is FF E1 len<16>
 \ SOI is FF D8
 
-0 Value jpeg-fd
-0 Value exif-end
-0 Value exif-endian
-0 Value exif-start
+require mini-oof2.fs
+require user-object.fs
 
 s" Invalid JPEG file" exception Constant !!no-jpeg!!
 s" Exif exhausted"    exception Constant !!oo-exif!!
 s" Not an Exif chunk" exception Constant !!no-exif!!
 
-Defer exb
-Defer ex-seek? ( -- u )
-Defer ex-seek ( u -- )
-Defer exif-read ( u -- addr u )
-Defer exif>read ( addr u -- )
-Defer >exif-open ( addr u -- )
+user-o exif-o
+
+object uclass exif-o
+    cell uvar exif-start
+    cell uvar exif-end
+    cell uvar exif-endian
+    cell uvar jpeg-fd
+    cell uvar thumb-off
+    cell uvar thumb-len
+    cell uvar img-orient
+
+    umethod exb
+    umethod ex-seek? ( -- u )
+    umethod ex-seek ( u -- )
+    umethod exif-read ( u -- addr u )
+    umethod exif>read ( addr u -- )
+    umethod >exif-open ( addr u -- )
+end-class exif-class
+
+exif-class new dup constant file-exif-o exif-o !
 
 \ file variant
 
-: file-exb ( -- c ) jpeg-fd key-file ;
+: file-exb ( -- c ) jpeg-fd @ key-file ;
 : file-ex-seek? ( -- u )
-    jpeg-fd file-position throw drop ;
+    jpeg-fd @ file-position throw drop ;
 : file-ex-seek ( u -- )
-    0 jpeg-fd reposition-file throw ;
+    0 jpeg-fd @ reposition-file throw ;
 : file-exif-read ( n -- addr u )
-    pad swap jpeg-fd read-file throw
+    pad swap jpeg-fd @ read-file throw
     pad swap ;
 : file-exif>read ( addr u -- u' )
-    jpeg-fd read-file throw ;
+    jpeg-fd @ read-file throw ;
 : file>exif-open ( addr u -- )
-    r/o open-file throw to jpeg-fd ;
+    r/o open-file throw jpeg-fd ! ;
 
-: file-exif ( -- )
-    ['] file-exb is exb
-    ['] file-ex-seek? is ex-seek?
-    ['] file-ex-seek is ex-seek
-    ['] file-exif-read is exif-read
-    ['] file-exif>read is exif>read
-    ['] file>exif-open is >exif-open ;
-file-exif
+' file-exb is exb
+' file-ex-seek? is ex-seek?
+' file-ex-seek is ex-seek
+' file-exif-read is exif-read
+' file-exif>read is exif>read
+' file>exif-open is >exif-open
+
+: file-exif  file-exif-o exif-o ! ;
 
 \ memory variant
 
-2Variable exif-mem
-0 Value exif-pos
-: mem-exif/ ( -- addr u )  exif-mem 2@ exif-pos safe/string ;
+exif-class uclass exif-o
+    2 cells uvar exif-mem
+    cell uvar exif-pos
+end-class exif-mem-class
+
+exif-mem-class new dup Constant exif-mem-o exif-o !
+
+: mem-exif/ ( -- addr u )  exif-mem 2@ exif-pos @ safe/string ;
 : mem-exb ( -- c )
-    mem-exif/ drop c@  1 +to exif-pos ;
+    mem-exif/ drop c@  1 exif-pos +! ;
+: mem-ex-seek? ( -- u )
+    exif-pos @ ;
 : mem-ex-seek ( u -- )
-    to exif-pos ;
+    exif-pos ! ;
 : mem-exif-read ( u -- addr u )
     >r mem-exif/ r@ umin pad swap 2dup 2>r move 2r>
-    r> +to exif-pos ;
+    r> exif-pos +! ;
 : mem-exif>read ( addr u -- u' )
-    2>r mem-exif/ r> umin dup +to exif-pos r> swap dup >r move r> ;
+    2>r mem-exif/ r> umin dup exif-pos +! r> swap dup >r move r> ;
 : mem>exif-open ( addr u -- )
-    exif-mem 2!  0 to exif-pos ;
+    exif-mem 2!  0 exif-pos ! ;
+
+' mem-exb is exb
+' mem-ex-seek? is ex-seek?
+' mem-ex-seek is ex-seek
+' mem-exif-read is exif-read
+' mem-exif>read is exif>read
+' mem>exif-open is >exif-open
 
 : mem-exif ( -- )
-    ['] mem-exb is exb
-    ['] exif-pos is ex-seek?
-    ['] mem-ex-seek is ex-seek
-    ['] mem-exif-read is exif-read
-    ['] mem-exif>read is exif>read
-    ['] mem>exif-open is >exif-open ;
+    exif-mem-class new exif-o ! ;
+: exif> ( -- )
+    exif-o @ .dispose file-exif ;
+
+exif>
 
 : jpeg+seek ( n -- )  ex-seek? + ex-seek ;
 
@@ -103,38 +128,38 @@ file-exif
     read-len ;
 
 : >exif-st ( -- flag )
-    ?soi search-exif  dup 0= ?EXIT  ex-seek? + to exif-end  true ;
+    ?soi search-exif  dup 0= ?EXIT  ex-seek? + exif-end !  true ;
 : >exif ( addr u -- flag )
     >exif-open  >exif-st ;
 
 \ exif tags
 
-: exif-seek ( n -- )  exif-start + ex-seek ;
+: exif-seek ( n -- )  exif-start @ + ex-seek ;
 
-: exif-read-at ( n offset -- addr u )
-    ex-seek? >r exif-seek exif-read  r> ex-seek ;
+: exif-read-at ( offset n -- addr u )
+    ex-seek? >r swap exif-seek exif-read  r> ex-seek ;
 
-: exif-slurp ( u offset -- addr u )
-    ex-seek? >r exif-seek >r
+: exif-slurp ( offset u -- addr u )
+    ex-seek? >r >r exif-seek
     r@ allocate throw dup r> exif>read
     r> ex-seek ;
 
-: ex>< ( n1 n2 -- n3 n4 )  exif-endian IF  swap  THEN ;
+: ex>< ( n1 n2 -- n3 n4 )  exif-endian @ IF  swap  THEN ;
 : exw ( -- word )
     exb exb ex><  8 lshift or ;
 : exl ( -- long )
     exw exw ex>< 16 lshift or ;
 
 : >exif-start ( -- )
-    ex-seek? to exif-start ;
+    ex-seek? exif-start ! ;
 
 : ?exif ( -- )
     6 exif-read "Exif\0\0" str= 0= IF  !!no-exif!! throw  THEN
     >exif-start
     8 exif-read 2dup "II*\0\10\0\0\0" str= IF
-	2drop false to exif-endian  EXIT  THEN
+	2drop exif-endian off  EXIT  THEN
     "MM\0*\0\0\0\10" str= IF
-	true to exif-endian  EXIT  THEN
+	exif-endian on  EXIT  THEN
     !!no-exif!! throw ;
 
 \ read and print exif information
@@ -147,7 +172,7 @@ DOES> + c@ ;
     cmd hex. type hex. len hex.
     type exif-sizes len * { size }
     size 4 > IF
-	cr size offset exif-read-at $100 umin dump
+	cr offset size exif-read-at $100 umin dump
     ELSE
 	offset hex. cr
     THEN ;
@@ -160,29 +185,26 @@ DOES> + c@ ;
 
 \ search for thumbnail image
 
-0 Value thumb-off
-0 Value thumb-len
-0 Value img-orient
-
 : >thumb ( -- )
     exw 0 ?DO
 	exw exw exl { cmd type len }
 	exl case cmd
-	    $112 of  to img-orient  endof
-	    $201 of  to thumb-off   endof
-	    $202 of  to thumb-len   endof
+	    $112 of  img-orient !  endof
+	    $201 of  thumb-off  !  endof
+	    $202 of  thumb-len  !  endof
 	    nip
 	endcase
     LOOP ;
 
 : >thumb-scan ( fn-addr u1 -- )
     >exif-open
-    0 to img-orient  0 to thumb-off  0 to thumb-len
+    img-orient off  thumb-off off  thumb-len off
     >exif-st IF  ?exif exw 12 * jpeg+seek exl exif-seek >thumb  THEN ;
 
 : exif-close ( -- )
-    jpeg-fd ?dup-IF   close-file 0 to jpeg-fd throw  THEN ;
+    jpeg-fd @ ?dup-IF   close-file jpeg-fd off throw
+    ELSE  exif-o @ .dispose  file-exif  THEN ;
 : thumbnail@ ( -- addr u )
-    thumb-len thumb-off exif-slurp ;
+    thumb-off @ thumb-len @ exif-slurp ;
 : >thumbnail ( fn-addr u1 -- jpeg-addr u2 )
     >thumb-scan thumbnail@ exif-close ;
