8f16b72430b5275486bf7eff7b91a4aebc449a2f
Bernd Paysan <bernd.paysan@gmx.de>
2020-01-27 22:43:37 +0100

    beef up random.fs

--- random.fs
+++ random.fs
@@ -23,23 +23,30 @@
     
     $10450405 Constant generator
     
-    : rnd  ( -- n )  seed @ generator um* drop 1+ dup seed ! ;
+    : rnd  ( -- x )  seed @ generator um* drop 1+ dup seed ! ;
     : seed-init ( -- )  ntime drop seed +!  rnd drop ;
+    : seed! ( x -- )  seed ! ;
 [ELSE]
     \ hash based generator, deterministic
     \ seed-init feeds entropy into the generator
     \ 2.2 times slower than lcg on 64 bit platform
     \ passed SmallCrush, Rabbit, Alphabit, FIPS-140-2
-    $10 buffer: rng-state
-    8 buffer: seed
+    user rng-state $10 cell- uallot drop
+    user seed $8 cell- uallot drop
 
-    : rnd ( -- n ) seed 8 0 rng-state hashkey2 rng-state 2@ xor ;
+    : rnd ( -- x )
+	\G generate a single-cell random number
+	seed 8 0 rng-state hashkey2 rng-state 2@ xor ;
     cell 8 = [IF]
 	: seed-init ( -- ) ntime drop seed ! rnd drop ;
     [ELSE]
 	: seed-init ( -- ) ntime seed 2! rnd drop ;
     [THEN]
+    : seed! ( x -- )
+	\G set seed to a specific value for deterministic random numbers
+	seed $8 erase rng-state $10 erase seed ! rnd drop ;
 [THEN]
 seed-init
+:noname defers 'cold seed-init ; is 'cold
 
 : random ( n -- 0..n-1 )  rnd um* nip ;
