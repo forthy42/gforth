c2475b088d47cfebe5dce923ce50b30645e90761
Anton Ertl <anton@mips.complang.tuwien.ac.at>
2020-01-28 17:34:32 +0100

    divspeed now also performs latency measurements and measures more operations

--- contrib/divspeed.fs
+++ contrib/divspeed.fs
@@ -6,89 +6,89 @@
 \ Revisions:
 \    2006-10-28  change DIVIDEND for 32-bit systems;
 \                add tests for */ and M*/  ae, km
-(
- In the development version of gforth, code has been added to test for division
- by zero in the division words. We want to measure the performance penalty
- introduced by these tests. Words to be checked are:
 
-   /
-   MOD
-   /MOD
-   */
-   */MOD
-   FM/MOD
-   SM/REM
-   UM/MOD
-   M*/
-)
+\ Mostly rewritten by Anton Ertl
 
 : ms@ ( -- u | return time in ms)  cputime d+ 1 1000 m*/ d>s ;
-: ?allot ( u -- a ) here swap allot ;
-: table ( v1 v2 ... vn n <name> -- | create a table of singles ) 
-        create dup cells ?allot over 1- cells + swap
-        0 ?do dup >r ! r> 1 cells - loop drop ;
+: .elapsed ( starttime -- ) ms@ #tab emit swap - 4 .r ."  ms" ;
 
 1 31 lshift 1- constant  DIVIDEND
+100000 constant DIVISOR
 DIVIDEND negate s>d 2constant D_DIVIDEND
 100000000     constant  N
+create AB-operands DIVIDEND DIVISOR 2,
+create CEF-operand1 D_DIVIDEND 2,
+create CDE-operand2 DIVISOR ,
+create D-operand1 DIVIDEND s>d 2,
+create F-operand2 1 DIVISOR 2,
 
-\ Some helpful macros
-variable xt
-s" :noname [ xt @ >name name>string ] 2literal type ms@" 2constant s1
+: '. ( "op" -- xt )
+    parse-name 2dup ]] sliteral cr type [[ find-name name>interpret ;
     
-: get-xt ( "op" -- ) bl word find 0= ABORT" Unknown word!" xt ! ;
-
 : testA ( "op" -- )
-    get-xt s1 evaluate
-    s" DIVIDEND  N 1 DO dup I" evaluate xt @ compile,
-    s" drop LOOP drop ;" evaluate ; immediate
+    '. >r ]]
+    ms@ DIVIDEND  N 1 DO dup I [[ r@ compile, ]] drop LOOP drop .elapsed
+    ms@ AB-operands N 1 DO 2@ [[ r@ compile, AB-operands dup 2@ r@ execute - ]] literal + LOOP drop .elapsed
+    [[ rdrop ; immediate
 
 : testB ( "op" -- )
-    get-xt s1 evaluate
-    s" DIVIDEND  N 1 DO dup I" evaluate xt @ compile,
-    s" 2drop LOOP drop ;" evaluate ; immediate
+    '. >r ]]
+    ms@ DIVIDEND  N 1 DO dup I [[ r@ compile, ]] 2drop LOOP drop .elapsed
+    ms@ AB-operands N 1 DO 2@ [[ r@ compile, AB-operands dup 2@ r@ execute + - ]] + literal + LOOP drop .elapsed
+    [[ rdrop ; immediate
  
 : testC ( "op" -- )
-    get-xt s1 evaluate
-    s" D_DIVIDEND N 1 DO  2dup I" evaluate xt @ compile,
-    s" 2drop LOOP 2drop ;" evaluate ; immediate
+    '. >r ]]
+    ms@ D_DIVIDEND  N 1 DO 2dup I [[ r@ compile, ]] 2drop LOOP 2drop .elapsed
+    ms@ CEF-operand1 N 1 DO 2@ CDE-operand2 @ [[ r@ compile, CEF-operand1 dup 2@ CDE-operand2 @ r@ execute + - ]] + literal + LOOP drop .elapsed
+    [[ rdrop ; immediate
 
 : testD ( "op" -- )
-    get-xt s1 evaluate
-    s" DIVIDEND s>d N 1 DO  2dup I" evaluate xt @ compile,
-    s" 2drop LOOP 2drop ;" evaluate ; immediate
+    '. >r ]]
+    ms@ DIVIDEND s>d N 1 DO 2dup I [[ r@ compile, ]] 2drop LOOP 2drop .elapsed
+    ms@ D-operand1 N 1 DO 2@ CDE-operand2 @ [[ r@ compile, D-operand1 dup 2@ CDE-operand2 @ r@ execute + - ]] + literal + LOOP drop .elapsed
+    [[ rdrop ; immediate
 
 : testE ( "op" -- )
-    get-xt s1 evaluate
-    s" D_DIVIDEND N 1 DO  2dup I" evaluate xt @ compile,
-    s" drop LOOP 2drop ;" evaluate ; immediate
+    '. >r ]]
+    ms@ D_DIVIDEND N 1 DO 2dup I [[ r@ compile, ]] drop LOOP 2drop .elapsed
+    ms@ CEF-operand1 N 1 DO 2@ CDE-operand2 @ [[ r@ compile, CEF-operand1 dup 2@ CDE-operand2 @ r@ execute - ]] literal + LOOP drop .elapsed
+    [[ rdrop ; immediate
 
 : testF ( "op" -- )
-    get-xt s1 evaluate
-    s" D_DIVIDEND N 1 DO  2dup 1 I" evaluate xt @ compile,
-    s" 2drop LOOP 2drop ;" evaluate ; immediate
+    '. >r ]]
+    ms@ D_DIVIDEND  N 1 DO 2dup 1 I [[ r@ compile, ]] 2drop LOOP 2drop .elapsed
+    ms@ CEF-operand1 N 1 DO 2@ F-operand2 2@ [[ r@ compile, CEF-operand1 dup 2@ F-operand2 2@ r@ execute + - ]] + literal + LOOP drop .elapsed
+    [[ rdrop ; immediate
 
 \ The tests return the start time in ms
 
-testA  /
-testA  MOD
-testB  /MOD
-testA  U/
-testA  UMOD
-testB  U/MOD
-testE  */
-testC  */MOD
-testC  FM/MOD
-testC  SM/REM
-testD  UM/MOD   ( testC causes overflow for UM/MOD )
-testF  M*/
-12 table tests
-
-: .elapsed ( starttime -- ) ms@ 9 emit swap - 4 .r ."  ms" ;
-
-: run-tests
-    cr ." Speed Tests:"
-    12 0 DO  cr I cells tests + @ execute .elapsed LOOP cr ;
-
+: run-tests ( -- )
+    cr ." warmup"
+    testA  U/
+    cr ."         thruput latency"
+    \ testA  /
+    testA  /S
+    testA  /F
+    testA  U/
+    \ testA  MOD
+    testA  MODS
+    testA  MODF
+    testA  UMOD
+    \ testB  /MOD
+    testB  /MODS
+    testB  /MODF
+    testB  U/MOD
+    \ testE  */
+    testE  */S
+    testE  */F
+    \ testC  */MOD
+    testC  */MODS
+    testC  */MODF
+    testC  FM/MOD
+    testC  SM/REM
+    testD  UM/MOD   ( testC causes overflow for UM/MOD )
+    testF  M*/
+    cr ;
 
 run-tests
