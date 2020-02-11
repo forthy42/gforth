7b0fec5453b1affb9431a1c09c3d6c56e108cafb
Bernd Paysan <bernd.paysan@gmx.de>
2020-02-11 17:34:49 +0100

    Don't build SOIL on MacOS

--- install-freetype-gl.sh
+++ install-freetype-gl.sh
@@ -22,7 +22,12 @@ case "$CC" in
 	;;
 esac
 
-git clone https://github.com/forthy42/soil2.git
-(cd soil2 && premake4 $platform gmake && (cd make/$OS; make config=release) && (cp $(find . -name '*.a') $(find . -name '*.so') $TARGET) && (cp src/SOIL2/SOIL2.h /usr/include))
+# Sorry, SOIL2 on Darwin is meh
+if [ `uname` != "Darwin" ]
+then
+    git clone https://github.com/forthy42/soil2.git
+    (cd soil2 && premake4 $platform gmake && (cd make/$OS; make config=release) && (cp $(find . -name '*.a') $(find . -name '*.so') $TARGET) && (cp src/SOIL2/SOIL2.h /usr/include))
+fi
+
 git clone -b android https://github.com/forthy42/freetype-gl.git
 (cd freetype-gl && ./autogen.sh && ./configure "$@" && make && sudo make install)
