06476dd349f1a3474f197538d9127b844fa015a2
Bernd Paysan <bernd.paysan@gmx.de>
2020-04-15 00:16:05 +0200

    Add more libraries to build more bindings

--- install-deps.sh
+++ install-deps.sh
@@ -19,6 +19,9 @@ install_linux() {
   sudo apt-get -y install libfreetype6-dev
   sudo apt-get -y install libgstreamer1.0-dev
   sudo apt-get -y install libgstreamer-plugins-base1.0-dev
+  sudo apt-get -y install libpulse-dev
+  sudo apt-get -y install libopus-dev
+  sudo apt-get -y install libavcodec-dev libavutil-dev
   if [ `uname -m`$M32 = x86_64-m32 ]; then
     sudo apt-get -y --fix-missing install gcc-multilib
     sudo apt-get -y install libx11-dev:i386
