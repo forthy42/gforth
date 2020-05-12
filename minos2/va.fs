2229ad26aa7eae745de3ad047071d802cbdca0d2
Bernd Paysan <bernd.paysan@gmx.de>
2020-05-12 21:51:52 +0200

    A bit more on vaapi bindings

--- minos2/va.fs
+++ minos2/va.fs
@@ -19,7 +19,6 @@
 \ along with this program. If not, see http://www.gnu.org/licenses/.
 
 require unix/va.fs
-require unix/va-x11.fs
 require unix/va-glx.fs
 
 get-current va also definitions
@@ -41,12 +40,53 @@ $100 buffer: va-error$
 
 : va-display ( dpy -- )
     { | w^ major w^ minor }
-    vaGetDisplay to va-dpy
+    vaGetDisplayGLX to va-dpy
     va-dpy major minor vaInitialize ?va-ior
     va( ." VA-API version: " major l@ 0 .r '.' emit minor l@ 0 .r cr
     dup vaQueryVendorString type cr )
     va-dpy vaMaxNumEntrypoints sfloats allocate throw to profile-list
-    va-dpy profile-list addr profiles# vaQueryConfigProfiles ?va-ior
-;
+    va-dpy profile-list addr profiles# vaQueryConfigProfiles ?va-ior ;
+
+' VAProfileNone , here
+' VAProfileMPEG2Simple ,
+' VAProfileMPEG2Main ,
+' VAProfileMPEG4Simple ,
+' VAProfileMPEG4AdvancedSimple ,
+' VAProfileMPEG4Main ,
+' VAProfileH264Baseline ,
+' VAProfileH264Main ,
+' VAProfileH264High ,
+' VAProfileVC1Simple ,
+' VAProfileVC1Main ,
+' VAProfileVC1Advanced ,
+' VAProfileH263Baseline ,
+' VAProfileJPEGBaseline ,
+' VAProfileH264ConstrainedBaseline ,
+' VAProfileVP8Version0_3 ,
+' VAProfileH264MultiviewHigh ,
+' VAProfileH264StereoHigh ,
+' VAProfileHEVCMain ,
+' VAProfileHEVCMain10 ,
+' VAProfileVP9Profile0 ,
+' VAProfileVP9Profile1 ,
+' VAProfileVP9Profile2 ,
+' VAProfileVP9Profile3 ,
+' VAProfileHEVCMain12 ,
+' VAProfileHEVCMain422_10 ,
+' VAProfileHEVCMain422_12 ,
+' VAProfileHEVCMain444 ,
+' VAProfileHEVCMain444_10 ,
+' VAProfileHEVCMain444_12 ,
+' VAProfileHEVCSccMain ,
+' VAProfileHEVCSccMain10 ,
+' VAProfileHEVCSccMain444 ,
+' VAProfileAV1Profile0 ,
+' VAProfileAV1Profile1 ,
+Constant profile-names
+
+: .profiles ( -- )
+    profile-list profiles# sfloats bounds U+DO
+	I sl@ cells profile-names + @ .name
+    1 sfloats  +LOOP ;
 
 previous set-current
--- unix/va-drm.i
+++ unix/va-drm.i
@@ -3,6 +3,7 @@
 %insert("include")
 %{
 #include <va/va_drm.h>
+#include <va/va_drmcommon.h>
 %}
 
 %apply long long { int64_t }
@@ -11,3 +12,4 @@
 // exec: sed -e 's/^c-library/get-current also [IFDEF] va va [THEN] definitions``c-library/g' -e 's/^end-c-library/end-c-library`previous set-current/g' -e 's/va_drm"/va-drm"/g' | tr '`' '\n'
 
 %include <va/va_drm.h>
+%include <va/va_drmcommon.h>
