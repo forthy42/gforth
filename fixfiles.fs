7e0a0e61d89ed1bca2e17e51e39ad6c31c4793e9
Bernd Paysan <bernd.paysan@gmx.de>
2020-08-21 16:02:56 +0200

    Print all fixed files for debugging

--- fixfiles.fs
+++ fixfiles.fs
@@ -19,4 +19,4 @@
 \ along with this program. If not, see http://www.gnu.org/licenses/.
 
 s" GFORTHDESTDIR" getenv ." Fix prefix: '" 2dup type ." '" cr
-included-files $[]# 0 [?DO]  [I] included-files $[]@ over [IF] open-fpath-file 0= [IF] rot close-file throw 2over 2over 2swap string-prefix? [IF] 2 pick /string compact-filename [THEN] [I] included-files $[] off [I] included-files $[]! [THEN] [ELSE] 2drop [THEN] [LOOP] 2drop
+included-files $[]# 0 [?DO]  [I] included-files $[]@ ." Fix file: " 2dup type over [IF] open-fpath-file 0= [IF] rot close-file throw 2over 2over 2swap string-prefix? [IF] 2 pick /string compact-filename [THEN] ."  with " 2dup type cr [I] included-files $[] off [I] included-files $[]! [THEN] [ELSE] 2drop [THEN] [LOOP] 2drop
