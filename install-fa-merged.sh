f2135f608fd25aa2ec6fab60068ec6d8f703cbd6
Bernd Paysan <bernd.paysan@gmx.de>
2020-09-14 15:35:08 +0200

    add installing fa-merged shell script

---
+++ install-fa-merged.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+wget -c https://github.com/forthy42/Font-Awesome/raw/master/webfonts/fa-merged-900.ttf
+sudo install -vDm 755 fa-merged-900.ttf /usr/share/fonts/truetype/emoji/fa-merged-900.ttf
