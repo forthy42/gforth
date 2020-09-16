313c0109e36c17d2d351694d7f52713a09b2fd28
Bernd Paysan <bernd.paysan@gmx.de>
2020-09-16 19:17:29 +0200

    Factor out check for rec-nt

--- rec-tick.fs
+++ rec-tick.fs
@@ -22,8 +22,8 @@
 [IFUNDEF] ?rec-nt
     : ?rec-nt ( addr u -- xt true / something 0 )
 	sp@ >in @ 2>r
-	forth-recognizer recognize rectype-nt =
-	2r> 2over 2>r >in ! sp!
+	forth-recognizer recognize rectype-nt = dup
+	if  2r> 2over  else  2r> #0.  then  2>r >in ! sp!
 	2drop 2r> ;
 [THEN]
 
