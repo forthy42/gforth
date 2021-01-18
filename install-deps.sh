5282f0d09fbcfaf16f068f61b03f4b309074aa5d
Bernd Paysan <bernd.paysan@gmx.de>
2021-01-18 21:49:58 +0100

    Add libstb-dev as dependency

--- install-deps.sh
+++ install-deps.sh
@@ -23,6 +23,7 @@ install_linux() {
   sudo apt-get -y install libopus-dev
   sudo apt-get -y install libva-dev
   sudo apt-get -y install libavcodec-dev libavutil-dev
+  sudo apt-get -y install libstb-dev
   if [ `uname -m`$M32 = x86_64-m32 ]; then
     sudo apt-get -y --fix-missing install gcc-multilib
     sudo apt-get -y install libx11-dev:i386
