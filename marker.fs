a533fbe649f269736909c8da91781fa36e5ca4e9
Anton Ertl <anton@mips.complang.tuwien.ac.at>
2021-08-07 18:24:39 +0200

    moved marker to its own file

---
+++ marker.fs
@@ -0,0 +1,99 @@
+\ marker                                               18dec94py
+
+\ Authors: Anton Ertl, Bernd Paysan, Neal Crook, Jens Wilke
+\ Copyright (C) 1995,1998,2000,2003,2005,2007,2009,2010,2011,2013,2014,2016,2017,2018,2019,2020 Free Software Foundation, Inc.
+
+\ This file is part of Gforth.
+
+\ Gforth is free software; you can redistribute it and/or
+\ modify it under the terms of the GNU General Public License
+\ as published by the Free Software Foundation, either version 3
+\ of the License, or (at your option) any later version.
+
+\ This program is distributed in the hope that it will be useful,
+\ but WITHOUT ANY WARRANTY; without even the implied warranty of
+\ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+\ GNU General Public License for more details.
+
+\ You should have received a copy of the GNU General Public License
+\ along with this program. If not, see http://www.gnu.org/licenses/.
+
+
+\ Marker creates a mark that is removed (including everything 
+\ defined afterwards) when executing the mark.
+
+: included-files-mark ( -- u )
+    included-files $@len ;
+
+\ hmm, most of the saving appears to be pretty unnecessary: we could
+\ derive the wordlists and the words that have to be kept from the
+\ saved value of dp value. - anton
+
+: marker, ( -- mark )
+    here
+    included-files-mark ,
+    dup A, \ here
+    voclink @ A, \ vocabulary list start
+    \ for all wordlists, remember wordlist-id (the linked list)
+    voclink
+    BEGIN
+	@ dup
+    WHILE
+	dup 0 wordlist-link - wordlist-id @ A,
+    REPEAT
+    drop
+    \ remember udp
+    udp @ ,
+    \ remember vtable-list
+    vtable-list @ ,
+    \ remember dyncode-ptr
+    here ['] noop , compile-prim1 finish-code ;
+
+: marker! ( mark -- )
+    \ reset included files count; resize will happen on next add-included-file
+    dup @ dup >r included-files $@ r> /string bounds +DO  I $off  cell +LOOP
+    included-files $!len cell+
+    \ rest of marker!
+    dup @ swap cell+ ( here rest-of-marker )
+    dup @ voclink ! cell+
+    \ restore wordlists to former words
+    voclink
+    BEGIN
+	@ dup 
+    WHILE
+	over @ over 0 wordlist-link - wordlist-id !
+	cell under+
+    REPEAT
+    drop
+    \ rehash wordlists to remove forgotten words
+    \ why don't we do this in a single step? - anton
+    voclink
+    BEGIN
+	@ dup
+    WHILE
+	dup 0 wordlist-link - initwl
+    REPEAT
+    drop
+    \ restore udp and dp
+[IFDEF] forget-dyncode
+    dup cell+ @ forget-dyncode drop
+[THEN]
+    dup @ udp !  cell+ @ vtable-list !  ->here
+    \ clean up vocabulary stack
+    0 ['] search-order >body $@ bounds
+    U+DO
+	I @ dup here u>
+	IF  drop  ELSE  swap 1+  THEN
+    cell +LOOP
+    dup 0= or set-order \ -1 set-order if order is empty
+    get-current here > IF
+	forth-wordlist set-current
+    THEN ;
+
+: marker ( "<spaces> name" -- ) \ core-ext
+    \G Create a definition, @i{name} (called a @i{mark}) whose
+    \G execution semantics are to remove itself and everything 
+    \G defined after it.
+    marker, Create A,
+DOES> ( -- )
+    @ marker! ;
