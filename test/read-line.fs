b3d1f5e00d2d488bcf3fad929b016e93719af870
Anton Ertl <anton@mips.complang.tuwien.ac.at>
2021-10-23 16:43:23 +0200

    a test of (READ-LINE)

---
+++ test/read-line.fs
@@ -0,0 +1,24 @@
+\ regression test of (read-line)
+
+create buf 16 allot
+
+: hex.r ( u1 u2 -- )
+    ['] u.r #16 base-execute ;
+
+: show-line ( addr u -- )
+    bounds ?do i c@ 2 hex.r space loop ;
+
+
+: test ( -- )
+    7 2 do
+        cr cr i . cr
+        s" test/read-line.input" r/o open-file throw { f }
+        begin
+            buf 16 erase
+            buf 1+ i f (read-line) throw cr . tuck . .
+            cr buf 16 show-line cr
+        0= until
+    loop ;
+
+test bye
+       
