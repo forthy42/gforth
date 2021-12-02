c535bdbe661989064d262a938fb21c5399ef2d28
Bernd Paysan <bernd.paysan@gmx.de>
2021-12-02 13:18:39 +0100

    Work on Docker containers (including one for SWIG)

--- docker/runner/build.sh
+++ docker/runner/build.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
+sudo docker build -f Dockerfile.swig -t forthy42/swig:latest .
 sudo docker build -t forthy42/gforth:latest .
 sudo docker build -f Dockerfile.gui -t forthy42/gforth-gui:latest .
 if [ "$1" != "nopush" ]
 then
+    docker push forthy42/swig:latest
     docker push forthy42/gforth:latest
     docker push forthy42/gforth-gui:latest
 fi
