fd87bd4907c416e0c411f74bc7a25428eec69ddc
Bernd Paysan <bernd.paysan@gmx.de>
2021-12-06 23:20:59 +0100

    Add swig flatpak build rule

--- flatpak/build.sh
+++ flatpak/build.sh
@@ -2,3 +2,4 @@
 
 flatpak-builder --user --install --force-clean build org.freedesktop.Sdk.Extension.stb.yml
 flatpak-builder --user --install --force-clean build org.gnu.gforth.yml
+flatpak-builder --user --install --force-clean build org.gnu.swig.yml
