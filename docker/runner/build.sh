43212db9d935c39bbe9dedf5e2ebb9c4127a6562
Bernd Paysan <bernd.paysan@gmx.de>
2021-12-09 16:40:10 +0100

    Split gforth gui into one with and without fonts

--- docker/runner/build.sh
+++ docker/runner/build.sh
@@ -3,9 +3,11 @@
 sudo docker build -f Dockerfile.swig -t forthy42/swig:latest .
 sudo docker build -t forthy42/gforth:latest .
 sudo docker build -f Dockerfile.gui -t forthy42/gforth-gui:latest .
+sudo docker build -f Dockerfile.gui+fonts -t forthy42/gforth-gui-fonts:latest .
 if [ "$1" != "nopush" ]
 then
     docker push forthy42/swig:latest
     docker push forthy42/gforth:latest
     docker push forthy42/gforth-gui:latest
+    docker push forthy42/gforth-gui-fonts:latest
 fi
