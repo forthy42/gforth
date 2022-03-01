fb7e053e662ea2509eb0631d77fe9dd840ae0c4c
Bernd Paysan <bernd.paysan@gmx.de>
2022-03-01 01:30:22 +0100

    Instruction decoder for risc-v disassembler works

--- arch/riscv/disasm.fs
+++ arch/riscv/disasm.fs
@@ -25,8 +25,20 @@ disassembler also definitions
 : .lformat   ( addr -- )  $C u.r ." :" ;
 : tab #tab emit ;
 : .,  ',' emit ;
+: .(  '(' emit ;
+: .)  ')' emit ;
+: hex.4 ( inst -- )
+    0 <# # # # # #> type ;
+: hex.8 ( inst -- )
+    0 <# # # # # # # # # #> type ;
 
 \ register names
+
+: ..of ( compilation  -- of-sys ; run-time x1 x2 x3 -- x1 ) \ core-ext
+    \g if x1 is within x2 and x3, continue (dropping x2 and x3); otherwise,
+    \g leave x1 on the stack and jump behind @code{endof} or @code{contof}.
+    lits# 2 u>= IF  lits> lits> postpone dup >lits >lits
+    ELSE  ]] third -rot [[  THEN  ]] within ?of [[ ; immediate
 
 : .reg ( n -- )
     case
@@ -35,32 +47,75 @@ disassembler also definitions
 	2 of  ." sp"    endof
 	3 of  ." gp"    endof
 	4 of  ." tp"    endof
-	dup  5  8 within ?of   5 - 't' emit 0 .r  endof
-	dup  8 10 within ?of   8 - 's' emit 0 .r  endof
-	dup 10 18 within ?of  10 - 'a' emit 0 .r  endof
-	dup 18 28 within ?of  16 - 's' emit 0 .r  endof
+	 5  8 ..of   5 - 't' emit 0 .r  endof
+	 8 10 ..of   8 - 's' emit 0 .r  endof
+	10 18 ..of  10 - 'a' emit 0 .r  endof
+	18 28 ..of  16 - 's' emit 0 ['] .r #10 base-execute  endof
 	dup 25 - 't' emit 0 .r
+    endcase ;
+
+: .freg ( n -- )
+    case
+	 0  8 ..of  ." ft" 0 .r  endof
+	 8 10 ..of  8 - ." fs" 0 .r  endof
+	10 18 ..of  10 - ." fa" 0 .r  endof
+	18 28 ..of  16 - ." fs" 0 .r  endof
+	dup 20 - ." ft" 0 .r
     endcase ;
 
 \ print registers from instructions
 
-: .rd ( x -- )    7 rshift $1F and .reg ;
-: .rs1 ( x -- )  15 rshift $1F and .reg ;
-: .rs2 ( x -- )  20 rshift $1F and .reg ;
-: .imm-i ( x -- ) l>s 20 arshift . ;
-: .imm-s ( x -- ) l>s dup 20 arshift -$20 and swap 7 rshift $1F and or . ;
+: .rd ( x -- x )   dup  7 rshift $1F and .reg ;
+: .rs1 ( x -- x )  dup 15 rshift $1F and .reg ;
+: .rs2 ( x -- x )  dup 20 rshift $1F and .reg ;
+: imm-i ( x -- x imm ) dup l>s 20 arshift ;
+: imm-s ( x -- x imm ) dup l>s dup 20 arshift -$20 and swap 7 rshift $1F and or ;
+: imm-u ( x -- x imm ) dup l>s -$1000 and ;
+: imm-b ( x -- x imm )  imm-s
+    dup $000FF000 and >r
+    dup $00100000 and 9 rshift >r
+    dup $7FE00000 and 20 rshift >r
+    -$8000000     and
+    r> r> r> or or or ;
 
 \ different format outputs
 
-: r-type ( x -- ) .rd ., .rs1 ., .rs2 ;
+: r-type ( x -- ) .rd ., .rs1 ., .rs2 drop ;
 : i-type ( x -- ) ;
+: l-type ( x -- ) .rd ., imm-i 0 .r .( .rs1 .) drop ;
 : s-type ( x -- ) ;
 : b-type ( x -- ) ;
 : u-type ( x -- ) ;
 : j-type ( x -- ) ;
 
+: inst, ( match mask "operation" "name" -- )
+    , , ' , parse-name string, align ;
+
+Create inst-table16
+$0000 $0000 inst, hex.4 reserved
+
+Create inst-table32
+$00000003 $0000703F inst, l-type lb
+$00001003 $0000703F inst, l-type lh
+$00002003 $0000703F inst, l-type lw
+$00003003 $0000703F inst, l-type ld
+$00004003 $0000703F inst, l-type lbu
+$00005003 $0000703F inst, l-type lhu
+$00006003 $0000703F inst, l-type lwu
+$00000000 $00000000 inst, hex.8 reserved
+
+: .inst ( inst table -- ) swap >r
+    BEGIN  dup 2@ r@ and <>  WHILE
+	    3 cells + count + aligned  REPEAT
+    dup 3 cells + count type tab
+    r> swap 2 cells + perform ;
+
 : .code ( addr -- addr' )
-    dup l@ $8 u.r sfloat+ ;
+    dup c@ $3 and 3 = IF
+	dup l@ inst-table32 .inst sfloat+
+    ELSE
+	dup w@ inst-table16 .inst 2 +
+    THEN ;
 
 Forth definitions
 
