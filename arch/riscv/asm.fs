a662050f340a66c411563f878e546e5d3401378f
Bernd Paysan <bernd.paysan@gmx.de>
2022-03-09 02:28:02 +0100

    start of riscv assembler

--- arch/riscv/asm.fs
+++ arch/riscv/asm.fs
@@ -28,16 +28,16 @@ regs: ft0 ft1 ft2 ft3 ft4 ft5 ft6 ft7 fs0 fs1 fa0 fa1 fa2 fa3 fa4 fa5 fa6 fa7 fs
 regs: x0 x1 x2 x3 x4 x5 x6 x7 x8 x9 x10 x11 x12 x13 x14 x15 x16 x17 x18 x19 x20 x21 x22 x23 x24 x25 x26 x27 x28 x29 x30 x31
 regs: f0 f1 f2 f3 f4 f5 f6 f7 f8 f9 f10 f11 f12 f13 f14 f15 f16 f17 f18 f19 f20 f21 f22 f23 f24 f25 f26 f27 f28 f29 f30 f31
 
-: >rd ( destreg inst -- inst' ) swap $1F and 7 lshift or ;
-: >rs0 ( destreg inst -- inst' ) swap $1F and 2 lshift or ;
-: >rs1 ( destreg inst -- inst' ) swap $1F and 15 lshift or ;
-: >rs2 ( destreg inst -- inst' ) swap $1F and 20 lshift or ;
-: >rs3 ( destreg inst -- inst' ) swap $1F and 27 lshift or ;
+: >rd ( reg inst -- inst' ) swap $1F and 7 lshift or ;
+: >rs0 ( reg inst -- inst' ) swap $1F and 2 lshift or ;
+: >rs1 ( reg inst -- inst' ) swap $1F and 15 lshift or ;
+: >rs2 ( reg inst -- inst' ) swap $1F and 20 lshift or ;
+: >rs3 ( reg inst -- inst' ) swap $1F and 27 lshift or ;
 
 s" not in compact register range" exception no-reg
 : >c-reg ( reg -- c-reg )
     8 - dup $8 u>= no-reg and throw ;
-: >rd' ( destreg inst -- inst' ) swap >c-reg 2 lshift or ;
-: >rs1' ( destreg inst -- inst' ) swap >c-reg 7 lshift or ;
+: >rd' ( reg inst -- inst' ) swap >c-reg 2 lshift or ;
+: >rs1' ( reg inst -- inst' ) swap >c-reg 7 lshift or ;
 
 previous previous set-current
