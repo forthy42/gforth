34e5394cda90594733b7ca479c54e84d86c15574
Bernd Paysan <bernd.paysan@gmx.de>
2023-06-14 15:51:00 +0200

    Switch swig clone from Gerald to Bernd until patch is merged

--- docker/runner/build.sh
+++ docker/runner/build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-sudo docker build -f Dockerfile.swig -t forthy42/swig:latest .
-sudo docker build -t forthy42/gforth:latest .
-sudo docker build -f Dockerfile.gui -t forthy42/gforth-gui:latest .
-sudo docker build -f Dockerfile.gui+fonts -t forthy42/gforth-gui-fonts:latest .
+docker build -f Dockerfile.swig -t forthy42/swig:latest .
+docker build -t forthy42/gforth:latest .
+docker build -f Dockerfile.gui -t forthy42/gforth-gui:latest .
+docker build -f Dockerfile.gui+fonts -t forthy42/gforth-gui-fonts:latest .
 if [ "$1" != "nopush" ]
 then
     docker push forthy42/swig:latest
