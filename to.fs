89276f5026c075d0ce7cb4cfe1815ac19f760dc4
Bernd Paysan <bernd.paysan@gmx.de>
2023-07-05 23:41:09 +0200

    TO actually should never be compiled without a corresponding value

--- to.fs
+++ to.fs
@@ -18,13 +18,15 @@
 \ You should have received a copy of the GNU General Public License
 \ along with this program. If not, see http://www.gnu.org/licenses/.
 
+s" TO without arg" exception Constant to-error
+
 : to+addr: ( xt table "name" -- ) \ gforth
     \G create a to-method with ADDR enabled, where
     \G @var{xt} creates the address to access the field,
     \G and @var{table} contains the operators to store to it.
     Create , ,
     [: >r r@ cell+ perform r> @ to-!exec ;] set-does>
-    [: >r lits# 0= IF  r> does,  EXIT  THEN
+    [: >r lits# 0= IF  to-error throw  THEN
     r@ cell+ @ compile, r> @ to-!, ;] set-optimizer ;
 
 : to: ( xt table "name" -- ) \ gforth
@@ -33,5 +35,5 @@
     \G and @var{table} contains the operators to store to it.
     Create , ,
     [: !!?addr!! >r r@ cell+ perform r> @ to-!exec ;] set-does>
-    [: !!?addr!! >r lits# 0= IF  r> does,  EXIT  THEN
+    [: !!?addr!! >r lits# 0= IF  to-error throw  THEN
     r@ cell+ @ compile, r> @ to-!, ;] set-optimizer ;
