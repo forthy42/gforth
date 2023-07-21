1f4ba38a66546b9c6393cd4fce9904222366ea58
Bernd Paysan <bernd.paysan@gmx.de>
2023-07-22 01:10:39 +0200

    Temporarily install swig from forthy42 instead of GeraldWodni

--- install-swig.sh
+++ install-swig.sh
@@ -2,5 +2,5 @@
 which sudo || alias sudo=eval
 which nproc || alias nproc="echo 1"
 
-git clone https://github.com/GeraldWodni/swig.git
+git clone https://github.com/forthy42/swig.git
 (cd swig && ./autogen.sh && ./configure "$@" && make -j`nproc` && sudo make install)
