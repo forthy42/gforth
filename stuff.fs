f580fcebb750575018b48f6673f35f291325e6ad
Bernd Paysan <bernd.paysan@gmx.de>
2023-08-03 00:28:03 +0200

    Fix defers@

--- stuff.fs
+++ stuff.fs
@@ -183,7 +183,7 @@ UValue $? ( -- n ) \ gforth dollar-question
 ' translate-dnum AConstant rectype-dnum
 
 : defers@ ( xt -- xt' )
-    dup BEGIN  ['] defer@ catch 0= WHILE  nip  REPEAT  drop ;
+    BEGIN  dup ['] defer@ catch 0= WHILE  nip  REPEAT  drop ;
 : >rec-stack ( xt -- stack )
     dup >code-address docol: =
     IF  >body cell+ @ @  ELSE  >body  THEN ;
