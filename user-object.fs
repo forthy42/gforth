056ef63ee06d9287626893c8db2f1bae655bde95
Bernd Paysan <bernd.paysan@gmx.de>
2023-08-05 18:59:23 +0200

    Add copyright header to user-object.fs

--- user-object.fs
+++ user-object.fs
@@ -1,5 +1,23 @@
 \ Mini-OOF extension: current object in user variable  08jan13py
 
+\ Authors: Bernd Paysan
+\ Copyright (C) 2014,2015,2019 Free Software Foundation, Inc.
+
+\ This file is part of Gforth.
+
+\ Gforth is free software; you can redistribute it and/or
+\ modify it under the terms of the GNU General Public License
+\ as published by the Free Software Foundation, either version 3
+\ of the License, or (at your option) any later version.
+
+\ This program is distributed in the hope that it will be useful,
+\ but WITHOUT ANY WARRANTY; without even the implied warranty of
+\ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+\ GNU General Public License for more details.
+
+\ You should have received a copy of the GNU General Public License
+\ along with this program. If not, see http://www.gnu.org/licenses/.
+
 Variable class-o
 
 : user-o ( "name" -- )
