e55ee485cf76037ab0573bc4c76c0aa1ec960d68
Bernd Paysan <bernd.paysan@gmx.de>
2023-09-21 02:43:00 +0200

    Use s1 to test skip beyond end of safe/string

--- test/gforth.fs
+++ test/gforth.fs
@@ -62,7 +62,7 @@ TESTING safe/string
 [IFUNDEF] s1
     T{ :  s1 S" abcdefghijklmnopqrstuvwxyz" ; -> }T
 [THEN]
-t{ s" bla bla" 2dup 9 safe/string 2swap + 0 d= -> true }t
+t{ s1 2dup 29 safe/string 2swap + 0 d= -> true }t
 T{ s1  5 safe/string -> s1 SWAP 5 + SWAP 5 - }T
 T{ s1 10 safe/string -4 safe/string -> s1 10 safe/string }T
 T{ s1  0 safe/string -> s1 }T
