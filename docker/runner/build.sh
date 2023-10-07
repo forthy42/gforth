e89b2b629656a17ba825daefc9e4f63bfea16b2e
Bernd Paysan <bernd.paysan@gmx.de>
2023-10-07 19:39:05 +0200

    docker build now needs --network host for Gforth runner

--- docker/runner/build.sh
+++ docker/runner/build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-docker build -f Dockerfile.swig -t forthy42/swig:latest .
-docker build -t forthy42/gforth:latest .
-docker build -f Dockerfile.gui -t forthy42/gforth-gui:latest .
-docker build -f Dockerfile.gui+fonts -t forthy42/gforth-gui-fonts:latest .
+docker build --network host -f Dockerfile.swig -t forthy42/swig:latest .
+docker build --network host -t forthy42/gforth:latest .
+docker build --network host -f Dockerfile.gui -t forthy42/gforth-gui:latest .
+docker build --network host -f Dockerfile.gui+fonts -t forthy42/gforth-gui-fonts:latest .
 if [ "$1" != "nopush" ]
 then
     docker push forthy42/swig:latest
