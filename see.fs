d79461190c9da3e48a76305b788f079922c8dd57
Anton Ertl <anton@mips.complang.tuwien.ac.at>
2024-02-22 06:51:24 +0100

    replaced (/loop)# by lit (/loop)

--- see.fs
+++ see.fs
@@ -418,8 +418,8 @@ ACONSTANT MaxTable
 : id.-without ( addr -- addr )
     \ !! the stack effect cannot be correct
     \ prints a name without () and without -LP+!#, e.g. a (+LOOP) or (s")
-    dup cell- @threaded>name dup IF
-	dup ``(/loop)# = over ``(/loop)#-lp+!# = or if drop ``+loop then
+    dup cell- @ threaded>name dup IF
+	dup ``(/loop) = over ``(/loop)-lp+!# = or if drop ``+loop then
 	name>string over c@ '( = IF
 	    1 /string
 	THEN
@@ -603,11 +603,6 @@ ACONSTANT MaxTable
     THEN ( addr1 ) \ perverse stack effect of MoreBranchAddr?
     cell+ ;
 
-: c-loop# ( addr -- addr1 )
-    Display? if
-	dup @ c-. then
-    c-loop cell+ ;
-
 : c-do ( addr -- addr )
     Display? IF
 	dup BranchAddr? IF ( addr addr1 )
@@ -759,7 +754,7 @@ CREATE C-Table \ primitives map to code only
         	' (+loop) A,        ' c-loop A,
 [IFDEF] (s+loop) ' (s+loop) A,      ' c-loop A, [THEN]
 [IFDEF] (-loop) ' (-loop) A,        ' c-loop A, [THEN]
-[IFDEF] (/loop)# ' (/loop)# A,      ' c-loop# A, [THEN]
+[IFDEF] (/loop) ' (/loop) A,        ' c-loop A, [THEN]
         	' (next) A,         ' c-loop A,
         	' ;s A,             ' c-exit A,
 \ [IFDEF] (abort") ' (abort") A,      ' c-abort" A, [THEN]
