b1316db5052e983409dbf67dce411877a70fb350
Bernd Paysan <bernd.paysan@gmx.de>
2024-07-17 22:03:32 +0200

    use slice metadata key for startfile

--- arch/arm/android/src/gnu/gforth/Gforth.java
+++ arch/arm/android/src/gnu/gforth/Gforth.java
@@ -507,6 +507,10 @@ public class Gforth
                 if (ln != null) libname = ln;
                 String sf = ai.metaData.getString(META_DATA_STARTFILE);
                 if (sf != null) startfile = sf;
+		String sliceuri = ai.metaData.getString(SLICE_METADATA_KEY);
+		if ((sliceuri != null) && sliceuri.startsWith("file://"))
+		    startfile = sliceuri.substring(7);
+		}
             }
         } catch (PackageManager.NameNotFoundException e) {
             throw new RuntimeException("Error getting activity info", e);
