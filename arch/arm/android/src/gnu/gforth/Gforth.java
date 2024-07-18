6cb553027fd7524d54e9643953fde6a16be0e63a
Bernd Paysan <bernd.paysan@gmx.de>
2024-07-18 14:43:05 +0200

    Debug metaData

--- arch/arm/android/src/gnu/gforth/Gforth.java
+++ arch/arm/android/src/gnu/gforth/Gforth.java
@@ -32,7 +32,6 @@ import android.content.BroadcastReceiver;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.pm.ActivityInfo;
-import android.content.pm.ResolveInfo;
 import android.content.pm.PackageManager;
 import android.content.res.Configuration;
 import android.content.res.AssetFileDescriptor;
@@ -509,14 +508,9 @@ public class Gforth
 
 	try {
             ai = getPackageManager().getActivityInfo(intent.getComponent(), PackageManager.GET_META_DATA);
-	    ResolveInfo ri = getPackageManager().resolveActivity(intent,
-								 PackageManager.GET_META_DATA);
             if (ai.metaData != null) {
 		for (String key : ai.metaData.keySet()) {
-		    Log.v(TAG, "ai.metaData."+key+"="+ai.metaData.getString(key));
-		}
-		for (String key : ri.activityInfo.metaData.keySet()) {
-		    Log.v(TAG, "ri.metaData."+key+"="+ri.activityInfo.metaData.getString(key));
+		    Log.v(TAG, "metaData."+key+"="+ai.metaData.getString(key));
 		}
                 String ln = ai.metaData.getString(META_DATA_LIB_NAME);
                 if (ln != null) {
@@ -526,7 +520,7 @@ public class Gforth
                 if (sf != null) {
 		    startfile = sf;
 		}
-		String sliceuri = ri.activityInfo.metaData.getString(SliceManager.SLICE_METADATA_KEY);
+		String sliceuri = ai.metaData.getString(SliceManager.SLICE_METADATA_KEY);
 		if (sliceuri != null) {
 		    Log.v(TAG, "sliceuri="+sliceuri);
 		    if(sliceuri.startsWith("file://")) {
