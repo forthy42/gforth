2e3034f103902ce719d55fde06d5c65b88c25675
Bernd Paysan <bernd.paysan@gmx.de>
2024-08-18 12:55:29 +0200

    bug fixes in script.fs

--- script.fs
+++ script.fs
@@ -21,7 +21,6 @@
 : >system ( addr u -- ) cr system ;
 : system, slit, postpone >system ;
 ' >system ' system, ' slit, >postponer translate: translate-eval
-' translate-eval Constant rectype-eval \ gforth-obsolete
 
 : rec-shell ( addr u -- addr u' translate-eval | notfound )
     \G evaluate string + rest of command line
