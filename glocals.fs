2f599c0bed82ef885f579076d3b3d72e15b472be
Bernd Paysan <bernd.paysan@gmx.de>
2024-11-27 01:00:56 +0100

    Clean up lastnt in locals-headers when locals are reset

--- glocals.fs
+++ glocals.fs
@@ -617,7 +617,7 @@ is adjust-locals-list
 : locals-;-hook ( sys addr xt sys -- sys )
     ?struc
     deactivate-locals
-    ['] ->here locals-headers
+    [: ->here 0 lastnt ! ;] locals-headers
     DEFERS ;-hook ;
 
 \ THEN (another control flow from before joins the current one):
