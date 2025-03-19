047d02cbdc2d9dd8787ff164ef2076a5a4249115
Anton Ertl <anton@mips.complang.tuwien.ac.at>
2025-03-19 18:53:52 +0100

    renamed timespec2ns into gf_timespec2ns to avoid conflict with NETBSD macro

--- engine/forth.h
+++ engine/forth.h
@@ -556,7 +556,7 @@ char *cstr(Char *from, UCell size);
 char *tilde_cstr(Char *from, UCell size);
 Cell opencreate_file(char *s, Cell wfam, int flags, Cell *wiorp);
 DCell timeval2us(struct timeval *tvp);
-DCell timespec2ns(struct timespec *tvp);
+DCell gf_timespec2ns(struct timespec *tvp);
 void cmove(Char *c_from, Char *c_to, UCell u);
 void cmove_up(Char *c_from, Char *c_to, UCell u);
 Cell compare(Char *c_addr1, UCell u1, Char *c_addr2, UCell u2);
--- engine/support.c
+++ engine/support.c
@@ -223,7 +223,7 @@ DCell timeval2us(struct timeval *tvp)
 #endif
 }
 
-DCell timespec2ns(struct timespec *tvp)
+DCell gf_timespec2ns(struct timespec *tvp)
 {
 #ifndef BUGGY_LONG_LONG
   return (tvp->tv_sec*(DCell)1000000000LL)+tvp->tv_nsec;
