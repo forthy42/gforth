cd230352d72601c547b2eee4bf417662174c2787
Bernd Paysan <bernd.paysan@gmx.de>
2025-07-17 14:56:04 +0200

    Bump version number

--- docker/runner/build.sh
+++ docker/runner/build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+docker pull alpine:latest
 docker build --network host -f Dockerfile.swig -t forthy42/swig:latest .
 docker build --network host -t forthy42/gforth:latest .
 docker build --network host -f Dockerfile.gui -t forthy42/gforth-gui:latest .
