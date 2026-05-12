92d83351ea6ceff7e0a2ec1c4b993364c12d8c6d
Bernd Paysan <bernd.paysan@gmx.de>
2026-05-12 11:44:52 +0200

    Refactor marker to allow plug-ins for more marker stuff

--- marker.fs
+++ marker.fs
@@ -22,43 +22,25 @@
 \ Marker creates a mark that is removed (including everything 
 \ defined afterwards) when executing the mark.
 
+Defer marker, \ compile markers, chained
+Defer marker! \ restore markers, chained
+Defer marker>here \ set here
+
+: @+ ( addr1 -- addr2 x )
+    dup cell+ swap @ ;
+
 : included-files-mark ( -- u )
     included-files $@len ;
-
-\ sections-marker:
-\ #extra-sections
-\ size of sections
-\ section-dps (as many as given by size)
-
-: sections-marker, ( -- )
-    here drop
-    current-section @ ,
-    #extra-sections @ ,
-    sections $@ dup , cell mem+do
-	['] section-dp i @ section-execute @ ,
-	['] lastnt     i @ section-execute @ ,
-    loop ;
-
-: sections-marker! ( addr1 -- addr2 )
-    dup @ current-section ! set-section
-    cell+ #extra-sections @ over @ ?do
-	sections back> free throw loop
-    dup @ #extra-sections !
-    cell+ sections $@len over @ ?do
-	sections stack> free throw
-    cell +loop
-    assert( sections $@len over @ = )
-    cell+ sections $@ cell mem+do
-	dup @ ['] section-dp i @ section-execute ! cell+
-	dup @ ['] lastnt     i @ section-execute ! cell+
-    loop ;
 
 \ hmm, most of the saving appears to be pretty unnecessary: we could
 \ derive the wordlists and the words that have to be kept from the
 \ saved value of dp value. - anton
 
-: marker, ( -- mark )
-    here
+:is marker, ( -- mark )
+    here align
+    action-of marker! ,
+    action-of marker, ,
+    action-of marker>here ,
     included-files-mark ,
     dup A, \ here
     voclink @ A, \ vocabulary list start
@@ -76,16 +58,18 @@
     hm-list @ ,
     \ remember dyncode-ptr
     here ['] noop , cell "\x80" drop "\x00" drop compile-prims
-    sections-marker, \ here is stored and restored separately
 ;
 
-: marker! ( mark -- )
+:is marker! ( mark-addr -- here mark-addr' )
     \ reset included files count; resize will happen on next add-included-file
+    @+ is marker!
+    @+ is marker,
+    @+ is marker>here
     dup @ dup >r included-files $@ r> /string cell MEM+DO  I $free  LOOP
     included-files $!len cell+
     \ rest of marker!
-    dup @ swap cell+ ( here rest-of-marker )
-    dup @ voclink ! cell+
+    @+ swap ( here rest-of-marker )
+    @+ voclink !
     \ restore wordlists to former words
     voclink
     BEGIN
@@ -108,8 +92,48 @@
     dup @ udp !
     cell+ dup @ hm-list !
     cell+ [IFDEF] forget-dyncode dup forget-dyncode3 drop [then]
-    cell+ sections-marker!
-    drop
+    cell+ ;
+
+: marker ( "<spaces> name" -- ) \ core-ext
+    \G Create a definition, @i{name} (called a @i{mark}) whose
+    \G execution semantics are to remove itself and everything 
+    \G defined after it.
+    marker, Create A,
+DOES> ( -- )
+    @ aligned dup perform drop marker>here ;
+
+\ sections-marker:
+\ #extra-sections
+\ size of sections
+\ section-dps (as many as given by size)
+
+:is marker, ( -- )
+    defers marker,
+    current-section @ ,
+    #extra-sections @ ,
+    sections $@ dup , cell mem+do
+	['] section-dp i @ section-execute @ ,
+	['] lastnt     i @ section-execute @ ,
+    loop ;
+
+:is marker! ( addr1 -- addr2 )
+    defers marker!
+    @+ current-section ! set-section
+    #extra-sections @ over @ ?do
+	sections back> free throw loop
+    @+ #extra-sections !
+    sections $@len over @ ?do
+	sections stack> free throw
+    cell +loop
+    assert( sections $@len over @ = )
+    cell+ sections $@ cell mem+do
+	@+ ['] section-dp i @ section-execute !
+	@+ ['] lastnt     i @ section-execute !
+    loop ;
+
+\ finally set here and check for things depending on here
+
+:is marker>here ( here -- )
     ->here
     \ clean up vocabulary stack
     0 ['] search-order >body $@ cell MEM+DO
@@ -120,11 +144,3 @@
     get-current here > IF
 	forth-wordlist set-current
     THEN ;
-
-: marker ( "<spaces> name" -- ) \ core-ext
-    \G Create a definition, @i{name} (called a @i{mark}) whose
-    \G execution semantics are to remove itself and everything 
-    \G defined after it.
-    marker, Create A,
-DOES> ( -- )
-    @ marker! ;
