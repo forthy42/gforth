f8afe095282595764d4452951b7aa221f66fabca
Bernd Paysan <bernd.paysan@gmx.de>
2026-08-14 10:21:35 +0200

    Return flag on ?include-wayland so that [IF] and [THEN] are both visible in the source code and indentation works

--- unix/wayland-interfaces.fs
+++ unix/wayland-interfaces.fs
@@ -18,12 +18,12 @@
 \ You should have received a copy of the GNU General Public License
 \ along with this program. If not, see http://www.gnu.org/licenses/.
 
-: ?include-wayland ( "file" -- )
+: ?include-wayland ( "file" -- flag )
     parse-name 2dup [: ." wayland/" type ;] $tmp open-fpath-file 0=
     IF  2drop close-file throw
-	[: ." #include <" type ." >" cr ;] c-source-file-execute
+	[: ." #include <" type ." >" cr ;] c-source-file-execute true
     ELSE
-	." Skip " type cr  postpone [ELSE]
+	." Skip " type cr   false
     THEN ;
 
 c-value wl_display_interface &wl_display_interface -- a
@@ -48,72 +48,72 @@ c-value wl_output_interface &wl_output_interface -- a
 c-value wl_region_interface &wl_region_interface -- a
 c-value wl_subcompositor_interface &wl_subcompositor_interface -- a
 c-value wl_subsurface_interface &wl_subsurface_interface -- a
-?include-wayland text-input-unstable-v3.c
-c-value zwp_text_input_v3_interface &zwp_text_input_v3_interface -- a
-c-value zwp_text_input_manager_v3_interface &zwp_text_input_manager_v3_interface -- a
+?include-wayland text-input-unstable-v3.c [IF]
+    c-value zwp_text_input_v3_interface &zwp_text_input_v3_interface -- a
+    c-value zwp_text_input_manager_v3_interface &zwp_text_input_manager_v3_interface -- a
 [THEN]
-?include-wayland xdg-shell.c
-c-value xdg_wm_base_interface &xdg_wm_base_interface -- a
-c-value xdg_positioner_interface &xdg_positioner_interface -- a
-c-value xdg_surface_interface &xdg_surface_interface -- a
-c-value xdg_toplevel_interface &xdg_toplevel_interface -- a
-c-value xdg_popup_interface &xdg_popup_interface -- a
+?include-wayland xdg-shell.c [IF]
+    c-value xdg_wm_base_interface &xdg_wm_base_interface -- a
+    c-value xdg_positioner_interface &xdg_positioner_interface -- a
+    c-value xdg_surface_interface &xdg_surface_interface -- a
+    c-value xdg_toplevel_interface &xdg_toplevel_interface -- a
+    c-value xdg_popup_interface &xdg_popup_interface -- a
 [THEN]
-?include-wayland xdg-decoration-unstable-v1.c
-c-value zxdg_toplevel_decoration_v1_interface &zxdg_toplevel_decoration_v1_interface -- a
-c-value zxdg_decoration_manager_v1_interface &zxdg_decoration_manager_v1_interface -- a
+?include-wayland xdg-decoration-unstable-v1.c [IF]
+    c-value zxdg_toplevel_decoration_v1_interface &zxdg_toplevel_decoration_v1_interface -- a
+    c-value zxdg_decoration_manager_v1_interface &zxdg_decoration_manager_v1_interface -- a
 [THEN]
-?include-wayland primary-selection-unstable-v1.c
-c-value zwp_primary_selection_device_manager_v1_interface &zwp_primary_selection_device_manager_v1_interface -- a
-c-value zwp_primary_selection_device_v1_interface &zwp_primary_selection_device_v1_interface -- a
-c-value zwp_primary_selection_offer_v1_interface &zwp_primary_selection_offer_v1_interface -- a
-c-value zwp_primary_selection_source_v1_interface &zwp_primary_selection_source_v1_interface -- a
+?include-wayland primary-selection-unstable-v1.c [IF]
+    c-value zwp_primary_selection_device_manager_v1_interface &zwp_primary_selection_device_manager_v1_interface -- a
+    c-value zwp_primary_selection_device_v1_interface &zwp_primary_selection_device_v1_interface -- a
+    c-value zwp_primary_selection_offer_v1_interface &zwp_primary_selection_offer_v1_interface -- a
+    c-value zwp_primary_selection_source_v1_interface &zwp_primary_selection_source_v1_interface -- a
 [THEN]
-?include-wayland tablet-unstable-v2.c
-c-value zwp_tablet_manager_v2_interface &zwp_tablet_manager_v2_interface -- a
-c-value zwp_tablet_seat_v2_interface &zwp_tablet_seat_v2_interface -- a
-c-value zwp_tablet_tool_v2_interface &zwp_tablet_tool_v2_interface -- a
-c-value zwp_tablet_v2_interface &zwp_tablet_v2_interface -- a
-c-value zwp_tablet_pad_ring_v2_interface &zwp_tablet_pad_ring_v2_interface -- a
-c-value zwp_tablet_pad_strip_v2_interface &zwp_tablet_pad_strip_v2_interface -- a
-c-value zwp_tablet_pad_group_v2_interface &zwp_tablet_pad_group_v2_interface -- a
-c-value zwp_tablet_pad_v2_interface &zwp_tablet_pad_v2_interface -- a
+?include-wayland tablet-unstable-v2.c [IF]
+    c-value zwp_tablet_manager_v2_interface &zwp_tablet_manager_v2_interface -- a
+    c-value zwp_tablet_seat_v2_interface &zwp_tablet_seat_v2_interface -- a
+    c-value zwp_tablet_tool_v2_interface &zwp_tablet_tool_v2_interface -- a
+    c-value zwp_tablet_v2_interface &zwp_tablet_v2_interface -- a
+    c-value zwp_tablet_pad_ring_v2_interface &zwp_tablet_pad_ring_v2_interface -- a
+    c-value zwp_tablet_pad_strip_v2_interface &zwp_tablet_pad_strip_v2_interface -- a
+    c-value zwp_tablet_pad_group_v2_interface &zwp_tablet_pad_group_v2_interface -- a
+    c-value zwp_tablet_pad_v2_interface &zwp_tablet_pad_v2_interface -- a
 [THEN]
-?include-wayland fractional-scale-v1.c
-c-value wp_fractional_scale_manager_v1_interface &wp_fractional_scale_manager_v1_interface -- a
-c-value wp_fractional_scale_v1_interface &wp_fractional_scale_v1_interface -- a
+?include-wayland fractional-scale-v1.c [IF]
+    c-value wp_fractional_scale_manager_v1_interface &wp_fractional_scale_manager_v1_interface -- a
+    c-value wp_fractional_scale_v1_interface &wp_fractional_scale_v1_interface -- a
 [THEN]
-?include-wayland viewporter.c
-c-value wp_viewporter_interface &wp_viewporter_interface -- a
-c-value wp_viewport_interface &wp_viewport_interface -- a
+?include-wayland viewporter.c [IF]
+    c-value wp_viewporter_interface &wp_viewporter_interface -- a
+    c-value wp_viewport_interface &wp_viewport_interface -- a
 [THEN]
-?include-wayland xdg-output-unstable-v1.c
-c-value zxdg_output_manager_v1_interface &zxdg_output_manager_v1_interface -- a
+?include-wayland xdg-output-unstable-v1.c [IF]
+    c-value zxdg_output_manager_v1_interface &zxdg_output_manager_v1_interface -- a
 [THEN]
-?include-wayland xdg-activation-v1.c
-c-value xdg_activation_v1_interface &xdg_activation_v1_interface -- a
+?include-wayland xdg-activation-v1.c [IF]
+    c-value xdg_activation_v1_interface &xdg_activation_v1_interface -- a
 [THEN]
-?include-wayland cursor-shape-v1.c
-c-value wp_cursor_shape_manager_v1_interface &wp_cursor_shape_manager_v1_interface -- a
-c-value wp_cursor_shape_device_v1_interface &wp_cursor_shape_device_v1_interface -- a
+?include-wayland cursor-shape-v1.c [IF]
+    c-value wp_cursor_shape_manager_v1_interface &wp_cursor_shape_manager_v1_interface -- a
+    c-value wp_cursor_shape_device_v1_interface &wp_cursor_shape_device_v1_interface -- a
 [THEN]
-?include-wayland idle-inhibit-unstable-v1.c
-c-value zwp_idle_inhibit_manager_v1_interface &zwp_idle_inhibit_manager_v1_interface -- a
-c-value zwp_idle_inhibitor_v1_interface &zwp_idle_inhibitor_v1_interface -- a
+?include-wayland idle-inhibit-unstable-v1.c [IF]
+    c-value zwp_idle_inhibit_manager_v1_interface &zwp_idle_inhibit_manager_v1_interface -- a
+    c-value zwp_idle_inhibitor_v1_interface &zwp_idle_inhibitor_v1_interface -- a
 [THEN]
-?include-wayland tearing-control-v1.c
-c-value wp_tearing_control_manager_v1_interface &wp_tearing_control_manager_v1_interface -- a
-c-value wp_tearing_control_v1_interface &wp_tearing_control_v1_interface -- a
+?include-wayland tearing-control-v1.c [IF]
+    c-value wp_tearing_control_manager_v1_interface &wp_tearing_control_manager_v1_interface -- a
+    c-value wp_tearing_control_v1_interface &wp_tearing_control_v1_interface -- a
 [THEN]
-?include-wayland color-management-v1.c
-c-value wp_color_manager_v1_interface &wp_color_manager_v1_interface -- a
-c-value wp_color_management_output_v1_interface &wp_color_management_output_v1_interface -- a
-c-value wp_color_management_surface_v1_interface &wp_color_management_surface_v1_interface -- a
-c-value wp_color_management_surface_feedback_v1_interface &wp_color_management_surface_feedback_v1_interface -- a
+?include-wayland color-management-v1.c [IF]
+    c-value wp_color_manager_v1_interface &wp_color_manager_v1_interface -- a
+    c-value wp_color_management_output_v1_interface &wp_color_management_output_v1_interface -- a
+    c-value wp_color_management_surface_v1_interface &wp_color_management_surface_v1_interface -- a
+    c-value wp_color_management_surface_feedback_v1_interface &wp_color_management_surface_feedback_v1_interface -- a
 [THEN]
-?include-wayland color-representation-v1.c
-c-value wp_image_description_creator_icc_v1_interface &wp_image_description_creator_icc_v1_interface -- a
-c-value wp_image_description_creator_params_v1_interface &wp_image_description_creator_params_v1_interface -- a
-c-value wp_image_description_v1_interface &wp_image_description_v1_interface -- a
-c-value wp_image_description_info_v1_interface &wp_image_description_info_v1_interface -- a
+?include-wayland color-representation-v1.c [IF]
+    c-value wp_image_description_creator_icc_v1_interface &wp_image_description_creator_icc_v1_interface -- a
+    c-value wp_image_description_creator_params_v1_interface &wp_image_description_creator_params_v1_interface -- a
+    c-value wp_image_description_v1_interface &wp_image_description_v1_interface -- a
+    c-value wp_image_description_info_v1_interface &wp_image_description_info_v1_interface -- a
 [THEN]
