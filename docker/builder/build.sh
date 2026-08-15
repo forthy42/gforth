581bd52f639dce60c04a51cea216afa54e784525
Bernd Paysan <bernd.paysan@gmx.de>
2026-08-15 18:12:10 +0200

    Another try at specifying the gcc version

--- docker/builder/build.sh
+++ docker/builder/build.sh
@@ -18,15 +18,15 @@ do
     arch1=$(echo $arch | tr '/' '-')
     for i in $VERSIONS
     do
-	case "$VERSION" in
-	    oldstable)
-		GCC=gcc
-		;;
+	case "$i" in
 	    stable)
 		GCC=gcc-14
 		;;
 	    unstable)
 		GCC=gcc-15
+		;;
+	    *)
+		GCC=gcc
 		;;
 	esac
 	docker build $* --network host --build-arg VERSION=$i --build-arg ARCH=${arch#*/} --build-arg GCC=$GCC --platform $arch -t forthy42/gforth-builder-$arch1:$i --progress=plain . 2>gforth-builder-$arch1-$i.log
