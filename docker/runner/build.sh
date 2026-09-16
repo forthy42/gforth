a30fbd7505d04df99a74592c7d0ce3c5a52eadbd
Bernd Paysan <bernd.paysan@gmx.de>
2026-09-16 14:16:28 +0200

    Bump version number

--- docker/runner/build.sh
+++ docker/runner/build.sh
@@ -3,10 +3,10 @@
 PLATFORMS=${PLATFORMS:-linux/amd64,linux/arm64}
 
 docker pull alpine:latest
-docker buildx build --platform $PLATFORMS --network host -f Dockerfile.swig -t forthy42/swig:latest .
-docker buildx build --platform $PLATFORMS --network host -t forthy42/gforth:latest .
-docker buildx build --platform $PLATFORMS --network host -f Dockerfile.gui -t forthy42/gforth-gui:latest .
-docker buildx build --platform $PLATFORMS --network host -f Dockerfile.gui+fonts -t forthy42/gforth-gui-fonts:latest .
+docker buildx build --platform $PLATFORMS --network host --progress=plain -f Dockerfile.swig -t forthy42/swig:latest . 2>swig-build.log
+docker buildx build --platform $PLATFORMS --network host --progress=plain -t forthy42/gforth:latest . 2>gforth-build.log
+docker buildx build --platform $PLATFORMS --network host --progress=plain -f Dockerfile.gui -t forthy42/gforth-gui:latest . 2>gforth-gui-build.log
+docker buildx build --platform $PLATFORMS --network host --progress=plain -f Dockerfile.gui+fonts -t forthy42/gforth-gui-fonts:latest . 2>gforth-fonts-build.log
 if [ "$1" != "nopush" ]
 then
     docker push forthy42/swig:latest
